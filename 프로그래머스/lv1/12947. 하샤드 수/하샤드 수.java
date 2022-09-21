755c804445256216f9c8c80f322e9050adc1d5a5
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-09-21 19:27:51 +0900

    [level 1] Title: 하샤드 수, Time: 0.83 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,12 +1,16 @@
 class Solution {
+    private int sum = 0;
     public boolean solution(int x) {
-        int sum = 0;
-        int n = x;
-        while(n > 0){
-            sum += (n % 10);
-            n /= 10;
-        }
+//         int sum = 0;
+//         int n = x;
+//         while(n > 0){
+//             sum += (n % 10);
+//             n /= 10;
+//         }
 
-        return (x % sum) == 0;
+//         return (x % sum) == 0;
+        sum = 0;
+        Integer.toString(x).chars().forEach(c -> sum += (c - '0'));
+        return x % sum == 0;
     }
 }
