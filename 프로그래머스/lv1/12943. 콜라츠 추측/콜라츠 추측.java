c13c5e5dc5b2c4aa18e84994e06babe78ee71674
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-09-22 21:46:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        while(num != 1){
+            if(answer >= 500){
+                answer = -1;
+                break;
+            }
+
+            num = (num % 2 == 0)? num/2 : num * 3 + 1;
+            answer++;
+        }
+
+        return answer;
+    }
+}
