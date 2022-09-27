c257fd7bd67db27a2a94d35bf1a18466bc03882c
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-09-27 16:26:46 +0900

    [level 1] Title: 소수 찾기, Time: 24.72 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -9,9 +9,11 @@ class Solution {
         flags[0] = flags[1] = false;
 
         // 주어진 n의 n*(1/2)까지만 검사하면 됨.
-        //
         for (int i = 2; i*i <=n ; i++) {
+            // 만약 i가 소수 혹은 아직 지워지지 않았다면.
             if(flags[i]){
+                // i의 배수 j들에 대해 flags[j] = false;
+                // i*i 미만은 이미 지워졌으므로 신경X
                 for (int j = i*i; j <= n; j+=i) {
                     flags[j] = false;
                 }
