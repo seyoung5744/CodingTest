400397898f9ed3729066140cca76135ea6a46e71
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-09-29 20:10:16 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,25 +1,24 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        ArrayList<Integer> set = new ArrayList<>();
+         int answer = 0;
+        ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
                 for (int k = j+1; k < nums.length; k++) {
-                    set.add(nums[i] + nums[j] + nums[k]);
+                    list.add(nums[i] + nums[j] + nums[k]);
                 }
             }
         }
 
-        for (int item : set){
+        for (int item : list){
             boolean flag = true;
             for (int i = 2; i*i <= item; i++) {
                 if(item % i == 0) {
                     flag = false;
                     break; // 소수가 아님
                 }
-                flag = true;
             }
 
             if(flag){
