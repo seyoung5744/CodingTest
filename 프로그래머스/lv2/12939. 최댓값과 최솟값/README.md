5781b5108e15b4a1d1f6c518d82e262bb6ea058e
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-10-03 22:21:26 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.21 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
 
 ### 성능 요약
 
-메모리: 73.7 MB, 시간: 8.26 ms
+메모리: 73.3 MB, 시간: 9.21 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,15 +1,12 @@
 class Solution {
     public String solution(String s) {
-        int min = Integer.parseInt(s.split(" ")[0]);
-        int max = Integer.parseInt(s.split(" ")[0]);
-        for (String ch : s.split(" ")){
+        String[] strNum = s.split(" ");
+        int min = Integer.parseInt(strNum[0]);
+        int max = Integer.parseInt(strNum[0]);
+        for (String ch : strNum){
             int num = Integer.parseInt(ch);
-            if(min > num){
-                min = num;
-            }
-            if(max < num){
-                max = num;
-            }
+            if(min > num) min = num;
+            if(max < num) max = num;
         }
 
         return min + " " +  max;
