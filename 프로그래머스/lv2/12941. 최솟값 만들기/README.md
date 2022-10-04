d4ead3c47b0b714f5a53d8fd9e5a7a73d2232414
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-10-04 21:28:58 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.25 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/README.md
+++ 프로그래머스/lv2/12941. 최솟값 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최솟값 만들기 - 12941 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941?language=java) 
 
 ### 성능 요약
 
-메모리: 60.4 MB, 시간: 2.33 ms
+메모리: 52.2 MB, 시간: 2.25 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,23 +1,14 @@
 import java.util.Arrays;
 class Solution
 {
-    public static int[] reverseSort(int[] arr){
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length/2; i++) {
-            int tmp = arr[i];
-            arr[i] = arr[ arr.length- i -1];
-            arr[arr.length - i - 1] = tmp;
-        }
-        return arr;
-    }
     public int solution(int []A, int []B)
     {
-        int answer = 0;
+       int answer = 0;
         Arrays.sort(A);
-        int[] b = reverseSort(B);
+        Arrays.sort(B);
 
         for (int i = 0; i < A.length; i++) {
-            answer += A[i] * b[i];
+            answer += A[i] * B[B.length - i -1];
         }
 
 
