a5692b88066cc8992f9e27ec7caccacbc3d765b1
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-10-05 20:07:43 +0900

    [level 2] Title: 숫자의 표현, Time: 0.09 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,12 +1,19 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            double x = (double)n/i - ((double)i/2) + 0.5;
+        
+        // 등차수열 공식 사용...
+//         for (int i = 1; i <= n; i++) {
+//             double x = (double)n/i - ((double)i/2) + 0.5;
 
-            if ((x == Math.floor(x)) && x > 0) {
+//             if ((x == Math.floor(x)) && x > 0) {
+//                 answer++;
+//             }
+//         }
+        
+         for (int i = 1; i <=n ; i+=2) {
+            if(n % i == 0)
                 answer++;
-            }
         }
 
         return answer;
