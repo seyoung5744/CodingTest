800632d52ee09d8277f2718bef4ba3b02e9fe566
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-10-09 20:10:06 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.39 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        int[] people = new int[n];
+        ArrayList<String> afterWords = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            people[i%n] += 1;
+//            System.out.println("사람 " + (i%n+1) + " 차례 : " + people[i%n]);
+            if(i > 0 && afterWords.get(i-1).charAt(afterWords.get(i-1).length()-1) != words[i].charAt(0)){
+                answer[0] = (i%n+1);
+                answer[1] = people[i%n];
+//                System.out.println("정답 " + (i%n+1)+ " 차례 : " + people[i%n]);
+                break;
+            }
+
+            if(afterWords.contains(words[i])){ // 같은 단어 말할 때.
+                answer[0] = (i%n+1);
+                answer[1] = people[i%n];
+                break;
+            }
+            afterWords.add(words[i]);
+        }
+      return answer;
+    }
+}
