25a61ca9729776cdea7f849aa981e7e9d8f4fd59
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-10-15 17:16:05 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,16 +1,17 @@
 class Solution {
     public long solution(int n) {
-         // long answer = 0;
+        int answer = 0;
         int a = 1;
         int b = 2;
+
         if(n<=1)
             return 1;
         for (int i = 1; i <n ; i++) {
-            int tmp = b;
+            answer = b;
             b = (a + b) % 1234567;
-            a = tmp;
+            a = answer;
         }
 
-        return a;
+        return answer;
     }
 }
