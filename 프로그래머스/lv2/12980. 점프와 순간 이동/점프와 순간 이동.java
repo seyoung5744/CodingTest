7a90de07389ebe93b5cc89e585c0c876a759b860
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-10-15 17:45:36 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -4,15 +4,12 @@ public class Solution {
         public int solution(int n) {
             int ans = 0;
 
-        while(n > 0){
-            if(n % 2 == 0){ // 짝수면
-                n /= 2;
-                // System.out.println("2 나누기 : " +n);
-            }else{ // 홀수면
-                n-=1;
-                ans+=1;
-                // System.out.println("1 빼기 : " + n);
+        while (n > 0) {
+            if (n % 2 == 1) {
+                n -= 1;
+                ans += 1;
             }
+            n /= 2;
         }
 
         return ans;
