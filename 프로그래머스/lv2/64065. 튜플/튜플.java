0567f9432b73e02008f2096604e24fa92ad150db
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-10-16 22:27:08 +0900

    [level 2] Title: 튜플, Time: 0.92 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -2,26 +2,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 class Solution {
     public int[] solution(String s) {
-        s = s.replaceAll("\\{", "").replaceAll("}", "");
-        String[] nums = s.split(",");
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
+         Set<String> set = new HashSet<>();
+        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
 
-        for (String num : nums) {
-            int number = Integer.parseInt(num);
-            if (!hashMap.containsKey(number)) {
-                hashMap.put(number, 1);
-            } else {
-                hashMap.put(number, hashMap.get(number) + 1);
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        for(String s1 : arr) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
             }
-        }
-
-        int[] answer = new int[hashMap.size()];
-        
-        List<Map.Entry<Integer, Integer>> entries = hashMap.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .collect(Collectors.toList());
-        for (int i = 0; i < entries.size(); i++) {
-            answer[i] = entries.get(entries.size()-i-1).getKey();
         }
         return answer;
     }
