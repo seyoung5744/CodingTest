f37625b05a4a0f5a4dd38c75e4d7346ef937a4f1
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-12-11 21:22:22 +0900

    [Silver III] Title: 스택 수열, Time: 4512 ms, Memory: 43868 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -3,12 +3,13 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        boolean flag = true;
         int idx = 1;
         Stack<Integer> stack = new Stack<>();
         ArrayList<String> answer = new ArrayList<>();
@@ -16,30 +17,29 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             int num = Integer.parseInt(br.readLine());
 
-            if (flag) {
-                if (!stack.contains(num)) {
-                    for (int j = idx; j <= num; j++) {
-                        stack.push(j);
-                        answer.add("+");
-                    }
-
-                    idx = num + 1;
+            if (!stack.contains(num)) {
+                for (int j = idx; j <= num; j++) {
+                    stack.push(j);
+                    answer.add("+");
                 }
 
-                if (stack.contains(num)) {
-                    if (stack.peek() == num) {
-                        stack.pop();
-                        answer.add("-");
-                    } else {
-                        answer.clear();
-                        answer.add("NO");
-                        flag = false;
-                    }
+                idx = num + 1;
+            }
+
+            if (stack.contains(num)) {
+                if (stack.peek() == num) {
+                    stack.pop();
+                    answer.add("-");
+                } else {
+                    answer.clear();
+                    answer.add("NO");
+                    break;
                 }
             }
+
         }
 
-        for(String an : answer){
+        for (String an : answer) {
             bw.write(an + "\n");
         }
 
