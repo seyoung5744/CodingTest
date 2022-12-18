65c9fa3960cb05c194072d2cc6d862dd2e0ff2f3
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2022-12-18 21:04:29 +0900

    [Silver V] Title: 영화감독 숌, Time: 352 ms, Memory: 158968 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int num = 666;
+        int cnt = 0;
+        while(true){
+            if(String.valueOf(num).contains("666")){
+                cnt++;
+            }
+
+            if(cnt == n){
+                break;
+            }
+            num++;
+        }
+
+        bw.write(num + "\n");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
