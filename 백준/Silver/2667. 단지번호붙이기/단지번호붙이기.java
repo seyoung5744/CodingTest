0ea104de0b36bf7ed6ee1c514e51b339494a847f
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-01-11 15:06:30 +0900

    [Silver I] Title: 단지번호붙이기, Time: 84 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -6,6 +6,7 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,7 +16,7 @@ public class Main {
 
     static int[][] dirt = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     static int N;
-    static int count = 1;
+    static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,28 +33,24 @@ public class Main {
         }
 
         visited = new boolean[N + 1][N + 1];
-        List<Integer> answer = new ArrayList<>();
-
-        int groupCnt = 1;
+        int total = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
 
         for (int i = 1; i < N + 1; i++) {
             for (int j = 1; j < N + 1; j++) {
                 if (map[i][j] == 1 && !visited[i][j]) {
-                    map[i][j] = groupCnt;
-                    visited[i][j] = true;
-                    dfs(i, j, groupCnt);
-                    answer.add(count);
-                    groupCnt += 1;
-                    count = 1;
+                    count = 0;
+                    total += 1;
+                    map[i][j] = -1;
+                    dfs(i, j);
+                    queue.add(count);
                 }
             }
         }
-        Collections.sort(answer);
 
-        bw.write(groupCnt - 1 + "\n");
-
-        for (int cnt : answer) {
-            bw.write(cnt + "\n");
+        bw.write(total + "\n");
+        while(!queue.isEmpty()){
+            bw.write(queue.poll() + "\n");
         }
 
         bw.flush();
@@ -61,17 +58,15 @@ public class Main {
         bw.close();
     }
 
-    public static void dfs(int i, int j, int groupCnt) {
-
+    public static void dfs(int i, int j) {
+        count += 1;
         for (int[] dir : dirt) {
             int x = i + dir[0];
             int y = j + dir[1];
 
-            if (x > 0 && y > 0 && x < N + 1 && y < N + 1 && map[x][y] == 1 && !visited[x][y]) {
-                visited[x][y] = true;
-                map[x][y] = groupCnt;
-                count += 1;
-                dfs(x, y, groupCnt);
+            if (x > 0 && y > 0 && x < N + 1 && y < N + 1 && map[x][y] == 1) {
+                map[x][y] = -1;
+                dfs(x, y);
             }
         }
     }
