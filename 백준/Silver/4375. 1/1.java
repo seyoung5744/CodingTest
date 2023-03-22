22338860523c7f7fff52a72783043d7ee2a53cfb
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-03-22 15:17:07 +0900

    [Silver III] Title: 1, Time: 76 ms, Memory: 11344 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,33 +1,27 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String strNum = "";
         while ((strNum = br.readLine()) != null) {
             int n = Integer.parseInt(strNum);
-
-            long onlyOne = 0;
+            int onlyOne = 0;
 
             for (int cnt = 1; ; cnt++) {
-                onlyOne = (onlyOne * 10 + 1) % n;
-                if (onlyOne % n == 0) {
+                onlyOne = (onlyOne * 10 + 1)% n;
+
+                if (onlyOne == 0) {
                     System.out.println(cnt);
                     break;
                 }
             }
         }
 
-        bw.flush();
         br.close();
-        bw.close();
     }
 }
