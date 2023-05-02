c9ea0d7692281825253d55fcf75327a61aaf257f
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-05-02 15:01:50 +0900

    [Bronze II] Title: 럭키 스트레이트, Time: 108 ms, Memory: 12752 KB -BaekjoonHub

---
+++ 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+
+    public static int summary;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String strNum = sc.next();
+
+        int length = strNum.length();
+
+        for (int i = 0; i < length / 2; i++) {
+            summary += (strNum.charAt(i) - '0');
+        }
+        for (int i = length / 2; i < length; i++) {
+            summary -= (strNum.charAt(i) - '0');
+        }
+
+        if(summary == 0){
+            System.out.println("LUCKY");
+        }else{
+            System.out.println("READY");
+        }
+    }
+}
