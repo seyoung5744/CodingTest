cc8ef5e0e764336f0ca24727822ed35348386542
seyoung5744 <seyoung574458@gmail.com>
2023-05-12 21:30:01 +0900

    Update 06_정렬 : 선택정렬

--- java-for-coding-test/src/유형별_일고리즘_이론/_06_정렬/선택정렬.java
+++ java-for-coding-test/src/유형별_일고리즘_이론/_06_정렬/선택정렬.java
@@ -2,13 +2,14 @@ package 유형별_일고리즘_이론._06_정렬;
 
 import java.util.Arrays;
 
-public class 선택정력 {
+public class 선택정렬 {
 
     public static int[] array = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
 
     public static void main(String[] args) {
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             int min_idx = i;
+
             for (int j = i + 1; j < array.length; j++) {
                 if (array[min_idx] > array[j]) {
                     min_idx = j;
