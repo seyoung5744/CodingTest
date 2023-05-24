32feedbe07a376fb19966bb27e5a19a455f7c686
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-05-24 21:53:53 +0900

    [level 4] Title: 가사 검색, Time: 278.55 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/60060. 가사 검색/가사 검색.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class Solution {
+     public static int lowerRound(ArrayList<String> arr, String target, int start, int end) {
+        while(start < end){
+            int mid = (start + end) / 2;
+            if(arr.get(mid).compareTo(target) > 0) end = mid;
+            else start = mid + 1;
+        }
+        return end;
+    }
+
+    public static int upperRound(ArrayList<String> arr, String target, int start, int end) {
+        while(start<end){
+            int mid = (start + end) / 2;
+            // arr[mid]가 target보다 사전순으로 뒤에 있다면
+            if(arr.get(mid).compareTo(target) > 0) end = mid;
+            else start = mid + 1;
+        }
+        return end;
+    }
+
+    public static int countByRange(ArrayList<String> arr, String leftValue, String rightValue) {
+        int leftIndex = lowerRound(arr, leftValue, 0, arr.size());
+        int rightIndex = upperRound(arr, rightValue, 0, arr.size());
+        return rightIndex - leftIndex;
+    }
+
+    // 모든 단어들을 길이마다 나누어서 저장하기 위한 리스트
+    public static ArrayList<ArrayList<String>> arr = new ArrayList<>();
+    // 모든 단어들을 길이마다 나누어서 뒤집어 저장하기 위한 리스트
+    public static ArrayList<ArrayList<String>> reversedArr = new ArrayList<>();
+    public int[] solution(String[] words, String[] queries) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < 10001; i++) {
+            arr.add(new ArrayList<>());
+            reversedArr.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            arr.get(word.length()).add(word);
+            word = (new StringBuilder(word)).reverse().toString();
+            reversedArr.get(word.length()).add(word);
+        }
+
+        for (int i = 0; i < 10001; i++) {
+            Collections.sort(arr.get(i));
+            Collections.sort(reversedArr.get(i));
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            String word = queries[i];
+
+            int res = 0;
+            if (word.charAt(0) != '?') {
+                res = countByRange(arr.get(word.length()), word.replaceAll("\\?","a"), word.replaceAll("\\?","z"));
+            } else {
+                word = (new StringBuilder(word)).reverse().toString();
+                res = countByRange(reversedArr.get(word.length()), word.replaceAll("\\?","a"), word.replaceAll("\\?","z"));
+            }
+            result.add(res);
+        }
+
+        // 배열로 바꾸어 반환
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+        return answer;
+    }
+}
