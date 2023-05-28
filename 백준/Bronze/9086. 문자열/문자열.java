93f4c60e466357d074dd2d436bd72a0de0996db2
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-05-28 17:10:15 +0900

    [Bronze V] Title: 문자열, Time: 108 ms, Memory: 12812 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while(t-->0){
+            String s = sc.next();
+            System.out.println(s.charAt(0)+""+s.charAt(s.length()-1));
+        }
+    }
+
+}
