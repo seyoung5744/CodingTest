2d0e7a3d36dbf8e2162acacb12953f80e29c5cbb
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-06-03 20:36:52 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1764 ms, Memory: 296344 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,51 +1,33 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+// https://st-lab.tistory.com/104
 public class Main {
 
-    public static int[] arr; // 수열의 원소
-    public static int[] counting = new int[10_001]; // 수의 범위 1~10,000
-    public static int[] result; // 정렬된 배열
+    public static int[] arr = new int[10_001]; // 수열의 원소
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[num] += 1;
+        }
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            while (arr[i]-- > 0) {
+                bw.write(i + "\n");
+            }
         }
-
-        // array 의 value 값을 index 로 하는 counting 배열 값 1 증가
-        for (int i = 0; i < arr.length; i++) {
-            counting[arr[i]]++;
-        }
-
-        // 누적 합 해주기 
-        for (int i = 1; i < counting.length; i++) {
-            counting[i] += counting[i - 1];
-        }
-
-        /*
-         *  i 번쨰 원소를 인덱스로 하는 counting 배열을 1 감소시킨 뒤
-         *  counting 배열의 값을 인덱스로 하여 result에 value 값을 저장한다.
-         */
-        for (int i = arr.length - 1; i >= 0; i--) {
-            int value = arr[i];
-            counting[value] -= 1;
-            result[counting[value]] = value;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < result.length; i++) {
-            sb.append(result[i]).append("\n");
-        }
-        bw.write(sb.toString());
 
         br.close();
         bw.close();
     }
+
 }
