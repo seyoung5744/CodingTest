84a5912069b4712d8948e440085469156779451f
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-06-16 20:00:28 +0900

    [level 2] Title: 문자열 압축, Time: 0.11 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/README.md
+++ 프로그래머스/lv2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
 
 ### 성능 요약
 
-메모리: 79.8 MB, 시간: 22.71 ms
+메모리: 74.6 MB, 시간: 0.11 ms
 
 ### 구분
 
--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -1,33 +1,44 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
-        int answer = s.length();
-
-        for (int step = 1; step < s.length() / 2 + 1; step++) {
-            String compressed = "";
-            String pre = s.substring(0, step);
-            int cnt = 1;
-
-
-            for (int i = step; i < s.length(); i += step) {
-                String sub = "";
-
-                for (int j = i; j < i + step; j++) {
-                    if(j < s.length()) sub += s.charAt(j);
-                }
-
-                if(pre.equals(sub)) cnt += 1;
-                else{
-                    compressed += (cnt >= 2)? cnt + pre : pre;
-
-                    pre = sub; // 다시 상태 초기화
-                    cnt = 1;
-                }
-            }
-
-            compressed += (cnt >= 2)? cnt + pre : pre;
-            answer = Math.min(answer, compressed.length());
+        int min = Integer.MAX_VALUE;
+        for (int length = 1; length <= s.length() / 2 + 1; length++) {
+            int compressed = compress(s, length);
+            if(min > compressed) min = compressed;
         }
-        
-        return answer;
+
+        return min;
+    }
+
+    private List<String> split(String source, int length) {
+        List<String> tokens = new ArrayList<>();
+        for (int startIndex = 0; startIndex < source.length(); startIndex += length) {
+            int endIndex = startIndex + length;
+            if(endIndex > source.length()) endIndex = source.length();
+            tokens.add(source.substring(startIndex, endIndex));
+        }
+        return tokens;
+    }
+
+    private int compress(String source, int length) {
+        StringBuilder sb = new StringBuilder();
+        String pre = "";
+        int count = 0;
+        for(String token : split(source, length)){
+            // 압축 문자열 구성
+            if(token.equals(pre)){
+                count++;
+            }else{
+                if(count > 1) sb.append(count);
+                sb.append(pre);
+                pre = token;
+                count = 1;
+            }
+        }
+
+        if(count > 1) sb.append(count);
+        sb.append(pre);
+        return sb.length();
     }
 }
