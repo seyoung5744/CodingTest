96f4371ef03603ca49e82914580256be40c6a2cc
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-06-16 21:03:16 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 73.1 MB, 시간: 1.25 ms
+메모리: 72.8 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,8 +1,13 @@
 class Solution {
+    private final static String[] words = {
+        "zero","one","two","three","four","five","six","seven","eight","nine"
+    };
+    
     public int solution(String s) {
-        String[] eng = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for (int i = 0; i < eng.length; i++) {
-            s = s.replaceAll(eng[i], Integer.toString(i));
+        int answer = 0;
+        
+        for(int num = 0; num < words.length; num++){
+            s = s.replace(words[num], Integer.toString(num));
         }
         
         return Integer.parseInt(s);
