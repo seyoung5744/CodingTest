59567208f08d5d84c4336e64b44c3ecfb037d371
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-06-25 21:50:03 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,29 +1,16 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        ArrayList<Integer> aliquotNum = new ArrayList<>();
-        int sum = brown + yellow;
-        for (int i = 1; i <= sum/2; i++) {
-            if(sum % i == 0){
-                aliquotNum.add(i);
-            }
-        }
-        aliquotNum.add(sum);
-
-        int row = 0, col = 0;
-        int length = aliquotNum.size() % 2 == 0? aliquotNum.size()/2 : (aliquotNum.size()/2) +1;
-        for (int i = 0; i < length; i++) {
-            row = aliquotNum.get(i);
-            col = sum / row;
-            if( col >= row){
-                if((row - 2)*(col -2)==yellow){
-                    answer[0] = col;
-                    answer[1] = row;
+        for (int width = 3; width <= 5000; width++) {
+            for (int height = 3; height <= width; height++) {
+                int boundary = (width + height - 2) * 2;
+                int center = width * height - boundary;
+                
+                if(boundary == brown && center == yellow){
+                    return new int[]{width, height};
                 }
             }
         }
-        return answer;
+        
+        return null;
     }
 }
