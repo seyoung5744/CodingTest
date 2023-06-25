e8f323571a4c03ed8cf9ac1d580305eee51a92fd
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-06-25 21:38:21 +0900

    [level 1] Title: 모의고사, Time: 3.91 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,37 +1,35 @@
-import java.util.*;
-class Solution {
-    public int[] solution(int[] answers) {
-        int[][] students = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
-        int[] count = new int[3];
+import java.util.stream.*;
 
-        for (int i = 0; i < answers.length; i++) { // 0 1 2 3 4 / 5 6 7 8 9
-            for (int j = 0; j < students.length; j++) {
-                if(answers[i] == students[j][(students[j].length + i) % students[j].length]){
-                    count[j]++;
+class Solution {
+    public static final int[][] students = {
+        {1, 2, 3, 4, 5},
+        {2, 1, 2, 3, 2, 4, 2, 5},
+        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
+
+    public static int idx, count;
+
+    public int[] solution(int[] answers) {
+        int[] corrects = new int[3];
+        int max = 0;
+
+        for (int i = 0; i < students.length; i++) {
+            int length = students[i].length;
+            for (int answer : answers) {
+                if (answer == students[i][idx++ % length]) {
+                    count++;
                 }
             }
-        }
-        System.out.println("맞힌 개수 : " + Arrays.toString(count));
-
-        int max = count[0];
-        for (int i = 0; i < count.length; i++) {
-            if(count[i] > max){
-                max = count[i];
-            }
+            corrects[i] = count;
+            max = Math.max(max, count);
+            idx = 0;
+            count = 0;
         }
 
-        ArrayList<Integer> an = new ArrayList<>();
-        for (int i = 0; i < count.length; i++) {
-            if(max == count[i]){
-                an.add(i+1);
-            }
-        }
-        
-        int[] answer = an.stream()
-                .mapToInt(i -> i)
-                .toArray();
-
-        return answer;
+        final int maxCorrects = max;
+        return IntStream.range(0, 3)
+            .filter(x -> corrects[x] == maxCorrects)
+            .map(x -> x + 1)
+            .toArray();
     }
-    
 }
