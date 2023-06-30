5ddb5216ad008117d750e04b2aaec4dedc933129
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-06-30 20:40:40 +0900

    [level 2] Title: 가장 큰 수, Time: 8.94 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,24 +1,22 @@
 import java.util.*;
-
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        String[] numStr = new String[numbers.length];
+        String[] strArr = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            numStr[i] = numbers[i] + "";
+            strArr[i] = numbers[i] + "";
         }
 
-        Arrays.sort(numStr, new Comparator<String>() {
+        Arrays.sort(strArr, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 return (o2 + o1).compareTo(o1 + o2);
             }
         });
-        if(numStr[0].equals("0")) return "0";
-        for(String str : numStr){
-            answer+= str;
+
+        if(strArr[0].equals("0")){
+            return "0";
         }
 
-        return answer;
+        return String.join("", strArr);
     }
 }
