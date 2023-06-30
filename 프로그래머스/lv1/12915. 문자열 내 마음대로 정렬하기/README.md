ec275e9aa31a703613edaeea0e8d6bc3e92c7bb6
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-06-30 20:25:06 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.92 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/README.md
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 마음대로 정렬하기 - 12915 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915) 
 
 ### 성능 요약
 
-메모리: 70.2 MB, 시간: 1.18 ms
+메모리: 80.1 MB, 시간: 0.92 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,19 +1,13 @@
-import java.util.*;
-
+import java.util.Arrays;
 class Solution {
     public String[] solution(String[] strings, int n) {
-        Arrays.sort(strings, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if(o1.charAt(n) > o2.charAt(n)){
-                    return 1;
-                }else if(o1.charAt(n) == o2.charAt(n)){
-                    return o1.compareTo(o2);
-                }else{
-                    return -1;
-                }
+        Arrays.sort(strings, (s1, s2) -> {
+            if(s1.charAt(n) != s2.charAt(n)){
+                return s1.charAt(n) - s2.charAt(n);
             }
+            return s1.compareTo(s2);
         });
+
         return strings;
     }
 }
