e7ea969e62dcd30c38007b17efa381619638bafa
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-07-17 20:34:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 167.51 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,33 +1,23 @@
 import java.util.*;
+
 class Solution {
-    public static Map<String, Integer> toHash(String[] participant){
-        Map<String, Integer> part = new HashMap<>();
-        int count = 1;
-        for (String s : participant){
-            if(part.containsKey(s)) {
-                part.put(s, part.get(s)+ 1);
-            }else{
-                part.put(s, count);
-            }
-        }
-        return part;
-    }
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Map<String, Integer> part = toHash(participant);
-
-        for (String comp : completion){
-            if(part.containsKey(comp)){
-                part.put(comp,part.get(comp)-1);
+        Map<String, Integer> map = new HashMap<>();
+        for(String name : participant){
+            map.putIfAbsent(name, 0);
+            map.put(name, map.get(name) + 1);
+        }
+        
+        for(String name : completion){
+            map.put(name, map.get(name) - 1);
+        }
+        
+        
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue() != 0){
+                return entry.getKey();
             }
         }
-
-        for (String name : part.keySet()){
-            if(part.get(name) == 1){
-                answer = name;
-                break;
-            }
-        }
-        return answer;
-    }    
+        return "";
+    }
 }
