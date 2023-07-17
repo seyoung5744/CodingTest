33787ef1e1bfa55a44fb478a10bf88b1659a1b3f
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-07-17 20:13:12 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,10 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int sum = 0;
+        Set<Integer> set = new HashSet<>();
         for(int num : numbers){
-            sum+=num;
+            set.add(num);
         }
         
-        return 45 - sum;
+        int answer = 0;
+        for (int i = 0; i < 10; i++) {
+            if(!set.contains(i)) answer += i;
+        }
+        
+        return answer;
     }
 }
