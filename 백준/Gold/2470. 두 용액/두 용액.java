d60a2e9a087401fe78a817bc2a5da501217c0190
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-07-23 17:48:35 +0900

    [Gold V] Title: 두 용액, Time: 280 ms, Memory: 32452 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,56 +1,46 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 용액 개수
-
-        st = new StringTokenizer(br.readLine());
-        int[] solution = new int[N];
-        for (int i = 0; i < N; i++) {
-            solution[i] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        String[] strArr = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(strArr[i]);
         }
 
-        Arrays.sort(solution);
-        int start = 0;
-        int end = N - 1;
-        int min = Integer.MAX_VALUE;
+        Arrays.sort(nums);
 
-        int a1 = 0;
-        int a2 = 0;
-        while (start < end) {
-            int sum = solution[start] + solution[end];
+        int start = 0;
+        int end = nums.length - 1;
+
+        int min = Integer.MAX_VALUE;
+        int[] result = new int[2];
+
+        while(start < end){
+            int sum = nums[start] + nums[end];
             int abs = Math.abs(sum);
-            if (abs < min) {
+
+            if(abs < min){
                 min = abs;
-                a1 = solution[start];
-                a2 = solution[end];
+                result[0] = nums[start];
+                result[1] = nums[end];
             }
 
-            if (sum < 0) {
+            if(sum < 0){
                 start++;
-            } else {
+            }else{
                 end--;
             }
         }
 
-        System.out.println(a1 + " " + a2);
-
-        bw.flush();
-        br.close();
-        bw.close();
+        System.out.println(result[0] + " " + result[1]);
     }
 
 }
