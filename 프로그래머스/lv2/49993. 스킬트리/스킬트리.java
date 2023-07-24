5e1f3cf132effc715092b0adb43112b1e9c3bee6
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-07-24 21:32:24 +0900

    [level 2] Title: 스킬트리, Time: 10.81 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        return (int) Arrays.stream(skill_trees)
+            .map(s -> s.replaceAll("[^" + skill + "]", ""))
+            .filter(skill::startsWith)
+            .count();
+    }
+}
