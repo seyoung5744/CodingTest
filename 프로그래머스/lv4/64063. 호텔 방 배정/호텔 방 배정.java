70a3727605dfee3c8db7463829e9ce2fec7ee958
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-07-24 20:58:03 +0900

    [level 4] Title: 호텔 방 배정, Time: 334.96 ms, Memory: 153 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+    public static Map<Long, Long> roomInfo = new HashMap<>();
+
+    private static long findEmptyRoom(long wantRoom){
+        if(!roomInfo.containsKey(wantRoom)){
+            roomInfo.put(wantRoom, wantRoom + 1);
+            return wantRoom;
+        }
+
+        long nextRoom = roomInfo.get(wantRoom);
+        long emptyRoom = findEmptyRoom(nextRoom);
+        roomInfo.put(wantRoom, emptyRoom);
+        return emptyRoom;
+    }
+
+    public long[] solution(long k, long[] room_number) {
+        long[] answer = new long[room_number.length];
+        for (int i = 0; i < room_number.length; i++) {
+            answer[i] = findEmptyRoom(room_number[i]);
+        }
+        return answer;
+    }
+}
