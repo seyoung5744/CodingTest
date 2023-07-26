6bd9b79b441e9502351edd8bf63a65c995105f95
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-07-26 21:26:40 +0900

    [level 1] Title: 체육복, Time: 1.65 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -1,36 +1,38 @@
-import java.util.*;
+import java.util.Arrays;
 
-class Solution {
+public class Solution {
+
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0; 
-        int[] student = new int[n];
         Arrays.sort(lost);
         Arrays.sort(reserve);
+
+        int[] students = new int[n];
         for (int i = 0; i < n; i++) {
-            if( i < reserve.length ){
-                student[reserve[i]-1] += 1;
+            if (i < lost.length) {
+                students[lost[i] - 1] -= 1;
             }
-            if(i < lost.length ){
-                student[lost[i]-1] -= 1;
+            if (i < reserve.length) {
+                students[reserve[i] - 1] += 1;
             }
-            student[i] += 1;
+            students[i] += 1;
+        }
+        
+        for (int i = 0; i < lost.length; i++) {
+            int idx = lost[i] - 1;
+
+            if (idx > 0 && students[idx - 1] > 1) {
+                students[idx] = 1;
+                students[idx- 1] -= 1;
+            } else if (idx < n-1 && students[idx + 1] > 1) {
+                students[idx] = 1;
+                students[idx + 1] -= 1;
+            }
         }
 
-        for(int lo : lost){
-            int idx = lo-1;
-            if(idx-1 > -1 && student[idx-1] > 1){
-                student[idx]=1;
-                student[idx-1] -= 1;
-            }else if(idx+1 < n && student[idx+1] > 1){
-                student[idx] = 1;
-                student[idx+1] -= 1;
-            }
-        }
-        for(int s : student){
-            if(s != 0){
-                answer++;
-            }
-        }
-        return answer;
+        return (int)Arrays.stream(students)
+            .filter(s -> s > 0)
+            .count();
+
     }
+
 }
