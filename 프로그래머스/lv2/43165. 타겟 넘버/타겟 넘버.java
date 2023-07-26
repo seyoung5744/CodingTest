ee08144094e2e75c263e59a887616830b4940009
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-07-26 20:21:59 +0900

    [level 2] Title: 타겟 넘버, Time: 0.41 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static int result;
+    public static boolean[] isVisited;
+
+    public int solution(int[] numbers, int target) {
+        isVisited = new boolean[numbers.length];
+
+        dfs(0, numbers, target, 0);
+
+        return result;
+    }
+
+    private static void dfs(int start, int[] numbers, int target, int count) {
+
+        if (start == numbers.length) {
+            if (count == target) {
+                result += 1;
+            }
+            return;
+        }
+
+        dfs(start + 1, numbers, target, count + numbers[start]);
+        dfs(start + 1, numbers, target, count - numbers[start]);
+    }
+}
