4eb466b9ef6596219ed3f689cd68b86700edeb32
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-08-01 20:55:29 +0900

    [level 1] Title: 시저 암호, Time: 1.80 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,21 +1,20 @@
 class Solution {
-    public String solution(String s, int n){
+    public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
-        
         for(char c : s.toCharArray()){
             sb.append(push(c, n));
         }
-        
         return sb.toString();
     }
-    
-    private char push(char c, int n){
-//        if(!Character.isAlphabetic(c)) return c;
-        if(!(c >= 'A' && c <= 'Z') && !(c >= 'a' && c <= 'z')) return c;
-        
-        int offset = Character.isUpperCase(c)? 'A' : 'a';
+
+    private static char push(char c, int n){
+        if(!Character.isAlphabetic(c)) return c;
+
+        int offset = Character.isUpperCase(c) ? 'A':'a';
         int position = c - offset;
         position = (position + n) % ('Z' - 'A' + 1);
-        return (char)(offset + position);
+
+        return (char)(position + offset);
     }
+
 }
