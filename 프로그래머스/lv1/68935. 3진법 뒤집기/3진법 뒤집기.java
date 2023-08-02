59924d91de400025d7ed9d6b2f9d655d5c88e6b2
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-08-02 20:51:30 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(int n) {
-        String str = Integer.toString(n, 3);
-        String reversed = new StringBuilder(str).reverse().toString();
-        return Integer.parseInt(reversed, 3);
+        return Integer.parseInt(new StringBuilder(Integer.toString(n, 3)).reverse().toString(), 3);
     }
 }
