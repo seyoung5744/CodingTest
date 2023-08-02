9fff27fa34b5826a7fb9e106dc8e8a808c1b9853
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-08-02 20:37:45 +0900

    [level 2] Title: 문자열 압축, Time: 0.12 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -1,44 +1,52 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public int solution(String s) {
         int min = Integer.MAX_VALUE;
-        for (int length = 1; length <= s.length() / 2 + 1; length++) {
-            int compressed = compress(s, length);
-            if(min > compressed) min = compressed;
+        for (int length = 1; length <= s.length(); length++) {
+            int compress = compress(s, length);
+            if (compress < min) {
+                min = compress;
+            }
         }
-
         return min;
     }
 
-    private List<String> split(String source, int length) {
+    private static List<String> split(String source, int length) {
         List<String> tokens = new ArrayList<>();
         for (int startIndex = 0; startIndex < source.length(); startIndex += length) {
             int endIndex = startIndex + length;
-            if(endIndex > source.length()) endIndex = source.length();
+            if (endIndex > source.length()) {
+                endIndex = source.length();
+            }
             tokens.add(source.substring(startIndex, endIndex));
         }
         return tokens;
     }
 
-    private int compress(String source, int length) {
+    private static int compress(String source, int length) {
         StringBuilder sb = new StringBuilder();
-        String pre = "";
+
+        String last = "";
         int count = 0;
-        for(String token : split(source, length)){
-            // 압축 문자열 구성
-            if(token.equals(pre)){
-                count++;
-            }else{
-                if(count > 1) sb.append(count);
-                sb.append(pre);
-                pre = token;
+        for (String token : split(source, length)) {
+            if (token.equals(last)) {
+                count += 1;
+            } else {
+                if (count > 1) {
+                    sb.append(count);
+                }
+                sb.append(last);
+                last = token;
                 count = 1;
             }
         }
+        if (count > 1) {
+            sb.append(count);
+        }
+        sb.append(last);
 
-        if(count > 1) sb.append(count);
-        sb.append(pre);
         return sb.length();
     }
 }
