2a75e6a7b641da4d7f98b883a1cd98662dce0988
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-08-06 17:39:21 +0900

    [level 1] Title: 모의고사, Time: 4.15 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,34 +1,33 @@
+import java.util.*;
 import java.util.stream.*;
 
 class Solution {
-    public static final int[][] students = {
+    private static final int[][] STUDENTS = {
         {1, 2, 3, 4, 5},
         {2, 1, 2, 3, 2, 4, 2, 5},
         {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
     };
 
-    public static int idx, count;
+    private static int count;
 
     public int[] solution(int[] answers) {
         int[] corrects = new int[3];
-        int max = 0;
+        int max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < students.length; i++) {
-            int length = students[i].length;
-            for (int answer : answers) {
-                if (answer == students[i][idx++ % length]) {
-                    count++;
+        for (int i = 0; i < STUDENTS.length; i++) {
+            for (int j = 0; j < answers.length; j++) {
+                if (STUDENTS[i][j % STUDENTS[i].length] == answers[j]) {
+                    count += 1;
                 }
             }
             corrects[i] = count;
             max = Math.max(max, count);
-            idx = 0;
             count = 0;
         }
 
-        final int maxCorrects = max;
+        final int maxCorrect = max;
         return IntStream.range(0, 3)
-            .filter(x -> corrects[x] == maxCorrects)
+            .filter(i -> corrects[i] == maxCorrect)
             .map(x -> x + 1)
             .toArray();
     }
