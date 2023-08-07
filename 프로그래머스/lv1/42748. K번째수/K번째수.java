72e532652544c0c44c4d13b8ac13c0135459253a
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-08-07 19:48:15 +0900

    [level 1] Title: K번째수, Time: 0.39 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -5,16 +5,24 @@ class Solution {
         int[] answer = new int[commands.length];
 
         for (int i = 0; i < commands.length; i++) {
-            int[] command = commands[i];
-            
-            int from = command[0] - 1;
-            int to = command[1];
-            int k = command[2] - 1;
-            
-            int[] sub = Arrays.copyOfRange(array, from, to);
-            Arrays.sort(sub);
-            answer[i] = sub[k];
+            int from = commands[i][0] - 1;
+            int to = commands[i][1];
+            int target = commands[i][2] - 1;
+
+            answer[i] = sortedSubArray(from, to, array)[target];
         }
         return answer;
+    }
+
+    private int[] sortedSubArray(int start, int end, int[] array) {
+        int[] subArray = new int[end - start];
+        int idx = 0;
+        for (int i = start; i < end; i++) {
+            subArray[idx++] = array[i];
+        }
+
+        Arrays.sort(subArray);
+
+        return subArray;
     }
 }
