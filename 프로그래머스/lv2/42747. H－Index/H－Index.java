b1eb5f6f09281db6c7fb85cdc14c34948ab644eb
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-08-07 20:21:31 +0900

    [level 2] Title: H-Index, Time: 0.54 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -1,17 +1,21 @@
 import java.util.*;
 
 class Solution {
-    private boolean isValid(int[] citations, int h){
-        int idx = citations.length - h;
-        return citations[idx] >= h;
-    }
     public int solution(int[] citations) {
+        int answer = 0;
         Arrays.sort(citations);
-        
+
         for (int h = citations.length; h >= 1; h--) {
             if(isValid(citations, h)) return h;
         }
-        
-        return 0;
+        return answer;
+    }
+
+    private boolean isValid(int[] citations, int h) {
+        int count = 0;
+        for(int citation : citations){
+            if(citation >= h) count++;
+        }
+        return count >= h;
     }
 }
