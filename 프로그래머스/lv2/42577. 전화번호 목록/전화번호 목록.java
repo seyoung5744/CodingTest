9c4d359c9b9fbe1873c3763e61586bba9d632173
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-08-10 20:38:31 +0900

    [level 2] Title: 전화번호 목록, Time: 305.97 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,17 +1,11 @@
 import java.util.*;
+
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> prefixes = new HashSet<>();
-        for(String phone : phone_book){
-            for (int end = 1; end < phone.length(); end++) {
-                prefixes.add(phone.substring(0, end));
-            }
-        }
-        
-        for(String phone : phone_book){
-            if(prefixes.contains(phone)){
-                return false;
-            }
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) return false;
         }
         
         return true;
