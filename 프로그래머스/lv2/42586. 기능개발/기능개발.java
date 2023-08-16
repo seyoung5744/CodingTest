f4674bd74fbf973bce49774abb14e93f91af4827
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-08-16 17:02:51 +0900

    [level 2] Title: 기능개발, Time: 2.21 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,32 +1,32 @@
 import java.util.*;
 
-class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> result = new ArrayList<>();
+public class Solution {
 
-        Queue<Integer> queue = new LinkedList<>();
+    public static int[] solution(int[] progresses, int[] speeds) {
+        Deque<Integer> q = new ArrayDeque<>();
         for (int i = 0; i < progresses.length; i++) {
-            queue.add(i);
+            q.add(i);
         }
 
-        int cnt = 0;
-        while (!queue.isEmpty()) {
-
-            for (int i = 0; i < speeds.length; i++) {
-                progresses[i] += speeds[i];
-
-                if (progresses[queue.peek()] >= 100) {
-                    cnt += 1;
-                    queue.poll();
+        List<Integer> result = new ArrayList<>();
+        int days = 0;
+        int count = 0;
+        while (!q.isEmpty()) {
+            int idx = q.poll();
+            int expiration = (int) Math.ceil((double) (100 - progresses[idx]) / speeds[idx]);
+  
+            if(expiration > days){
+                if(days != 0){
+                    result.add(count);
+                    count = 0;
                 }
+                days = expiration;
             }
-
-            if(cnt != 0){
-                result.add(cnt);
-                cnt = 0;
-            }
+            count++;
         }
-        
+
+        result.add(count);
         return result.stream().mapToInt(Integer::intValue).toArray();
     }
+
 }
