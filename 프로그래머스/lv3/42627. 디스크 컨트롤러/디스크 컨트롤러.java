d52af0ddd3e10506db40a32062e676643b444101
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-08-21 17:01:35 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.83 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Solution {
+
+    static class Disk {
+
+        public final int start;
+        public final int jobTime;
+
+        public Disk(int start, int jobTime) {
+            this.start = start;
+            this.jobTime = jobTime;
+        }
+    }
+
+    public int solution(int[][] jobs) {
+        Disk[] disks = new Disk[jobs.length];
+
+        for (int i = 0; i < jobs.length; i++) {
+            disks[i] = new Disk(jobs[i][0], jobs[i][1]);
+        }
+
+        Arrays.sort(disks, (a, b) -> a.start - b.start);
+
+        Queue<Disk> q = new LinkedList<>(Arrays.asList(disks));
+        PriorityQueue<Disk> pq = new PriorityQueue<>((a, b) -> a.jobTime - b.jobTime);
+
+        int exec = 0; // 누적 시간
+        int time = 0; // 현재 시간
+
+        while(!q.isEmpty() || !pq.isEmpty()) {
+            while(!q.isEmpty() && q.peek().start <= time){
+                pq.add(q.poll());
+            }
+
+            if(pq.isEmpty()) {
+                time = q.peek().start;
+                continue;
+            }
+
+            Disk curJob = pq.poll();
+            exec += time + curJob.jobTime - curJob.start;
+            time += curJob.jobTime;
+        }
+
+        return exec / disks.length;
+    }
+}
