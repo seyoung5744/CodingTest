70ff6f0e6c7716b4c34fd865beee0387a14f0302
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-09-01 17:25:48 +0900

    [Silver III] Title: 파도반 수열, Time: 80 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,32 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
-    private static Long[] dp = new Long[101];
+    public static long[] dp = new long[101];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        dp[0] = 0L;
-        dp[1] = dp[2] = dp[3] = 1L;
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
 
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            System.out.println(triangle(num));
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            System.out.println(triangle(N));
         }
     }
 
-    private static Long triangle(int n) {
-
-        if (dp[n] == null) {
-            dp[n] = triangle(n - 2) + triangle(n - 3);
+    public static long triangle(int N) {
+        if (dp[N] == -1) {
+            return dp[N] = triangle(N - 2) + triangle(N - 3);
         }
-
-        return dp[n];
+        return dp[N];
     }
 
 }
