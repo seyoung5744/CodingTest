efb4a7a2058e3779272e4052d44f1f5d6ed02a60
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-09-12 17:54:15 +0900

    [level 2] Title: 삼각 달팽이, Time: 16.87 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,39 +1,38 @@
 public class Solution {
+    public static final int[] dx = {0, 1, -1}; // 하, 우, 대각선 위
+    public static final int[] dy = {1, 0, -1};
 
-    private static final int[] dx = {0, 1, -1};
-    private static final int[] dy = {1, 0, -1};
-    
-    public int[] solution(int n) {
+    public static int[] solution(int n) {
         int[][] triangle = new int[n][n];
-        int v = 1;
+        int num = 1;
         int x = 0;
         int y = 0;
-        int d = 0;
-        
-        while(true){
-            triangle[y][x] = v++;
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            
-            if(nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0){
-                d = (d + 1) % 3;
-                nx = x + dx[d];
-                ny = y + dy[d];
+        int direction = 0;
+
+        while(true) {
+            triangle[y][x] = num++;
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
+
+            if(nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                direction = (direction + 1) % 3;
+                nx = x + dx[direction];
+                ny = y + dy[direction];
 
                 if(nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
             }
-            
             x = nx;
-            y = ny;           
+            y = ny;
         }
-        
-        int[] result = new int[v - 1];
-        int idx = 0;
+
+        int[] result = new int[num - 1];
+        int index = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i ; j++) {
-                result[idx++] = triangle[i][j];
+            for (int j = 0; j <= i; j++) {
+                result[index++] = triangle[i][j];
             }
         }
         return result;
     }
+
 }
