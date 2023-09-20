ad64459c4b629d75e04318d7ff9e7a0c5945b859
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-09-20 20:31:03 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,17 +1,19 @@
-class Solution {
-     private static final int MAX_WIDTH = 5000;
+public class Solution {
+
+    public static final int MAX_WIDTH = 5000;
 
     public int[] solution(int brown, int yellow) {
         for (int width = 3; width <= MAX_WIDTH; width++) {
             for (int height = 3; height <= width; height++) {
-                int boundary = (width + height - 2) * 2;
-                int center = (width * height) - boundary;
-                if(boundary == brown && center == yellow){
+                int boundary = 2 * (width + height - 2);
+                int center = width * height - boundary;
+
+                if (boundary == brown && center == yellow) {
                     return new int[]{width, height};
                 }
             }
         }
-
         return null;
     }
+
 }
