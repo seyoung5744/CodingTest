e3ead7e2545db81a471836f3bd8d4392cd4bdf7c
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-09-20 20:00:54 +0900

    [level 1] Title: 모의고사, Time: 4.93 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,34 +1,41 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.stream.IntStream;
 
-class Solution {
-    private static final int[][] STUDENTS = {
+public class Solution {
+
+    public static final int[][] RULES = {
         {1, 2, 3, 4, 5},
         {2, 1, 2, 3, 2, 4, 2, 5},
         {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
     };
 
-    private static int count;
+    private int getPicked(int person, int problem) {
+        int[] rule = RULES[person];
+        int index = problem % rule.length;
+        return rule[index];
+    }
 
     public int[] solution(int[] answers) {
         int[] corrects = new int[3];
         int max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < STUDENTS.length; i++) {
-            for (int j = 0; j < answers.length; j++) {
-                if (STUDENTS[i][j % STUDENTS[i].length] == answers[j]) {
-                    count += 1;
+        for (int problem = 0; problem < answers.length; problem++) {
+            int answer = answers[problem];
+
+            for (int person = 0; person < 3; person++) {
+                int picked = getPicked(person, problem);
+                if (answer == picked) {
+                    if (++corrects[person] > max) {
+                        max = corrects[person];
+                    }
                 }
             }
-            corrects[i] = count;
-            max = Math.max(max, count);
-            count = 0;
         }
 
-        final int maxCorrect = max;
+        final int finalMax = max;
         return IntStream.range(0, 3)
-            .filter(i -> corrects[i] == maxCorrect)
+            .filter(i -> corrects[i] == finalMax)
             .map(x -> x + 1)
             .toArray();
     }
+
 }
