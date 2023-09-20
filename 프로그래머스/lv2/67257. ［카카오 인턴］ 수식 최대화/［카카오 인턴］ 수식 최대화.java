e243b0b0db2ffee80f27e4a2dabd4a14f3bb0092
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-09-20 21:05:07 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.85 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,57 +1,38 @@
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Solution {
 
-    private static final String[][] precedences =
-        {
-            "+-*".split(""),
-            "+*-".split(""),
-            "-+*".split(""),
-            "-*+".split(""),
-            "*+-".split(""),
-            "*-+".split("")
-        };
+    public static String[][] precedences = {
+        "+-*".split(""),
+        "+*-".split(""),
+        "-+*".split(""),
+        "-*+".split(""),
+        "*+-".split(""),
+        "*-+".split("")
+    };
 
     public long solution(String expression) {
-        StringTokenizer st = new StringTokenizer(expression, "-+*", true);
-        List<String> tokens = new ArrayList<>();
-        
-        while  (st.hasMoreTokens()) {
-            tokens.add(st.nextToken());
-        }
-
         long max = 0;
-        for (String[] precedence : precedences) {
-            max = Math.max(max, Math.abs(calculate(new ArrayList<>(tokens), precedence)));
+        StringTokenizer tokenizer = new StringTokenizer(expression, "+-*", true);
+        List<String> tokens = new ArrayList<>();
+        while (tokenizer.hasMoreTokens()) {
+            tokens.add(tokenizer.nextToken());
         }
-
-        return max;
-    }
-
-    private static long calculate(List<String> tokens, String[] precedence) {
-        for (String op : precedence) {
-            for (int i = 0; i < tokens.size(); i++) {
-                if (tokens.get(i).equals(op)) {
-                    long num1 = Long.parseLong(tokens.get(i - 1));
-                    long num2 = Long.parseLong(tokens.get(i + 1));
-                    long result = calculate(num1, num2, op);
-                    tokens.remove(i - 1);
-                    tokens.remove(i - 1);
-                    tokens.remove(i - 1);
-
-                    tokens.add(i - 1, String.valueOf(result));
-                    i -= 1;
-                }
+        
+        for(String[] precedence : precedences) {
+            long value = Math.abs(calculate(new ArrayList<>(tokens), precedence));
+            if(value > max){
+                max = value;
             }
         }
-        
-        return Long.parseLong(tokens.get(0));
+        return max;
     }
 
     private static long calculate(long num1, long num2, String operation) {
         long result = 0;
+
         switch (operation) {
             case "+":
                 result = num1 + num2;
@@ -64,5 +45,26 @@ public class Solution {
                 break;
         }
         return result;
+    }
+
+    private static long calculate(List<String> tokens, String[] precedence) {
+
+        for (String op : precedence) {
+            for (int i = 0; i < tokens.size(); i++) {
+                if (tokens.get(i).equals(op)) {
+                    long num1 = Long.parseLong(tokens.get(i - 1));
+                    long num2 = Long.parseLong(tokens.get(i + 1));
+                    long result = calculate(num1, num2, op);
+
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.add(i - 1, String.valueOf(result));
+                    i -= 2;
+                }
+            }
+        }
+
+        return Long.parseLong(tokens.get(0));
     }
 }
