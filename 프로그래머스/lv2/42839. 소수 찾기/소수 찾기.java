5e408c6d01610f1fceaaa2a0b7c9144d9a5e5373
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-10-01 13:31:08 +0900

    [level 2] Title: 소수 찾기, Time: 2.51 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -1,35 +1,40 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
+
     public int solution(String numbers) {
         int[] numArr = numbers.chars().map(i -> i - '0').toArray();
         boolean[] isUsed = new boolean[numArr.length];
         Set<Integer> primes = new HashSet<>();
         
         getPrime(0, numArr, isUsed, primes);
+        
         return primes.size();
     }
+    
+    public static void getPrime(int num, int[] numArr, boolean[] isUsed, Set<Integer> primes) {
+        if(isPrime(num)) primes.add(num);
 
-    private static void getPrime(int acc, int[] numbers, boolean[] isUsed, Set<Integer> primes){
-        if(isPrime(acc)) primes.add(acc);
-
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 0; i < numArr.length; i++) {
             if(isUsed[i]) continue;
             
-            int nextAcc = acc * 10 + numbers[i];
+            int nextNum = num * 10 + numArr[i]; 
             isUsed[i] = true;
-            getPrime(nextAcc, numbers, isUsed, primes);
+            getPrime(nextNum, numArr, isUsed, primes);
             isUsed[i] = false;
         }
     }
-
-    private static boolean isPrime(int num){
+    
+    public static boolean isPrime(int num) {
         if(num <= 1) return false;
 
         for (int i = 2; i * i <= num; i++) {
-            if(num % i == 0) return false;
+            if(num % i == 0){
+                return false;
+            }
         }
+        
         return true;
     }
+
 }
