eefa3d892bf4a039f9ea08fe93f8df68a8b55b11
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-10-09 15:12:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.23 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class Solution {
+class Solution {
+    public static String solution(String[] participant, String[] completion) {
+        String answer = "";
 
-    public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
-
-        for (String name : participant) {
-            map.putIfAbsent(name, 0);
-            map.put(name, map.get(name) + 1);
+        for(String participate : participant) {
+            map.put(participate, map.getOrDefault(participate, 0) + 1);
         }
 
-        for (String name : completion) {
-            int value = map.get(name) - 1;
-            map.put(name, value);
-            if(value == 0) map.remove(name);
+        for(String complete : completion) {
+            if(map.containsKey(complete)) {
+                map.put(complete, map.get(complete) - 1);
+                if(map.get(complete) == 0){
+                    map.remove(complete);
+                }
+            }
         }
-        
-        return map.keySet().iterator().next();
+
+        return map.keySet().toArray(String[]::new)[0];
     }
-
 }
