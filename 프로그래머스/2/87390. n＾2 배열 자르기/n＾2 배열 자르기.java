c5e6e1e99e653730623ea2afb7f0d7edb374cb19
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-10-18 18:51:07 +0900

    [level 2] Title: n^2 배열 자르기, Time: 12.07 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,23 +1,27 @@
+import java.util.stream.*;
+
 class Solution {
     private static long[] getRowCol(int n, long left){
         return new long[]{left / n, left % n};
     }
     
     public int[] solution(int n, long left, long right) {
-        int[] answer = new int[(int)(right - left + 1)];
+        // int[] answer = new int[(int)(right - left + 1)];
         
-        for(long i = left; i <= right; i++) {
-            long[] rowcol = getRowCol(n, i);
-            long row = rowcol[0];
-            long col = rowcol[1];
-            int index = (int)(i - left);
-            if(col <= row) {
-                answer[index] = (int)row + 1;
-            }else {
-                answer[index] = (int)col + 1;
-            }
-        }
+        return LongStream.rangeClosed(left, right)
+            .mapToInt(value -> (int)Math.max(value / n, value % n) + 1).toArray();
+//         for(long i = left; i <= right; i++) {
+//             long[] rowcol = getRowCol(n, i);
+//             long row = rowcol[0];
+//             long col = rowcol[1];
+//             int index = (int)(i - left);
+//             if(col <= row) {
+//                 answer[index] = (int)row + 1;
+//             }else {
+//                 answer[index] = (int)col + 1;
+//             }
+//         }
         
-        return answer;
+//         return answer;
     }
 }
