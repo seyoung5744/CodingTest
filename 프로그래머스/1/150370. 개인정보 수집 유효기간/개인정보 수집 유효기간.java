06d8e214a5a40e88d19210f0e2fb34849acbb839
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-10-18 17:50:08 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 6.14 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,43 +2,40 @@ import java.util.*;
 
 public class Solution {
 
+    private static final int DAY_OF_MONTH = 28;
+    private static final int MONTH_OF_YEAR = 12;
+    
     public static int[] solution(String today, String[] terms, String[] privacies) {
 
         List<Integer> answer = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
-        for(String term : terms) {
+
+        for (String term : terms) {
             String[] termArr = term.split(" ");
-            map.put(termArr[0], Integer.parseInt(termArr[1]) * 28);
+            map.put(termArr[0], Integer.parseInt(termArr[1]) * DAY_OF_MONTH);
         }
 
-        int[] dates = Arrays.stream(today.split("\\.")).mapToInt(Integer::parseInt).toArray();
-        System.out.println(Arrays.toString(dates));
-        int todays = extracted(dates);
-        int index = 1;
+        int todayDays = calculateDay(Arrays.stream(today.split("\\.")).mapToInt(Integer::parseInt).toArray());
 
-        for(String privacy : privacies) {
-            String[] privacyArr = privacy.split(" ");
-            dates = Arrays.stream(privacyArr[0].split("\\.")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < privacies.length; i++) {
+
+            String[] privacyArr = privacies[i].split(" ");
+
+            int privacyDays = calculateDay(Arrays.stream(privacyArr[0].split("\\.")).mapToInt(Integer::parseInt).toArray());
+
             String key = privacyArr[1];
+            privacyDays += map.get(key) - 1;
 
-            int extracted = extracted(dates);
-
-            extracted += map.get(key) - 1;
-
-            if (todays > extracted)
-            {
-                answer.add(index);
+            if (todayDays > privacyDays) {
+                answer.add(i + 1);
             }
-
-            index++;
         }
 
-        Collections.sort(answer);
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    private static int extracted(int[] dates) {
-        int year = 12 * 28;
-        return  (dates[0] * year) + (dates[1] * 28) + dates[2];
+    private static int calculateDay(int[] dates) {
+        return (dates[0] * MONTH_OF_YEAR * DAY_OF_MONTH) + (dates[1] * DAY_OF_MONTH) + dates[2];
     }
+
 }
