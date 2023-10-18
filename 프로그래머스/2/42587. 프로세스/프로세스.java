d2c601c7d89f045a24f6ebf4ebb302723331d2c1
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-10-18 19:10:32 +0900

    [level 2] Title: 프로세스, Time: 1.02 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    
+    static class Process {
+        int location;
+        int priority;
+        
+        public Process(int location, int priority) {
+            this.location = location;
+            this.priority = priority;
+        }
+    }
+    
+
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        
+        Queue<Process> processes = new ArrayDeque<>();
+        List<Integer> all = new ArrayList<>();
+        
+        for(int i = 0; i < priorities.length; i++) {
+            processes.add(new Process(i, priorities[i]));
+            all.add(priorities[i]);
+        }
+        
+        Collections.sort(all, Collections.reverseOrder());
+        int index = 0;
+        int count = 1;
+        while(!processes.isEmpty()) {
+            Process cur = processes.poll();
+            
+            if(cur.priority == all.get(index))
+            {
+                if (cur.location == location)
+                    break;
+                ++count;
+                ++index;
+            }
+            else
+            {
+                processes.add(cur);
+            }
+            
+        }
+        
+        answer = count;
+        return answer;
+    }
+}
