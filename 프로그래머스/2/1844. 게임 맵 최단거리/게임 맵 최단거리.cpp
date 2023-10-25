634865c10f7f091c1a6564ea8df9c924adab6040
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-10-25 17:07:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.46 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,8 +1,9 @@
-#include<vector>
-#include<iostream>
-#include<queue>
+#include <vector>
+#include <queue>
 
 using namespace std;
+
+#define INF 100001
 
 struct Step
 {
@@ -13,44 +14,49 @@ struct Step
     Step(int x, int y, int mc) : x(x), y(y), moveCount(mc){}
 };
 
-int check[101][101];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
+
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
 
 int solution(vector<vector<int> > maps)
 {
-    int n, m;
-    n = maps.size();
-    m = maps[0].size();
+    int answer = -1;
+    int endX = maps[0].size()-1;
+    int endY = maps.size()-1;
+    int maxX = maps[0].size();
+    int maxY = maps.size();
+    
+    //vector<vector<int>> visited (maps.size(), vector<int> (maps[1].size(), INF));
+    
     queue<Step*> q;
     q.push(new Step(0, 0, 1));
-    check[0][0] = true;
-
-    while(!q.empty()){
-        int x = q.front()->x;
-        int y = q.front()->y;
+    maps[0][0] = 0;
+    
+    while(!q.empty())
+    {
+        Step* cur = q.front();
         q.pop();
-
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(0<=nx && nx<n && 0<=ny && ny<m){
-                if(check[nx][ny] == false && maps[nx][ny] > 0){
-                    check[nx][ny] = true;
-                    maps[nx][ny] = maps[x][y] + 1;
-                    q.push(new Step(nx, ny, 1));
+        
+        for(int i = 0; i < 4; ++i)
+        {
+            int newX = cur->x + dx[i];
+            int newY = cur->y + dy[i];
+            
+            if (newX >= 0 && newX < maxX && newY >= 0 && newY < maxY)
+            {
+                if (maps[newY][newX] != 0)
+                {   
+                    if (newY == endY && newX == endX)
+                        return cur->moveCount + 1;
+                    maps[newY][newX] = 0;
+                    
+                    q.push(new Step(newX, newY, cur->moveCount + 1));
+                    
                 }
             }
         }
     }
-
-
-
-    int answer = 0;
-    if(maps[n-1][m-1] == 1){
-        answer = -1;
-    }else{
-        answer = maps[n-1][m-1];
-    }
+    
+    
     return answer;
 }
