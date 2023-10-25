fd448854a80fac8fecebdf3792ea7839ddea2a01
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-10-25 13:23:29 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,38 +1,28 @@
 import java.util.*;
 
-public class Solution {
-    public static int solution(String[] want, int[] number, String[] discount) {
-        int answer = 0;
-        Map<String, Integer> wantMap = new HashMap<>();
-        for(int i = 0; i < want.length; i++) {
-            wantMap.put(want[i], number[i]);
-        }
+class Solution {
+        public int solution(String[] want, int[] number, String[] discount) {
+            int answer = 0;
+            int left = 0;
+            int right = 9;
+            Map<String, Integer> map = new HashMap<>();
+            for(int i =0;i<10;i++) map.put(discount[i], map.getOrDefault(discount[i],0)+1);
+            while(right < discount.length){
+                if(check(map,want,number)) answer++;
 
-        int start = 0;
-        int end = 0;
-
-        Map<String, Integer> discountMap = new HashMap<>();
-
-        while(start <= discount.length - 10) {
-
-            if (end - start < 10) {
-                discountMap.put(discount[end], discountMap.getOrDefault(discount[end], 0) + 1);
-                ++end;
-            } else {
-                if(wantMap.equals(discountMap)) {
-                    System.out.println(start);
-                    answer++;
-                }
-
-                discountMap.put(discount[start], discountMap.get(discount[start]) - 1);
-                if(discountMap.get(discount[start]) == 0) {
-                    discountMap.remove(discount[start]);
-                }
-                ++start;
+                right++;
+                if(right < discount.length)
+                    map.put(discount[right], map.getOrDefault(discount[right],0)+1);
+                map.put(discount[left],map.get(discount[left])-1);
+                left++;
             }
-            
-        }
-        return answer;
-    }
 
-}
+            return answer;
+        }
+        private boolean check(Map<String,Integer> map,String[] want,int[] number){
+            for(int i =0;i<want.length;i++){
+                if(map.getOrDefault(want[i],0) != number[i]) return false;
+            }
+            return true;
+        }
+    }
