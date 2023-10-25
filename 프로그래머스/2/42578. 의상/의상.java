236b939ab27fd1029d8237d58f3a05cc7ebe9a1a
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-10-25 13:32:15 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        }
+        
+        for(int value : map.values()) {
+            answer *= (value + 1);
+        }
+        return answer - 1;
+    }
+}
