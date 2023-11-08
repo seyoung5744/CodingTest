163941dbe42df66a9abf08c48adcf0fdc2b74061
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-11-08 14:10:44 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 43.87 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public static int solution(int[] queue1, int[] queue2) {
+        int answer = 0;
+        Deque<Long> q1 = new ArrayDeque<>();
+        Deque<Long> q2 = new ArrayDeque<>();
+
+        for(int i = 0; i < queue1.length; i++) {
+            q1.add((long)queue1[i]);
+            q2.add((long)queue2[i]);
+        }
+
+        long sum1 = sum(q1);
+        long sum2 = sum(q2);
+
+        while (sum1 != sum2) {
+            if (sum1 > sum2) {
+                long remove = q1.poll();
+                sum1 -= remove;
+                sum2 += remove;
+                q2.add(remove);
+                answer++;
+            } else {
+                long remove = q2.poll();
+                sum2 -= remove;
+                sum1 += remove;
+                q1.add(remove);
+                answer++;
+            }
+            
+            if(q1.isEmpty() || q2.isEmpty() || answer >= queue1.length * 3){
+                answer = -1;
+                break;
+            }
+        
+        }
+
+        return answer;
+    }
+
+    public static long sum(Deque<Long> arr) {
+        long result = 0;
+        for(long num : arr){
+            result += num;
+        }
+        return result;
+    }
+}
