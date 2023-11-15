7349a47d2590f76bce52b8e240d3cc3de0dfa856
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-11-15 14:55:49 +0900

    [level 3] Title: 징검다리 건너기, Time: 24.61 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/3/64062. 징검다리 건너기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 2.74 ms
+메모리: 61.8 MB, 시간: 24.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 14:35:45
+2023년 11월 3일 14:55:47
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        
+        int start = 1;
+        int end = 200_000_000;
+        
+        while(start <= end) {
+            int mid = (start + end) / 2;
+                
+            int length = 1;
+            int max = Integer.MIN_VALUE;
+            for(int i = 0; i < stones.length; i++) {
+                if(stones[i] - mid < 0) {
+                    length++;
+                } else {
+                    length = 1;
+                }
+                
+                max = Math.max(max, length);
+            }
+            
+            if(max > k) {
+                end = mid - 1;  
+            } else {
+                start = mid + 1;
+                answer = mid;
+            }
+        }
+        
+        return answer;
+    }
+}
