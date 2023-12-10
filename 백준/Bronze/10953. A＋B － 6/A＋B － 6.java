d0126f453e07af96af39c41ea8f1d724bd2469b1
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-12-10 21:45:44 +0900

    [Bronze III] Title: A+B - 6, Time: 128 ms, Memory: 12992 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int i = 0; i < T; i++) {
+            String[] s = scanner.next().split(",");
+            int A = Integer.parseInt(s[0]);
+            int B = Integer.parseInt(s[1]);
+            System.out.println(A + B);
+        }
+    }
+}
