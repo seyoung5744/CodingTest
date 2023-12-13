0a7236d9ec2c823994647c27e0c8b05a8c8da569
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-12-13 17:57:34 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,51 +1,30 @@
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Solution {
 
-    public static int[] parents;
+    public static boolean[] visited;
 
     public static int solution(int n, int[][] computers) {
         int answer = 0;
-        parents = new int[n];
+
+        visited = new boolean[n];
 
         for (int i = 0; i < n; i++) {
-            parents[i] = i;
+            if(visited[i]) continue;
+
+            dfs(computers, i);
+            answer++;
         }
-        for (int i = 0; i < computers.length; i++) {
-            for (int j = 0; j < computers[i].length; j++) {
-                if(computers[i][j] == 1) {
-                    if (find(i) != find(j))
-                        union(i, j);
-                }
-            }
-        }
-        
-        for(int i = 0; i < computers.length; ++i) {
-            find(i);
-        }
-        
-        Set<Integer> collect = Arrays.stream(parents).boxed().collect(Collectors.toSet());
-        return collect.size();
+
+        return answer;
     }
 
-    public static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
+    public static void dfs(int[][] computers, int cur) {
+        visited[cur] = true;
 
-        if (a < b) {
-            parents[b] = a;
-        } else {
-            parents[a] = b;
+        for (int i = 0; i < visited.length; i++) {
+            if(i == cur || computers[cur][i] == 0 || visited[i]) continue;
+
+            dfs(computers, i);
         }
-    }
-
-    public static int find(int n) {
-        if (n != parents[n]) {
-            parents[n] = find(parents[n]);
-        }
-
-        return parents[n];
     }
 }
