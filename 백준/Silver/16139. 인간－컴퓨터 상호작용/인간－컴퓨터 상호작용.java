878c0e8c48016c7e6a1b2809033a078de2096438
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2023-12-18 23:38:14 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 856 ms, Memory: 131308 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,7 +1,7 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,25 +9,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
+        String str = br.readLine(); // 200_000
+
+        int[][] mem = new int[str.length()][26];
+
+        mem[0][str.charAt(0) - 'a'] += 1;
+
+        for (int i = 1; i < str.length(); i++) {
+            for (int j = 0; j < 26; j++) {
+                mem[i][j] = mem[i - 1][j];
+            }
+            mem[i][str.charAt(i) - 'a'] += 1;
+        }
 
         int T = Integer.parseInt(br.readLine());
 
-        while (T-- > 0) {
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) { // 200_000
             String[] tokens = br.readLine().split(" ");
-            char target = tokens[0].charAt(0);
+            int target = tokens[0].charAt(0) - 'a';
             int start = Integer.parseInt(tokens[1]);
-            int end = Integer.parseInt(tokens[2]) + 1;
+            int end = Integer.parseInt(tokens[2]);
 
-            String subStr = str.substring(start, end);
-//            System.out.println(subStr);
-
-            int count =  0;
-            for(char c : subStr.toCharArray()) {
-                if(c == target) count++;
+            if (start == 0) {
+                sb.append(mem[end][target]).append("\n");
+            } else {
+                sb.append(mem[end][target] - mem[start - 1][target]).append("\n");
             }
-            System.out.println(count);
         }
+
+        System.out.println(sb);
     }
 
 }
