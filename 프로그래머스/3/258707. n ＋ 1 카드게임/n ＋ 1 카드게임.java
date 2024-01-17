0a26ade1eac2a9edc371812143dfe09372b750ad
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-01-17 14:27:31 +0900

    [level 3] Title: n + 1 카드게임, Time: 1.39 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,65 +1,49 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Solution {
 
-    public static List<Integer> hand = new ArrayList<>(); // 갖고 있는 숫자들
-    public static boolean[] isInHand;
-    public static boolean[] isRequestCoin;
+    public static Map<Integer, Integer> hands = new LinkedHashMap<>();
     public static int target;
 
     public static int solution(int coin, int[] cards) {
         int round = 1;
         target = cards.length + 1;
-        isInHand = new boolean[target];
-        isRequestCoin = new boolean[target];
 
         for (int i = 0; i < cards.length / 3; i++) {
-            hand.add(cards[i]);
-            isInHand[cards[i]] = true;
+            hands.put(cards[i], 0);
         }
 
         for (int i = cards.length / 3; i < cards.length; i += 2, ++round) {
-            System.out.println(cards[i] + "," + cards[i + 1]);
+            hands.put(cards[i], 1);
+            hands.put(cards[i + 1], 1);
 
-            hand.add(cards[i]);
-            hand.add(cards[i + 1]);
-            isInHand[cards[i]] = true;
-            isInHand[cards[i + 1]] = true;
-            isRequestCoin[cards[i]] = true;
-            isRequestCoin[cards[i + 1]] = true;
-
-            boolean flag = false;
-
-            for (int card : hand) {
-                if (isInHand[target - card]) {
-                    int needCoins = 0;
-
-                    if (isRequestCoin[target - card]) {
-                        needCoins++;
-                    }
-                    if (isRequestCoin[card]) {
-                        needCoins++;
-                    }
+            int num = -1;
+            int needCoins = 0;
+                
+            for (int card : hands.keySet()) {
+                if (hands.containsKey(target - card)) {
+                    needCoins = hands.get(card) + hands.get(target - card);
 
                     if (needCoins <= coin) {
-                        isInHand[card] = false;
-                        isInHand[target - card] = false;
-                        hand.remove(Integer.valueOf(card));
-                        hand.remove(Integer.valueOf(target - card));
-                        coin -= needCoins;
-                        flag = true;
+                        num = card;
                         break;
                     }
                 }
             }
-
-            if (!flag) {
-                break;
+            
+            if (num != -1)
+            {
+                hands.remove(num);
+                hands.remove(target - num);
+                coin -= needCoins;
             }
+            else break;
 
         }
 
         return round;
     }
+
 }
