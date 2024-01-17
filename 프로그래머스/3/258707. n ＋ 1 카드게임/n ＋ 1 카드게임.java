4dec911f218699928ee7db73abbc775aa35b5fe2
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-01-17 14:30:14 +0900

    [level 3] Title: n + 1 카드게임, Time: 1.45 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,49 +1,62 @@
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-public class Solution {
+import java.util.PriorityQueue;
 
-    public static Map<Integer, Integer> hands = new LinkedHashMap<>();
-    public static int target;
+class Solution {
 
-    public static int solution(int coin, int[] cards) {
-        int round = 1;
-        target = cards.length + 1;
+    int n;
+    int[] indexInfo;
 
-        for (int i = 0; i < cards.length / 3; i++) {
-            hands.put(cards[i], 0);
+    public int solution(int coin, int[] cards) {
+
+        n = cards.length;
+        indexInfo = new int[n + 1];
+        indexInfo[0] = -1;
+
+        for (int idx = 0; idx < n; idx++) {
+            int value = cards[idx];
+            indexInfo[value] = idx;
         }
 
-        for (int i = cards.length / 3; i < cards.length; i += 2, ++round) {
-            hands.put(cards[i], 1);
-            hands.put(cards[i + 1], 1);
+        PriorityQueue<int[]> stack = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> hand = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
-            int num = -1;
-            int needCoins = 0;
-                
-            for (int card : hands.keySet()) {
-                if (hands.containsKey(target - card)) {
-                    needCoins = hands.get(card) + hands.get(target - card);
+        for (int i = 1; i <= n / 2; i++)
+            stack.add(calcPairInfo(i));
 
-                    if (needCoins <= coin) {
-                        num = card;
-                        break;
-                    }
-                }
+        int round = 1, index = n / 3;
+
+        while (coin >= 0 && !stack.isEmpty()) {
+
+            index += 2;
+
+            while (!stack.isEmpty() && stack.peek()[0] < index)
+                hand.add(stack.poll());
+
+            if (!hand.isEmpty() && hand.peek()[1] <= coin) {
+                coin -= hand.poll()[1];
+            } else {
+                return round;
             }
-            
-            if (num != -1)
-            {
-                hands.remove(num);
-                hands.remove(target - num);
-                coin -= needCoins;
-            }
-            else break;
+            round++;
 
         }
 
         return round;
+    }
+
+    private int[] calcPairInfo(int value) {
+
+        int[] ans = new int[2];
+        int value2 = n + 1 - value;
+
+        ans[0] = Math.max(indexInfo[value], indexInfo[value2]);
+
+        if (indexInfo[value] >= n / 3)
+            ans[1]++;
+        if (indexInfo[value2] >= n / 3)
+            ans[1]++;
+
+        return ans;
     }
 
 }
