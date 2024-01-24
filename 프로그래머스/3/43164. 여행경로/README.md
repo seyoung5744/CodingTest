cc80867e4bade1e7edb099846c6dfb6af579e8f9
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-01-24 15:22:53 +0900

    [level 3] Title: 여행경로, Time: 17.98 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=java) 
 
 ### 성능 요약
 
-메모리: 4.22 MB, 시간: 0.03 ms
+메모리: 77.7 MB, 시간: 17.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 14:45:30
+2024년 1월 3일 15:22:50
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+class Solution {
+    public static List<String> result = new ArrayList<>();
+    public static boolean[] visited;
+
+    public static String[] solution(String[][] tickets) {
+        visited = new boolean[tickets.length];
+
+        dfs("ICN", 0, "ICN", tickets);
+
+        Collections.sort(result);
+        return result.get(0).split("_");
+    }
+
+    public static void dfs(String start, int ticketCnt, String word, String[][] tickets) {
+
+        if(ticketCnt == tickets.length) {
+            result.add(word);
+            return;
+        }
+
+        for (int i = 0; i < tickets.length; i++) {
+            if(!visited[i] && tickets[i][0].equals(start)) {
+                visited[i] = true;
+                dfs(tickets[i][1], ticketCnt + 1, word + "_" + tickets[i][1], tickets);
+                visited[i] = false;
+
+            }
+        }
+    }
+
+}
