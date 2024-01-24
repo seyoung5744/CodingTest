d93d367dacae375a59a01727cc696143557d1304
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-01-24 13:41:13 +0900

    [level 3] Title: 가장 먼 노드, Time: 274.82 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Solution {
+
+    public static List<List<Integer>> graph = new ArrayList<>();
+    public static int[] distance;
+    public final static int INF = (int) 2e9;
+
+    public static int solution(int n, int[][] edge) {
+        int answer = 0;
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] point : edge) {
+            int from = point[0];
+            int to = point[1];
+
+            graph.get(from).add(to);
+            graph.get(to).add(from);
+        }
+
+        distance = new int[n + 1];
+        Arrays.fill(distance, INF);
+
+        distance[0] = 0;
+        distance[1] = 1;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+
+        while (!q.isEmpty()) {
+
+            int cur = q.poll();
+
+            for (int i = 0; i < graph.get(cur).size(); i++) {
+                int next = graph.get(cur).get(i);
+
+                if (distance[next] <= distance[cur] + 1) {
+                    continue;
+                }
+
+                distance[next] = distance[cur] + 1;
+                q.add(next);
+            }
+        }
+
+        return (int) Arrays.stream(distance)
+            .filter(i -> i == Arrays.stream(distance).max().getAsInt())
+            .count();
+    }
+
+
+}
