abe4deba2e21f0f0132fd9ca0c33cb0486f8c4c2
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-02-09 21:12:22 +0900

    [Silver III] Title: 이친수, Time: 76 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+    public static long d[];
+    public static void main(String args[]) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int count = Integer.parseInt(br.readLine());
+            d = new long[count+1];
+            System.out.println(Calculate(count));
+    }
+
+    public static long Calculate(int count){
+        if(count == 0){
+            return 0;
+        }
+        if(count == 1){
+            return 1;
+        }
+        if(d[count] > 0){
+            return d[count];
+        }
+        d[count] = Calculate(count-1) + Calculate(count-2);
+        return d[count];
+    }
+}
