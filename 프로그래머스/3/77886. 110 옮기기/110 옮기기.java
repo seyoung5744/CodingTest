8e8fe01b2aa7facec0ef19236ebb0fae808ca0cc
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-03-13 13:57:44 +0900

    [level 3] Title: 110 옮기기, Time: 50.31 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
@@ -3,22 +3,22 @@ import java.util.Arrays;
 public class Solution {
 
     public static final String TOKEN = "110";
+    public static final int MIN_LENGTH = 3;
 
     public static String[] solution(String[] s) {
         String[] answer = new String[s.length];
 
         for (int i = 0; i < s.length; i++) {
-            StringBuilder str = new StringBuilder(s[i]);
+            String str = s[i];
             StringBuilder temp = new StringBuilder();
 
             int count = 0;
 
-            for(int j = 0; j < str.length(); ++j)
-            {
+            for (int j = 0; j < str.length(); ++j) {
                 temp.append(str.charAt(j));
-                if(temp.length() >= 3) {
+                if (temp.length() >= MIN_LENGTH) {
                     int length = temp.length();
-                    if(temp.charAt(length - 1) == '0' && temp.charAt(length - 2) == '1' && temp.charAt(length -3) == '1') {
+                    if (temp.charAt(length - 1) == '0' && temp.charAt(length - 2) == '1' && temp.charAt(length - 3) == '1') {
                         temp = temp.delete(length - 3, length);
                         ++count;
                     }
@@ -27,29 +27,11 @@ public class Solution {
 
             int cur = temp.length() - 1;
 
-            while(cur >= 0 && temp.charAt(cur) == '1')
-            {
+            while (cur >= 0 && temp.charAt(cur) == '1') {
                 --cur;
             }
-
-            ++cur;
-
-            answer[i] = temp.insert(cur, TOKEN.repeat(count)).toString();;
+            answer[i] = temp.insert(++cur, TOKEN.repeat(count)).toString();
         }
         return answer;
     }
-
-    public static int findToken(String s, int startIdx)
-    {
-
-        for(int i = startIdx; i < s.length() - 2; ++i)
-        {
-            if (s.charAt(i) == '1' && s.charAt(i+1) == '1' && s.charAt(i+2) == '0')
-                return i;
-        }
-
-        return -1;
-    }
-
-
 }
--- 프로그래머스/3/77886. 110 옮기기/README.md
+++ 프로그래머스/3/77886. 110 옮기기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 110 옮기기 - 77886 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77886#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77886#) 
 
 ### 성능 요약
 
-메모리: 92.2 MB, 시간: 51.56 ms
+메모리: 100 MB, 시간: 50.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 13일 13:48:14
+2024년 03월 13일 13:57:43
 
 ### 문제 설명
 
