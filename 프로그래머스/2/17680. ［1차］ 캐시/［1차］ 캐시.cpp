9bfa4c2fa19276fd36e94551d9e8ecc492e76396
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-05 17:47:12 +0900

    [level 2] Title: [1차] 캐시, Time: 0.68 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <queue>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities) {
+    int answer = 0;
+    map<string, int> cache;
+    queue<string> q;
+
+    for (int i = 0; i < cities.size(); ++i)
+    {
+        string c = cities[i];
+        transform(c.begin(), c.end(), c.begin(), [](auto c){return std::tolower(c);});
+        q.push(c);
+
+        if (cache[c] == 0)
+        {
+            answer += 5;
+        }
+        else
+        {
+            answer += 1;
+        }
+
+        ++cache[c];
+
+        while (cache.size() > cacheSize)
+        {
+            string r = q.front();
+            q.pop();
+
+            --cache[r];
+
+            if (cache[r] == 0)
+            {
+                cache.erase(r);
+            }
+        }
+    }
+    return answer;
+}
