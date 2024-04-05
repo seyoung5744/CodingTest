4eb8888b3ea9a2629806dc78717772ea5c75aa49
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-05 13:39:10 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        Set<String> appearedWords = new HashSet<>();
+        appearedWords.add(words[0]);
+
+        for (int i = 1; i < words.length; i++) {
+            if (appearedWords.contains(words[i])) {
+                return new int[]{i % n + 1, i / n + 1};
+            }
+
+            if (words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)) {
+                return new int[]{i % n + 1, i / n + 1};
+            }
+            appearedWords.add(words[i]);
+        }
+
+        return new int[]{0, 0};
+    }
+}
