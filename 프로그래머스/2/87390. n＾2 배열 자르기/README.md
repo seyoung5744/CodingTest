cb068bbbd16748e9d9869948e660036ed5fe317f
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-05 16:11:37 +0900

    [level 2] Title: n^2 배열 자르기, Time: 4.07 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/README.md
+++ 프로그래머스/2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390) 
 
 ### 성능 요약
 
-메모리: 109 MB, 시간: 12.07 ms
+메모리: 110 MB, 시간: 4.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 18:51:6
+2024년 04월 05일 16:11:35
 
 ### 문제 설명
 
--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,27 +1,19 @@
-import java.util.stream.*;
-
 class Solution {
-    private static long[] getRowCol(int n, long left){
-        return new long[]{left / n, left % n};
-    }
-    
     public int[] solution(int n, long left, long right) {
-        // int[] answer = new int[(int)(right - left + 1)];
-        
-        return LongStream.rangeClosed(left, right)
-            .mapToInt(value -> (int)Math.max(value / n, value % n) + 1).toArray();
-//         for(long i = left; i <= right; i++) {
-//             long[] rowcol = getRowCol(n, i);
-//             long row = rowcol[0];
-//             long col = rowcol[1];
-//             int index = (int)(i - left);
-//             if(col <= row) {
-//                 answer[index] = (int)row + 1;
-//             }else {
-//                 answer[index] = (int)col + 1;
-//             }
-//         }
-        
-//         return answer;
+       int[] answer = new int[(int) (right - left + 1)];
+
+        int idx = 0;
+        for (long i = left; i <= right; i++) {
+            long row = i / n;
+            long col = i % n;
+
+            if (col <= row) {
+                answer[idx++] = (int) row + 1;
+            } else {
+                answer[idx++] = (int) col + 1;
+            }
+        }
+
+        return answer;
     }
 }
