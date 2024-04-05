f56eb352c4ec441ca29dae027cb0cfb7a4690dc6
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-05 15:29:15 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.39 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=java) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 0.81 ms
+메모리: 76.9 MB, 시간: 1.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 1일 19:34:7
+2024년 04월 05일 15:29:14
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,56 +1,44 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
 
 public class Solution {
 
-    public int solution(String s) {
+    public static Map<Character, Character> parenthesis = Map.of('(', ')', '{', '}', '[', ']');
+
+    public static int solution(String s) {
         int answer = 0;
-        Deque<Character> queue = new ArrayDeque<>();
-
-        for (char c : s.toCharArray()) {
-            queue.add(c);
-        }
-
-        for (int i = 0; i < queue.size(); i++) {
-            if(rotation(queue)) {
-                answer += 1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            String temp = s.substring(i, s.length()) + s.substring(0, i);
+            if(isValid(temp)) {
+                answer++;
             }
-            queue.add(queue.poll());
         }
         return answer;
     }
 
-    public static boolean rotation(Deque<Character> queue) {
-        Deque<Character> copyQueue = new ArrayDeque<>(queue);
+    public static boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
 
-        boolean isCheck = true;
+        for (char ch : s.toCharArray()) {
 
-        Map<Character, Character> map = new HashMap<>();
-        map.put('(', ')');
-        map.put('[', ']');
-        map.put('{', '}');
-
-        while (!copyQueue.isEmpty()) {
-            char cur = copyQueue.poll();
-
-            if (map.containsKey(cur)) { // 괄호 열기라면
-                stack.push(cur);
-            } else if (map.containsValue(cur)) { // 괄호 닫기라면
-                if (stack.isEmpty()) { // 비었으므로 false;
-                    isCheck = false;
-                    break;
+            if (parenthesis.containsKey(ch)) {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
                 } else {
-                    char top = stack.pop();
-
-                    if(cur != map.get(top)) {
-                        isCheck = false;
-                        break;
+                    if (parenthesis.get(stack.peek()) != ch) {
+                        return false;
+                    } else {
+                        stack.pop();
                     }
                 }
             }
-
         }
 
-        return stack.isEmpty() && isCheck;
+        return stack.isEmpty();
     }
+
 }
