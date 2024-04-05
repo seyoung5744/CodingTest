3c3bf9ef410fdd624a53d0e7fe8772d55e197f04
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-05 18:02:37 +0900

    [level 2] Title: 기능개발, Time: 2.10 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,29 +1,34 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
-    public static int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> result = new ArrayList<>();
 
-        Deque<Integer> progress = new ArrayDeque<>();
-        for (int i = 0; i < progresses.length; i++) {
-               progress.add((int) Math.ceil((double) (100 - progresses[i]) / speeds[i]));
+    public int[] solution(int[] progresses, int[] speeds) {
+
+        int[] completeDays = new int[speeds.length];
+
+        for (int i = 0; i < speeds.length; i++) {
+            completeDays[i] = (100 - progresses[i]) / speeds[i];
+            if ((100 - progresses[i]) % speeds[i] != 0)
+                ++completeDays[i];
         }
-        
-        int max = progress.peek();
-        int num = 0;
-        while(!progress.isEmpty()) {
-            int cur = progress.poll();
 
-            if(max >= cur) {
-                num++;
+        List<Integer> answer = new ArrayList<>();
+
+        int max = completeDays[0];
+        int count = 1;
+
+        for (int i = 1; i < completeDays.length; i++) {
+            if (completeDays[i] > max) {
+                answer.add(count);
+                max = completeDays[i];
+                count = 1;
             } else {
-                result.add(num);
-                num = 1;
+                count++;
             }
-            max = Math.max(max, cur);
         }
-        result.add(num);
-        return result.stream().mapToInt(Integer::intValue).toArray();
-    }
 
+        answer.add(count);
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
