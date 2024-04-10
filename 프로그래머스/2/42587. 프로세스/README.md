b6c8fc35d99ad39587db125a295c8b2ea0fcc7ed
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-10 14:08:05 +0900

    [level 2] Title: 프로세스, Time: 1.00 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
 
 ### 성능 요약
 
-메모리: 75.9 MB, 시간: 0.91 ms
+메모리: 80.8 MB, 시간: 1.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 19:13:43
+2024년 04월 10일 14:08:02
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,47 +1,48 @@
-import java.util.*;
-import java.util.stream.*;
 
-class Solution {
-    
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Solution {
+
     static class Process {
-        int location;
-        int priority;
-        
-        public Process(int location, int priority) {
-            this.location = location;
-            this.priority = priority;
-        }
-    }
-    
 
-    public int solution(int[] priorities, int location) {        
-        Queue<Process> processes = new ArrayDeque<>();
-        List<Integer> all = new ArrayList<>();
-        
-        for(int i = 0; i < priorities.length; i++) {
-            processes.add(new Process(i, priorities[i]));
-            all.add(priorities[i]);
+        int priority;
+        int index;
+
+        public Process(int priority, int index) {
+            this.priority = priority;
+            this.index = index;
         }
-        
-        Collections.sort(all, Collections.reverseOrder());
-        int index = 0;
-        int count = 1;
-        while(!processes.isEmpty()) {
-            Process cur = processes.poll();
-            
-            if(cur.priority == all.get(index)) {
-                if (cur.location == location)
-                    break;
-                count++;
-                index++;
-                // ++count;
-                // ++index;
-            } else {
-                processes.add(cur);
-            }
-            
-        }
-        
-        return count;
     }
+
+    public static int solution(int[] priorities, int location) {
+        int answer = 1;
+        Queue<Process> q = new LinkedList<>();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int i = 0; i < priorities.length; i++) {
+            q.offer(new Process(priorities[i], i));
+            pq.offer(priorities[i]);
+        }
+
+        while (!q.isEmpty()) {
+            Process cur = q.poll();
+
+            if (cur.priority == pq.peek()) {
+                pq.poll();
+
+                if (cur.index == location)
+                    break;
+
+                ++answer;
+            } else {
+                q.offer(cur);
+            }
+
+        }
+        return answer;
+    }
+
 }
