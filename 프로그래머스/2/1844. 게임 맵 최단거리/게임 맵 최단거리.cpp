5926cd816d77af89c38048bcadb3bedd12342d07
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-10 14:40:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.23 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,62 +1,46 @@
 #include<vector>
-#include<queue>
-
-const int MAX = 100000;
-
+#include <queue>
 using namespace std;
+
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
 
 int solution(vector<vector<int> > maps)
 {
-    int row = maps.size();
-    int col = maps[0].size();
-    bool Done = false;
-
-    vector<vector<int>> dist(row);
-    for (int i = 0; i < row; i++) 
-        for(int j = 0; j < col; j++)
-            dist[i].push_back(MAX);
-
+    int answer = 0;
+    int endY = maps.size() - 1;
+    int endX = maps[0].size() - 1;
+    
+    vector<vector<int>> visited (maps.size(), vector<int> (maps[0].size(), 1000001));
     queue<pair<int, int>> q;
-    dist[0][0] = 1;
-    q.push(make_pair(0, 0));
-
-    int count = 0;
-    while (!q.empty() && count < row * col) {
-        int r = q.front().first;
-        int c = q.front().second;
+    q.push({0, 0});
+    visited[0][0] = 1;
+    
+    while(!q.empty())
+    {
+        pair<int, int> cur = q.front();
         q.pop();
-
-        if (r == row - 1 && c == col - 1) {
-            Done = true;
+        
+        if (cur.first == endX && cur.second == endY)
+            break;
+        
+        for(int i = 0; i < 4; ++i)
+        {
+            int nx = cur.first + dx[i];
+            int ny = cur.second + dy[i];
+            
+            if (nx < 0 || nx > endX || ny < 0 || ny > endY || maps[ny][nx] == 0)
+                continue;
+            
+            if (visited[ny][nx] <= visited[cur.second][cur.first] + 1)
+                continue;
+            
+            visited[ny][nx] = visited[cur.second][cur.first] + 1;
+            q.push({nx, ny});
         }
-
-        if (r < row-1 && maps[r+1][c] == 1) 
-            if (dist[r+1][c] > dist[r][c] + 1) {
-                dist[r+1][c] = dist[r][c] + 1;
-                q.push(make_pair(r+1, c));
-            }
-
-        if (c < col-1 && maps[r][c+1] == 1)
-            if (dist[r][c+1] > dist[r][c] + 1) {
-                dist[r][c+1] = dist[r][c] + 1;
-                q.push(make_pair(r, c+1));
-            }
-
-        if (r > 0 && maps[r-1][c] == 1)
-            if (dist[r-1][c] > dist[r][c] + 1) {
-                dist[r-1][c] = dist[r][c] + 1;
-                q.push(make_pair(r-1, c));
-            }
-
-        if (c > 0 && maps[r][c-1] == 1) 
-            if (dist[r][c-1] > dist[r][c] + 1) {
-                dist[r][c-1] = dist[r][c] + 1;
-                q.push(make_pair(r, c-1));
-            }
-        count++;
     }
-
-    if (Done)
-        return dist[row-1][col-1];
-    return -1;
+    
+    answer = visited[endY][endX] == 1000001 ? -1 : visited[endY][endX];
+    
+    return answer;
 }
