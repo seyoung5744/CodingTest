f44670779b7cd441e030005250875c47458948de
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-17 17:31:19 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 93.55 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,32 +1,34 @@
-class Solution {
+import java.util.Arrays;
+
+public class Solution {
+
     public static long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            answer[i] = find(numbers[i]);
+        int idx = 0;
+        for (long number : numbers) {
+
+            if (number % 2 == 0)
+            {
+                answer[idx++] = number + 1;
+                continue;
+            }
+
+            String originBits = Long.toString(number, 2);
+            originBits = '0' + originBits;
+            char[] next = originBits.toCharArray();
+            for(int i = next.length - 1; i > 0; --i)
+            {
+                if (next[i-1] == '0')
+                {
+                    next[i-1] = '1';
+                    next[i] = '0';
+                    break;
+                }
+            }
+
+            answer[idx++] = Long.parseLong(String.valueOf(next), 2);
         }
 
         return answer;
-    }
-
-    public static long find(long number) {
-
-        if(number % 2 == 0) {
-            return number + 1;
-        }else {
-            long num = 2;
-            long tmp = number;
-
-            while(tmp <= number) {
-                tmp = number ^ num;
-
-                if(tmp < number) {
-//                    tmp ^= num;
-                    num *= 2;
-                }else {
-                    tmp ^= num / 2;
-                }
-            }
-            return tmp;
-        }
     }
 }
