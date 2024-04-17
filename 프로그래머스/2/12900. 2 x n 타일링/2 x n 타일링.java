25558f7b9340b34e3293b494aa7a587783486306
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-17 16:46:17 +0900

    [level 2] Title: 2 x n 타일링, Time: 3.19 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,16 +1,15 @@
-public class Solution {
-
-    public static Integer[] dp;
-
-    public static int MAX = 1_000_000_007;
-    public static int solution(int n) {
-        dp = new Integer[n + 1];
-
-        dp[0] = 0;
+class Solution {
+    public static int[] dp;
+    
+    public int solution(int n) {
+        int answer = 0;
+        dp = new int[n + 1];
+       
         dp[1] = 1;
         dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % MAX;
+        
+        for(int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1_000_000_007;
         }
         return dp[n];
     }
