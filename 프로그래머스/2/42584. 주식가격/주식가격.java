eeca56ad2e65ebaccd81cc39fe41c69010ccfeb4
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-17 12:27:32 +0900

    [level 2] Title: 주식가격, Time: 23.46 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,23 +1,26 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Solution {
 
     public static int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+
         Deque<Integer> stack = new ArrayDeque<>();
 
         for (int i = 0; i < prices.length; i++) {
-            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                int index = stack.pop();
-                answer[index] = i - index;
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+                answer[stack.peek()] = i - stack.pop();
             }
+            
             stack.push(i);
         }
-
+        
         while(!stack.isEmpty()) {
-            int index = stack.pop();
-            answer[index] = prices.length - index - 1;
+            int idx = stack.pop();
+            answer[idx] = (prices.length - 1) - idx;
         }
         return answer;
     }
+
 }
