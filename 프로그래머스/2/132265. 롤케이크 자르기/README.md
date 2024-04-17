673a0d10cfa1086044ccde0c572d108207f46398
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-17 14:09:31 +0900

    [level 2] Title: 롤케이크 자르기, Time: 304.81 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=java) 
 
 ### 성능 요약
 
-메모리: 178 MB, 시간: 343.02 ms
+메모리: 184 MB, 시간: 304.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 17:47:17
+2024년 04월 17일 14:09:29
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,27 +1,29 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
-    public static int solution(int[] topping) {
-        int answer = 0;
-        Map<Integer, Integer> map1 = new HashMap<>();
-        Map<Integer, Integer> map2 = new HashMap<>();
 
+    public int solution(int[] topping) {
+        int answer = 0;
+
+        Map<Integer, Integer> person1 = new HashMap<>();
         for (int i = 0; i < topping.length; i++) {
-            map2.put(topping[i], map2.getOrDefault(topping[i], 0) + 1);
+            person1.put(topping[i], person1.getOrDefault(topping[i], 0) + 1);
         }
 
+        Map<Integer, Integer> person2 = new HashMap<>();
         for (int i = 0; i < topping.length; i++) {
-            map1.put(topping[i], map1.getOrDefault(topping[i], 0) + 1);
-            map2.put(topping[i], map2.get(topping[i]) - 1);
-
-            if(map2.get(topping[i]) == 0) {
-                map2.remove(topping[i]);
+            person2.put(topping[i], person2.getOrDefault(topping[i], 0) + 1);
+            person1.put(topping[i], person1.getOrDefault(topping[i], 0) - 1);
+            if(person1.get(topping[i]) == 0) {
+                person1.remove(topping[i]);
             }
-      
-            if(map2.keySet().size() == map1.keySet().size()) {
+
+            if(person2.keySet().size() == person1.keySet().size()) {
                 answer++;
             }
         }
+
         return answer;
     }
 }
