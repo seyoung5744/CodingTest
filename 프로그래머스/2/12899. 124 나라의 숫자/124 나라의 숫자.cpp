f139d92c2c4c44615bd655770614bc2050538c19
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-04-24 15:44:46 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 3.79 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.cpp
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.cpp
@@ -4,20 +4,16 @@
 
 using namespace std;
 
-map<int, int> m = {{0, 1}, {1, 2}, {2, 4}};
+map<int, char> m = {{0, '1'}, {1, '2'}, {2, '4'}};
+
 string solution(int n) {
     string answer = "";
-    
     
     while(n > 0)
     {
         --n;
-        int rest = n % 3;
-        
-        answer = to_string(m[rest]) + answer;
-        
+        answer = m[n % 3] + answer;
         n /= 3;
     }
-    
     return answer;
 }
--- 프로그래머스/2/12899. 124 나라의 숫자/README.md
+++ 프로그래머스/2/12899. 124 나라의 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 124 나라의 숫자 - 12899 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899?language=cpp) 
 
 ### 성능 요약
 
-메모리: 52.1 MB, 시간: 0.03 ms
+메모리: 3.79 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 14:50:9
+2024년 04월 24일 15:44:43
 
 ### 문제 설명
 
