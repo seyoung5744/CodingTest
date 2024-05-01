4cbc1ac388b9a872265bbba0cbb827966778b78e
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-01 15:45:47 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
@@ -1,42 +1,49 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <set>
-#include <algorithm>
-#include <iostream>
 using namespace std;
 
 long long solution(vector<int> weights) {
     long long answer = 0;
-    sort(weights.begin(), weights.end());
-    
     map<int, long long> m;
-    vector<int> s;
     
     for(int i = 0; i < weights.size(); ++i)
     {
-        ++m[weights[i]];
-        if (s.empty() || s.back() != weights[i])
-            s.push_back(weights[i]);
+        m[weights[i]]++;
     }
     
-    for(int i = 0; i < s.size(); ++i)
+    for(auto t : m)
     {
-        int w1 = s[i];
-        for(int j = i+1; j < s.size(); ++j)
+        int weight = t.first;
+        long long count = t.second;
+        answer += (count * (count - 1)) / 2;
+        
+        int targetWeight = weight * 3;
+        
+        if (targetWeight % 2 == 0)
         {
-            int w2 = s[j];
-            
-            if (w1 * 4 < w2)
-                break;
-            
-            if (w1 * 3 == w2 * 2 || w1 * 4 == w2 * 2 || w1 * 4 == w2 * 3)
+            if (m.find(targetWeight / 2) != m.end())
             {
-                answer += m[w1] * m[w2];
+                answer += count * m[targetWeight / 2];
             }
         }
         
-        answer += m[w1] * (m[w1] - 1) / 2;
+        targetWeight = weight * 4;
+        if (targetWeight % 2 == 0)
+        {
+            if (m.find(targetWeight / 2) != m.end())
+            {
+                answer += count * m[targetWeight/2];
+            }
+        }
+        
+        if (targetWeight % 3 == 0)
+        {
+            if (m.find(targetWeight / 3) != m.end())
+            {
+                answer += count * m[targetWeight/3];
+            }
+        }
     }
     
     return answer;
