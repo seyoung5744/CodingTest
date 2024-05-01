f50c33a91c24fb21d8a81a5cf1263fbcdbbeaeb8
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-01 17:38:16 +0900

    [level 2] Title: [3차] 방금그곡, Time: 5.02 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,53 +1,51 @@
+import java.util.Arrays;
+
 public class Solution {
 
-
-    public static String solution(String m, String[] musicInfos) {
+    public static String solution(String m, String[] musicinfos) {
         String answer = "(None)";
+        int maxPlayTime = 0;
 
-        m = replaceSharp(m);
+        m = convertMelody(m);
+        // "12:00,12:14,HELLO,CDEFGAB"
+        for (int i = 0; i < musicinfos.length; i++) {
+            String[] tokens = musicinfos[i].split(",");
+            int playTime = convertTime(tokens[1]) - convertTime(tokens[0]) + 1;
 
-        int max = Integer.MIN_VALUE;
+            String melo = convertMelody(tokens[3]);
+            while (playTime > melo.length()) {
+                melo += melo;
+            }
 
-        for (int i = 0; i < musicInfos.length; i++) {
-            String[] tokens = musicInfos[i].split(",");
-            int totalMin = toMin(tokens[1]) - toMin(tokens[0]);
-            String title = tokens[2];
-            String musicInfo = totalMusicInfo(totalMin, replaceSharp(tokens[3]));
+            melo = melo.substring(0, playTime);
 
-            if (musicInfo.contains(m)) {
-                if (totalMin > max) {
-                    answer = title;
-                    max = totalMin;
+            if (melo.contains(m)) {
+                if (playTime > maxPlayTime) {
+                    maxPlayTime = playTime;
+                    answer = tokens[2];
                 }
             }
         }
+
         return answer;
     }
 
-    public static String replaceSharp(String str) {
-        StringBuilder sb = new StringBuilder();
+    private static int convertTime(String time) {
+        int[] tokens = Arrays.stream(time.split(":")).mapToInt(Integer::parseInt).toArray();
+        return tokens[0] * 60 + tokens[1];
+    }
 
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '#') {
-                sb.replace(sb.length() - 1, sb.length(), String.valueOf(str.charAt(i - 1)).toLowerCase());
-            } else {
-                sb.append(str.charAt(i));
+    private static String convertMelody(String m) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m.length(); ++i) {
+            if (m.charAt(i) == '#') {
+                sb.replace(sb.length() - 1, sb.length(), String.valueOf(m.charAt(i - 1)).toLowerCase());
+                continue;
             }
+
+            sb.append(m.charAt(i));
         }
-        return sb.toString();
-    }
 
-    public static int toMin(String times) {
-        String[] tokens = times.split(":");
-        return Integer.parseInt(tokens[0]) * 60 + Integer.parseInt(tokens[1]);
-    }
-
-    public static String totalMusicInfo(int totalMin, String musicInfo) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < totalMin; i++) {
-            sb.append(musicInfo.charAt(i % musicInfo.length()));
-        }
         return sb.toString();
     }
 }
