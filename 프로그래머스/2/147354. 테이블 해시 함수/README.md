9607349c036a02fc9ede9d9be4632d73961611d5
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-08 14:38:33 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/README.md
+++ 프로그래머스/2/147354. 테이블 해시 함수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 테이블 해시 함수 - 147354 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147354?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147354?language=cpp) 
 
 ### 성능 요약
 
-메모리: 74.3 MB, 시간: 0.36 ms
+메모리: 4.13 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 18:3:28
+2024년 05월 08일 14:38:31
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
+    int answer = 0;
+    
+    sort(data.begin(), data.end(), [col](vector<int> a, vector<int> b){
+        if (a[col - 1] == b[col - 1])
+            return a[0] > b[0];
+        
+        return a[col - 1] < b[col - 1];
+         });
+    
+    for(int i = row_begin-1; i <= row_end-1; ++i)
+    {
+        int sum = 0;
+        
+        for(int j = 0; j < data[0].size(); ++j)
+        {
+            sum += data[i][j] % (i + 1);
+        }
+        
+        
+        answer = answer^sum;
+    }
+    
+    return answer;
+}
