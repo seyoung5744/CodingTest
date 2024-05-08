f45f65cf95e514186d37960f53eec59a7f3e730a
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-08 15:44:56 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 19.01 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+int solution(vector<int> food_times, long long k) {
+    int answer = 0;
+    map<int, int> count;
+
+    for (int i = 0; i < food_times.size(); ++i)
+    {
+        count[food_times[i]]++;
+    }
+
+    long long curTime = 0;
+    long long totalFood = food_times.size();
+
+    int target = 0;
+    int cycle = 0;
+    int totalCycle = 0;
+    for (auto item : count)
+    {
+        int min = item.first;
+        int minCount = item.second;
+        
+        while (cycle < min)
+        {
+            if (curTime + totalFood > k)
+                break;
+
+            curTime += totalFood;
+            ++cycle;
+        }
+
+        if (cycle == min)
+            totalFood -= minCount;
+        else
+        {
+            target = min;
+            break;
+        }
+    }
+
+    if (totalFood == 0)
+        return -1;
+    
+    for (int i = 0; i < food_times.size(); i++)
+    {
+        if (food_times[i] < target) continue;
+
+        if (curTime == k)
+            return i + 1;
+
+        curTime++;
+    }
+
+    return -1;
+}
