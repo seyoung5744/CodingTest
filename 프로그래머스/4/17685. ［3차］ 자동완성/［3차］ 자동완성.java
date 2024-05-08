855a7114808585b41255b4f811c44d2424692ff7
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-08 17:30:49 +0900

    [level 4] Title: [3차] 자동완성, Time: 323.95 ms, Memory: 301 MB -BaekjoonHub

---
+++ 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.java
@@ -0,0 +1,61 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    static class Node {
+
+        public Map<Character, Node> child = new HashMap<>();
+        public int wordsCount;
+        public boolean isEnd;
+    }
+
+    static class Trie {
+
+        private Node rootNode = new Node();
+
+        public void insert(String str) {
+            Node node = this.rootNode;
+            for (int i = 0; i < str.length(); i++) {
+                if (!node.child.containsKey(str.charAt(i))) {
+                    node.child.put(str.charAt(i), new Node());
+                }
+                node = node.child.get(str.charAt(i));
+                node.wordsCount++;
+            }
+
+            node.isEnd = true;
+        }
+
+        public int getMinDepth(String str)
+        {
+            Node node = this.rootNode;
+
+            for(int i = 0; i < str.length(); ++i)
+            {
+                node = node.child.get(str.charAt(i));
+
+                if (node.wordsCount == 1)
+                    return i + 1;
+            }
+
+            return str.length();
+        }
+
+    }
+
+    public static int solution(String[] words) {
+        int answer = 0;
+
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.insert(word);
+        }
+
+        for (String word : words) {
+            answer += trie.getMinDepth(word);
+        }
+
+        return answer;
+    }
+}
