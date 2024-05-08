8d441706fb36695403b88874479c045cac488c91
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-08 12:15:51 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -3,38 +3,39 @@ public class Solution {
     public static int solution(int[] arrayA, int[] arrayB) {
         int answer = 0;
 
-        int a = 0;
-        for (int i = 0; i < arrayA.length; i++) {
-            a = gcd(a, arrayA[i]);
+        int gcdA = gcd(arrayA);
+        int gcdB = gcd(arrayB);
+
+        if (canDivided(arrayA, gcdB)) {
+            answer = gcdB;
         }
 
-        int b = 0;
-        for (int i = 0; i < arrayB.length; i++) {
-            b = gcd(b, arrayB[i]);
-        }
-
-        boolean flagA = true;
-        for (int i = 0; i < arrayA.length; i++) {
-            if(arrayA[i] % b == 0) flagA = false;
-        }
-
-        boolean flagB = true;
-        for (int i = 0; i < arrayB.length; i++) {
-            if(arrayB[i] % a == 0) flagB = false;
-        }
-
-        if (flagA) {
-            answer = b;
-        }
-
-        if (flagB) {
-            answer = Math.max(answer, a);
+        if (canDivided(arrayB, gcdA)) {
+            answer = Math.max(answer, gcdA);
         }
 
         return answer;
     }
 
-    public static int gcd(int a, int b) {
+    public static boolean canDivided(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % target == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int gcd(int[] arr) {
+        int result = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            result = gcd(result, arr[i]);
+        }
+        return result;
+    }
+
+    private static int gcd(int a, int b) {
         while (b > 0) {
             int temp = b;
             b = a % b;
