8aae14518287c49be084d29a24eeb5558eee61b1
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-09 19:03:27 +0900

    [level 4] Title: 호텔 방 배정, Time: 334.96 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -1,33 +1,24 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
+    public static Map<Long, Long> roomInfo = new HashMap<>();
 
-    public static long[] solution(long k, long[] room_number) {
+    private static long findEmptyRoom(long wantRoom){
+        if(!roomInfo.containsKey(wantRoom)){
+            roomInfo.put(wantRoom, wantRoom + 1);
+            return wantRoom;
+        }
+
+        long nextRoom = roomInfo.get(wantRoom);
+        long emptyRoom = findEmptyRoom(nextRoom);
+        roomInfo.put(wantRoom, emptyRoom);
+        return emptyRoom;
+    }
+
+    public long[] solution(long k, long[] room_number) {
         long[] answer = new long[room_number.length];
-        Map<Long, Long> map = new HashMap<>();
-
-        int idx = 0;
-
-        for (long roomNum : room_number) {
-            List<Long> temp = new ArrayList<>();
-
-            while (true) {
-                if (!map.containsKey(roomNum)) {
-                    map.put(roomNum, roomNum + 1);
-                    for (int i = 0; i < temp.size(); ++i) {
-                        map.put(temp.get(i), roomNum + 1);
-                    }
-                    answer[idx++] = roomNum;
-                    break;
-                } else {
-                    temp.add(roomNum);
-                    roomNum = map.get(roomNum);
-                }
-            }
+        for (int i = 0; i < room_number.length; i++) {
+            answer[i] = findEmptyRoom(room_number[i]);
         }
         return answer;
     }
