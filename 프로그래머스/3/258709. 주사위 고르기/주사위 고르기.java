5d4fd0cd9c6a50eb9fd4a9598c93d660c111675a
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-11 21:24:01 +0900

    [level 3] Title: 주사위 고르기, Time: 335.92 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -1,11 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Solution{
 
