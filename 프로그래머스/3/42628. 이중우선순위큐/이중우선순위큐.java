32300f99dcd4aa2db80d4fbac302aaa28f08e0d6
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-22 17:51:08 +0900

    [level 3] Title: 이중우선순위큐, Time: 1.15 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,69 +1,77 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class Solution {
-    static class DoublyPriorityQueue {
-        Queue<Integer> minPq = new PriorityQueue<>();
-        Queue<Integer> maxPq = new PriorityQueue<>((a, b) -> b - a);
-        int size;
-        
-        public void add(int num){
-            maxPq.add(num);
-            minPq.add(num);
-            size++;
-        }
-        
-        public void removeMax() {
-            if(size == 0) return;
-            maxPq.poll();
-            
-            if(--size == 0) {
-                maxPq.clear();
-                minPq.clear();
-            }
-        }
-        
-        public void removeMin() {
-            if(size == 0) return;
-            minPq.poll();
-            
-            if(--size == 0){
-                minPq.clear();
-                maxPq.clear();
-            }
-        }
-        
-        public int max() {
-            if(size == 0) return 0;
-            return maxPq.peek();
-        }
-        
-        public int min() {
-            if(size == 0) return 0;
-            return minPq.peek();
-        }
-        
-    }
-    public int[] solution(String[] operations) {
-        DoublyPriorityQueue dpq = new DoublyPriorityQueue();
-        
-        for(String operation : operations){
-            String[] op = operation.split(" ");
-            switch(op[0]){
-                case "I":
-                    dpq.add(Integer.parseInt(op[1]));            
-                    break;
-                case "D":
-                    if(op[1].equals("1")){
-                        dpq.removeMax();
-                    }else{
-                        dpq.removeMin();
+
+    public static int[] solution(String[] operations) {
+        PriorityQueue<Integer> maxQueue = new PriorityQueue<>((a, b) -> b - a);
+        PriorityQueue<Integer> minQueue = new PriorityQueue<>((a, b) -> a - b);
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (String op : operations) {
+            String[] tokens = op.split(" ");
+
+            if (tokens[0].equals("I")) { // 숫자 삽입
+                int num = Integer.parseInt(tokens[1]);
+                maxQueue.add(num);
+                minQueue.add(num);
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            } else {
+                if (Integer.parseInt(tokens[1]) == 1) { //최댓값 삭제
+                    if (maxQueue.isEmpty())
+                        continue;
+
+                    int max = maxQueue.poll();
+                    if(map.get(max) == 0){
+                        map.remove(max);
                     }
-                    break;
+                    if(map.containsKey(max) && map.get(max) != 0) {
+                        map.put(max, map.getOrDefault(max, 0) - 1);
+                    }
+                } else { // 최솟값 삭제
+                    if (minQueue.isEmpty())
+                        continue;
+                    
+                    int min = minQueue.poll();
+                    if(map.get(min) == 0) {
+                        map.remove(min);
+                    }
+                    if(map.containsKey(min) && map.get(min) != 0) {
+                        map.put(min, map.getOrDefault(min, 0) - 1);
+                    }
+                }
+            }
+            if(maxQueue.size() == 0) {
+                while(!minQueue.isEmpty()) minQueue.poll();
+            }
+
+            if(minQueue.size() == 0) {
+                while(!maxQueue.isEmpty()) maxQueue.poll();
             }
         }
-        
-        return new int[]{dpq.max(), dpq.min()};
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() > 0) {
+                if(entry.getKey() < min) {
+                    min = entry.getKey();
+                }
+
+                if(entry.getKey() > max) {
+                    max = entry.getKey();
+                }
+            }
+        }
+        if(max == Integer.MIN_VALUE && min == Integer.MAX_VALUE) {
+            return new int[]{0, 0};
+        }
+        return new int[]{max, min};
     }
 
 }
