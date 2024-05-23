cd82aaceba4dc20f8aa8157cf0417e47fb5d769e
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-23 17:36:40 +0900

    [level 3] Title: 기지국 설치, Time: 0.58 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -5,6 +5,7 @@ public class Solution {
         int answer = 0;
         int start = 0;
         int end = 0;
+        
         for(int station : stations) {
             end = station - w - 1;
             if (end > start) {
@@ -14,14 +15,12 @@ public class Solution {
                     answer += 1;
                 }
             }
-
             start = station + w;
         }
-        if(start < n)
-        {
-            end = n;
-            int remian = (end - start) % (w * 2 + 1);
-            answer += (end - start) / (w * 2 + 1);
+        
+        if(start < n) {
+            int remian = (n - start) % (w * 2 + 1);
+            answer += (n - start) / (w * 2 + 1);
             if(remian > 0) {
                 answer += 1;
             }
