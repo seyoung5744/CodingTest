f4e1c98b29bcdcf049fa6f413a74a7a6132cc6c6
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-23 21:26:53 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.04 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/README.md
+++ 프로그래머스/2/150368. 이모티콘 할인행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이모티콘 할인행사 - 150368 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150368#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150368) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 0.05 ms
+메모리: 83.7 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 15:26:31
+2024년 05월 23일 21:26:52
 
 ### 문제 설명
 
--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -4,45 +4,35 @@ public class Solution {
 
     public static int[] discount = {40, 30, 20, 10};
     public static int[] answer = {0, 0};
+    
     public static int[] solution(int[][] users, int[] emoticons) {
         dfs(0, new int[users.length], emoticons, users);
         return answer;
     }
 
-    public static void dfs(int depth, int[] totalMoney, int[] emoticons, int[][] users)
-    {
-        if (depth == emoticons.length)
-        {
+    public static void dfs(int depth, int[] totalMoney, int[] emoticons, int[][] users) {
+        if (depth == emoticons.length) {
             int pCount = 0;
             int totalCost = 0;
 
-            for(int i = 0; i < users.length; ++i)
-            {
-                if (users[i][1] <= totalMoney[i])
-                {
+            for(int i = 0; i < users.length; ++i) {
+                if (users[i][1] <= totalMoney[i]) {
                     pCount++;
-                }
-                else
-                {
+                } else {
                     totalCost += totalMoney[i];
                 }
             }
 
-            if (pCount > answer[0])
-            {
+            if (pCount > answer[0]) {
                 answer[0] = pCount;
                 answer[1] = totalCost;
-            }
-            else if (pCount == answer[0] && totalCost > answer[1])
-            {
+            } else if (pCount == answer[0] && totalCost > answer[1]) {
                 answer[1] = totalCost;
             }
-
             return;
         }
 
-        for(int i = 0; i < 4; ++i)
-        {
+        for(int i = 0; i < 4; ++i) {
             for (int j = 0; j < users.length; j++) {
                 if(discount[i] >= users[j][0]) {
                     totalMoney[j] += (emoticons[depth] * (100 - discount[i])) / 100;
@@ -58,5 +48,4 @@ public class Solution {
             }
         }
     }
-
 }
