8f7a9a818c7bdd31adef10d91876e387a3637c79
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-26 19:27:56 +0900

    [level 3] Title: 단어 변환, Time: 0.27 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Solution {
+
+    private static class State {
+
+        private final String word;
+        private final int step;
+
+        public State(String word, int step) {
+            this.word = word;
+            this.step = step;
+        }
+    }
+
+    public int solution(String begin, String target, String[] words) {
+        boolean[] isVisited = new boolean[words.length];
+
+        Queue<State> q = new LinkedList<>();
+        q.offer(new State(begin, 0));
+
+        while (!q.isEmpty()) {
+            State state = q.poll();
+            
+            if(state.word.equals(target)){
+                return state.step;
+            }
+            
+            for (int i = 0; i < words.length; i++) {
+                String next = words[i];
+                
+                if (!isConvertable(state.word, next)) {
+                    continue;
+                }
+                
+                if(isVisited[i]) continue;
+                isVisited[i] = true;
+                
+                q.offer(new State(next, state.step + 1));
+
+            }
+        }
+        
+        return 0;
+    }
+
+    private static boolean isConvertable(String from, String to) {
+        char[] fromArr = from.toCharArray();
+        char[] toArr = to.toCharArray();
+
+        int diff = 0;
+        for (int i = 0; i < fromArr.length; i++) {
+            if (fromArr[i] != toArr[i]) {
+                diff++;
+            }
+        }
+
+        return diff == 1;
+    }
+}
