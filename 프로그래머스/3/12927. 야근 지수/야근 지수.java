d87012d9957a74a1d3c61fcc04d8d85494c002f0
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-05-26 19:20:18 +0900

    [level 3] Title: 야근 지수, Time: 134.56 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
+        
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
         for(int work : works) {
@@ -19,6 +20,7 @@ class Solution {
         while(!pq.isEmpty()) {
             answer += (long) Math.pow(pq.poll(), 2);
         }
+        
         return answer;
     }
 }
