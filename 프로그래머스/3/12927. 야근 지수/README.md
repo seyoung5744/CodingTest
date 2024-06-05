d68ecbcbbd3921493716c83528b2c90ba1813a51
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-05 15:00:26 +0900

    [level 3] Title: 야근 지수, Time: 99.08 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/README.md
+++ 프로그래머스/3/12927. 야근 지수/README.md
@@ -1,10 +1,10 @@
 # [level 3] 야근 지수 - 12927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927?language=java) 
 
 ### 성능 요약
 
-메모리: 69.2 MB, 시간: 134.56 ms
+메모리: 67.7 MB, 시간: 99.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 26일 19:20:16
+2024년 06월 05일 15:00:24
 
 ### 문제 설명
 
--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,26 +1,26 @@
-import java.util.*;
+import java.util.PriorityQueue;
 
-class Solution {
-    public long solution(int n, int[] works) {
+public class Solution {
+
+    public static long solution(int n, int[] works) {
         long answer = 0;
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int work : works) {
-            pq.add(work);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int work : works) {
+            pq.offer(work);
         }
-        
-        while(n-- > 0 && !pq.isEmpty()) {
-            int remain = pq.poll() - 1;
-            
-            if(remain != 0)
-                pq.add(remain);
+
+        while (n-- > 0 && !pq.isEmpty()) {
+            int cur = pq.poll();
+            if(cur -1 == 0)
+                continue;
+            pq.offer(cur - 1);
         }
-            
-        while(!pq.isEmpty()) {
-            answer += (long) Math.pow(pq.poll(), 2);
+
+        while (!pq.isEmpty()) {
+            answer += (int) Math.pow(pq.poll(), 2);
         }
-        
         return answer;
     }
+
 }
