b84edb5d7d6d5dc1e649944a3ada82855da888b3
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-05 14:29:48 +0900

    [level 3] Title: 모두 0으로 만들기, Time: 232.23 ms, Memory: 301 MB -BaekjoonHub

---
+++ 프로그래머스/3/76503. 모두 0으로 만들기/모두 0으로 만들기.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+public class Solution {
+
+    public static class Node {
+
+        int idx;
+        Set<Integer> linkedNode;
+
+        public Node(int idx) {
+            this.idx = idx;
+            this.linkedNode = new HashSet<>();
+        }
+    }
+
+    public static List<Node> graph = new ArrayList<>();
+
+    public static long solution(int[] ab, int[][] edges) {
+        long answer = 0;
+
+        long[] a = new long[ab.length];
+        for (int i = 0; i < ab.length; i++) {
+            a[i] = ab[i];
+            graph.add(new Node(i));
+        }
+
+        for (int[] edge : edges) {
+            int x = edge[0];
+            int y = edge[1];
+            graph.get(x).linkedNode.add(y);
+            graph.get(y).linkedNode.add(x);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < graph.size(); i++) {
+            if (graph.get(i).linkedNode.size() == 1) {
+                q.offer(i);
+            }
+        }
+
+        int nodeCount = 0;
+        int lastIdx = 0;
+        while (!q.isEmpty()) {
+            int curIdx = q.poll();
+            nodeCount++;
+            lastIdx = curIdx;
+
+            if (graph.get(curIdx).linkedNode.size() == 0) {
+                continue;
+            }
+
+            int next = graph.get(curIdx).linkedNode.iterator().next();
+
+            answer += Math.abs(a[curIdx]);
+            a[next] += a[curIdx];
+            a[curIdx] = 0;
+
+            graph.get(next).linkedNode.remove(Integer.valueOf(curIdx));
+
+            if (graph.get(next).linkedNode.size() == 1) {
+                q.offer(next);
+            }
+        }
+
+        if (a[lastIdx] != 0) {
+            return -1;
+        }
+
+        return nodeCount == a.length ? answer : -1;
+    }
+}
