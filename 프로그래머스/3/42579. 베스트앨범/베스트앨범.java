8c9fe9a156c3a8f5482f80537d4ddceada98130e
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-05 17:44:43 +0900

    [level 3] Title: 베스트앨범, Time: 3.33 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,71 +1,68 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Solution {
-    private static class Music implements Comparable<Music> {
-        int index;
-        int play;
-        String genre;
 
-        public Music(int index, int play, String genre) {
-            this.index = index;
-            this.play = play;
+    static class Genre {
+
+        String genre;
+        int total;
+        PriorityQueue<Music> musics;
+
+        public Genre(String genre) {
             this.genre = genre;
+            this.musics = new PriorityQueue<>();
+        }
+
+        public void addMusic(Music music) {
+            this.total += music.playTime;
+            this.musics.add(music);
+        }
+    }
+
+    static class Music implements Comparable<Music> {
+
+        int idx;
+        String genre;
+        int playTime;
+
+        public Music(int idx, String genre, int playTime) {
+            this.idx = idx;
+            this.genre = genre;
+            this.playTime = playTime;
         }
 
         @Override
         public int compareTo(Music o) {
-            if(this.play == o.play) {
-                return this.index - o.index;
+            if (this.playTime == o.playTime) {
+                return this.idx - o.idx;
             }
-            return o.play - this.play;
+
+            return o.playTime - this.playTime;
         }
 
     }
 
-    static class Song implements Comparable<Song>{
-        String genre;
-        List<Music> musics;
-        int totalPlay;
-
-        public Song(String genre) {
-            this.genre = genre;
-            this.musics = new ArrayList<>();
-        }
-
-        @Override
-        public int compareTo(Song o) {
-            return o.totalPlay - this.totalPlay;
-        }
-
- 
-    }
+    public static Map<String, Genre> album = new HashMap<>();
 
     public static int[] solution(String[] genres, int[] plays) {
-        List<Song> songs = new ArrayList<>();
-
-        Set<String> set = Arrays.stream(genres).collect(Collectors.toSet());
-        for (String genre : set) {
-            songs.add(new Song(genre));
+        for (int i = 0; i < genres.length; i++) {
+            album.putIfAbsent(genres[i], new Genre(genres[i]));
+            album.get(genres[i]).addMusic(new Music(i, genres[i], plays[i]));
         }
 
-        for(Song song : songs) {
-            for (int i = 0; i < plays.length; i++) {
-                if(genres[i].equals(song.genre)) {
-                    song.musics.add(new Music(i, plays[i], genres[i]));
-                    song.totalPlay += plays[i];
-                }
-            }
-        }
+        List<Genre> list = new ArrayList<>();
+        list.addAll(album.values());
+        list.sort((o1, o2) -> o2.total - o1.total);
 
-        Collections.sort(songs);
-        for(Song song : songs) {
-            Collections.sort(song.musics);
-        }
         List<Integer> answer = new ArrayList<>();
-        for(Song song : songs) {
-            for (int i = 0; i < 2 && i < song.musics.size(); i++) {
-                answer.add(song.musics.get(i).index);
+        for(Genre genre : list) {
+            int count = 0;
+            while(!genre.musics.isEmpty())
+            {
+                answer.add(genre.musics.poll().idx);
+                count++;
+
+                if(count >= 2) break;
             }
         }
         return answer.stream().mapToInt(Integer::intValue).toArray();
