bc69589b089163620a7594c79aabbc7275ec4eed
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-07 18:11:02 +0900

    [level 4] Title: 호텔 방 배정, Time: 359.49 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -1,24 +1,29 @@
 import java.util.*;
 
 public class Solution {
-    public static Map<Long, Long> roomInfo = new HashMap<>();
 
-    private static long findEmptyRoom(long wantRoom) {
-        if(!roomInfo.containsKey(wantRoom)) {
-            roomInfo.put(wantRoom, wantRoom + 1);
-            return wantRoom;
-        }
-
-        long nextRoom = roomInfo.get(wantRoom);
-        long emptyRoom = findEmptyRoom(nextRoom);
-        roomInfo.put(wantRoom, emptyRoom);
-        return emptyRoom;
-    }
-
-    public long[] solution(long k, long[] room_number) {
+    public static long[] solution(long k, long[] room_number) {
         long[] answer = new long[room_number.length];
-        for (int i = 0; i < room_number.length; i++) {
-            answer[i] = findEmptyRoom(room_number[i]);
+        Map<Long, Long> map = new HashMap<>();
+
+        int idx = 0;
+
+        for (long roomNum : room_number) {
+            List<Long> temp = new ArrayList<>();
+
+            while (true) {
+                if (!map.containsKey(roomNum)) {
+                    map.put(roomNum, roomNum + 1);
+                    for (int i = 0; i < temp.size(); ++i) {
+                        map.put(temp.get(i), roomNum + 1);
+                    }
+                    answer[idx++] = roomNum;
+                    break;
+                } else {
+                    temp.add(roomNum);
+                    roomNum = map.get(roomNum);
+                }
+            }
         }
         return answer;
     }
