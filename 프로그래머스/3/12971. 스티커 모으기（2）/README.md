ebaf2a9e51e6134514b306ec765b43955fe4723b
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-12 15:30:58 +0900

    [level 3] Title: 스티커 모으기(2), Time: 18.07 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/README.md
+++ 프로그래머스/3/12971. 스티커 모으기（2）/README.md
@@ -1,10 +1,10 @@
 # [level 3] 스티커 모으기(2) - 12971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12971?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12971) 
 
 ### 성능 요약
 
-메모리: 56.2 MB, 시간: 6.39 ms
+메모리: 58.5 MB, 시간: 18.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 13:11:14
+2024년 06월 12일 15:30:56
 
 ### 문제 설명
 
--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,24 +1,44 @@
-class Solution {
+import java.util.Arrays;
+
+public class Solution {
+
+    public static int[] dp1;
+    public static int[] dp2;
+
     public static int solution(int[] sticker) {
-        if(sticker.length == 1) {
+        if (sticker.length == 1)
+        {
             return sticker[0];
-        }else if (sticker.length == 2){
+        }
+        else if (sticker.length == 2)
+        {
             return Math.max(sticker[0], sticker[1]);
         }
-        return Math.max(getMax(0, sticker.length - 2, sticker, new int[sticker.length]), getMax(1, sticker.length - 1, sticker, new int[sticker.length]));
-    }
+        
+        dp1 = new int[sticker.length];
+        dp1[0] = sticker[0];
+        dp1[1] = sticker[1];
 
-    public static int getMax(int start, int end, int[] sticker, int[] dp) {
-
-        dp[start] = sticker[start];
-        dp[start + 1] = Math.max(sticker[start], sticker[start + 1] );
-
-        for (int i = start + 2; i <= end; i++) {
-            if(dp[i] == 0) {
-                dp[i] = Math.max(dp[i-1], dp[i-2] + sticker[i]);
+        for (int i = 2; i < sticker.length - 1; i++) {
+            if (i == 2) {
+                dp1[i] += sticker[i] + dp1[i - 2];
+                continue;
             }
+
+            dp1[i] += sticker[i] + Math.max(dp1[i - 2], dp1[i - 3]);
         }
 
-        return dp[end];
+        dp2 = new int[sticker.length];
+        dp2[1] = sticker[1];
+        dp2[2] = sticker[2];
+        for (int i = 3; i < sticker.length; i++) {
+            dp2[i] += sticker[i] + Math.max(dp2[i - 2], dp2[i - 3]);
+        }
+
+        return Math.max(
+            Arrays.stream(dp1).max().getAsInt(),
+            Arrays.stream(dp2).max().getAsInt()
+        );
     }
+
 }
