068a51c3605ff7a19e2ce7d3fe993999f32d136f
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-12 15:54:07 +0900

    [level 3] Title: 가장 먼 노드, Time: 48.19 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
 
 ### 성능 요약
 
-메모리: 103 MB, 시간: 37.96 ms
+메모리: 104 MB, 시간: 48.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 13:41:48
+2024년 06월 12일 15:54:05
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,51 +3,47 @@ import java.util.*;
 public class Solution {
 
     public static List<List<Integer>> graph = new ArrayList<>();
-    public static int[] distance;
-    public final static int INF = (int) 2e9;
 
-    public static int solution(int n, int[][] edge) {
-        int answer = 0;
+    public static int solution(int n, int[][] edges) {
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for (int[] point : edge) {
-            int from = point[0];
-            int to = point[1];
-
-            graph.get(from).add(to);
-            graph.get(to).add(from);
+        for (int[] edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
 
-        distance = new int[n + 1];
-        Arrays.fill(distance, INF);
+        int start = 1;
+        int[] visited = bfs(start, n);
 
-        distance[0] = 0;
-        distance[1] = 1;
+        int max = Arrays.stream(visited).max().getAsInt();
+        return (int) Arrays.stream(visited)
+            .filter(value -> value == max)
+            .count();
+    }
+
+    public static int[] bfs(int start, int n) {
+        int[] visited = new int[n + 1];
         Queue<Integer> q = new LinkedList<>();
-        q.add(1);
+        q.add(start);
+        visited[start] = 1;
 
         while (!q.isEmpty()) {
-
             int cur = q.poll();
 
             for (int i = 0; i < graph.get(cur).size(); i++) {
                 int next = graph.get(cur).get(i);
 
-                if (distance[next] <= distance[cur] + 1) {
-                    continue;
+                if (visited[next] == 0 || visited[next] > visited[cur] + 1) {
+                    visited[next] += visited[cur] + 1;
+                    q.add(next);
                 }
-
-                distance[next] = distance[cur] + 1;
-                q.add(next);
             }
         }
-        int max = Arrays.stream(distance).max().getAsInt();
-        return (int) Arrays.stream(distance)
-            .filter(i -> i == max)
-            .count();
+
+        return visited;
     }
-
-
 }
