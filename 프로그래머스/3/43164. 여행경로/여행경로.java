65cc831f5404c215b1a7f78c3a5f5637c2927bdf
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-12 17:07:36 +0900

    [level 3] Title: 여행경로, Time: 1.81 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,32 +1,74 @@
 
 import java.util.*;
-class Solution {
-    public static List<String> result = new ArrayList<>();
-    public static boolean[] visited;
+
+public class Solution {
+
+    public static List<List<String>> graph = new ArrayList<>();
+    public static Map<String, Integer> index = new HashMap<>();
+    public static Map<Integer, String> airport = new HashMap<>();
 
     public static String[] solution(String[][] tickets) {
-        visited = new boolean[tickets.length];
+        Set<String> count = new HashSet<>();
+        for (String[] ticket : tickets) {
+            count.add(ticket[0]);
+            count.add(ticket[1]);
+        }
 
-        dfs("ICN", 0, "ICN", tickets);
+        int idx = 0;
+        for (String str : count) {
+            airport.put(idx, str);
+            index.put(str, idx);
+            idx++;
+            graph.add(new ArrayList<>());
+        }
 
-        Collections.sort(result);
-        return result.get(0).split("_");
+        for (String[] ticket : tickets) {
+            String start = ticket[0];
+            String end = ticket[1];
+            graph.get(index.get(start)).add(end);
+        }
+
+        int start = index.get("ICN");
+        List<String> result = new ArrayList<>();
+        result.add("ICN");
+        dfs(start, tickets, new boolean[tickets.length], 0, result);
+
+        answer.sort((a, b) -> {
+            for (int i = 0; i < a.size(); i++) {
+                int cmp = a.get(i).compareTo(b.get(i));
+                if (cmp != 0) {
+                    return cmp;
+                }
+            }
+            return 0;
+        });
+        return answer.get(0).stream().toArray(String[]::new);
     }
 
-    public static void dfs(String start, int ticketCnt, String word, String[][] tickets) {
+    public static List<List<String>> answer = new ArrayList<>();
+    public static void dfs(int curAirport, String[][] tickets, boolean[] used, int useCnt, List<String> result) {
 
-        if(ticketCnt == tickets.length) {
-            result.add(word);
+        if (useCnt >= tickets.length) {
+            answer.add(new ArrayList<>(result));
             return;
         }
 
         for (int i = 0; i < tickets.length; i++) {
-            if(!visited[i] && tickets[i][0].equals(start)) {
-                visited[i] = true;
-                dfs(tickets[i][1], ticketCnt + 1, word + "_" + tickets[i][1], tickets);
-                visited[i] = false;
+            int start = index.get(tickets[i][0]);
 
+            if (start != curAirport) {
+                continue;
             }
+
+            if (used[i]) {
+                continue;
+            }
+
+            used[i] = true;
+            result.add(tickets[i][1]);
+            dfs(index.get(tickets[i][1]), tickets, used, useCnt + 1, result);
+            used[i] = false;
+            result.remove(result.size() - 1);
         }
     }
 
