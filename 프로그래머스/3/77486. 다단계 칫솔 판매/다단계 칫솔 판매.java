e83c4b42acb38c55df63b31b632b6d8c7ec9d4c4
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-19 17:11:22 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 2814.92 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,52 +1,50 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Solution {
-
-    static class Person {
-
-        String name;
-        String parent;
-        int amount;
-
-        public Person(String name, String parent) {
-            this.name = name;
-            this.parent = parent;
-            this.amount = 0;
-        }
-
-        public int getAmount() {
-            return amount;
-        }
-
-    }
+    public static Map<String, Integer> mapping = new HashMap<>();
+    public static int[] parent;
 
     public static int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-
-        amount = Arrays.stream(amount).map(i -> i * 100).toArray();
-        Map<String, Person> personMap = new LinkedHashMap<>();
-
-        for (int i = 0; i < enroll.length; i++) {
-            personMap.put(enroll[i], new Person(enroll[i], referral[i]));
+        int[] answer = new int[enroll.length];
+        parent = new int[enroll.length+1];
+        mapping.put("-", enroll.length);
+        for(int i = 0; i < enroll.length; ++i)
+        {
+            mapping.put(enroll[i], i);
         }
-        for (int i = 0; i < seller.length; i++) {
-            Person current = personMap.get(seller[i]);
 
-            int total = amount[i];
+        for(int i = 0; i < referral.length; ++i)
+        {
+            parent[i] = mapping.get(referral[i]);
+        }
+        parent[mapping.get("-")] = mapping.get("-");
 
-            while (current != null && total != 0) {
-                int parentProfit = (int) (total * 0.1);
 
-                current.amount += (total - parentProfit);
-                total = parentProfit;
-                current = personMap.get(current.parent);
+        for(int i = 0; i < seller.length; ++i) {
+            String name = seller[i];
+            int idx = mapping.get(name);
+            int count = amount[i];
+            int money = count * 100;
+            while(parent[idx] != idx)
+            {
+                int div = (int)(money * 0.1);
+
+                if (div < 1)
+                    div = 0;
+
+                answer[idx] += money - div;
+
+                money = (int)div;
+
+                idx = parent[idx];
             }
         }
 
-        return personMap.values().stream()
-            .mapToInt(Person::getAmount)
-            .toArray();
+        return answer;
     }
 
 }
