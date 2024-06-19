b6bdddc1a44e8644074a971ad343877da8d9d90e
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-19 15:44:13 +0900

    [level 3] Title: 풍선 터트리기, Time: 31.48 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/README.md
+++ 프로그래머스/3/68646. 풍선 터트리기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 풍선 터트리기 - 68646 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68646?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68646) 
 
 ### 성능 요약
 
-메모리: 137 MB, 시간: 23.80 ms
+메모리: 137 MB, 시간: 31.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 17:14:39
+2024년 06월 19일 15:44:11
 
 ### 문제 설명
 
--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,18 +1,28 @@
-class Solution {
-    public int solution(int[] a) {
+public class Solution {
+
+    public static int solution(int[] a) {
         int answer = 0;
+
         int[] leftMin = new int[a.length];
         int[] rightMin = new int[a.length];
+
         leftMin[0] = a[0];
         rightMin[a.length - 1] = a[a.length - 1];
 
-        for (int i = 1; i < a.length; i++) {
+        for (int i = 1; i < a.length; ++i) {
             leftMin[i] = Math.min(leftMin[i - 1], a[i - 1]);
-            rightMin[a.length - 1 - i] = Math.min(rightMin[a.length - i], a[a.length - i]);
         }
 
-        for (int i = 0; i < a.length; i++) {
-            if(a[i] > leftMin[i] && a[i] > rightMin[i]) continue;
+        for (int i = a.length - 2; i >= 0; --i) {
+            rightMin[i] = Math.min(rightMin[i + 1], a[i + 1]);
+        }
+
+        for (int mid = 0; mid < a.length; mid++) {
+            int num = a[mid]; // 가능하냐 안하냐;
+
+            if (num > leftMin[mid] && num > rightMin[mid]) {
+                continue;
+            }
 
             answer++;
         }
