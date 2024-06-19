e3d80b7620191dd5ae4b11f35e5bcf53ee4680dd
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-19 15:18:29 +0900

    [level 3] Title: 부대복귀, Time: 71.72 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -3,50 +3,50 @@ import java.util.*;
 public class Solution {
 
     public static List<List<Integer>> map = new ArrayList<>();
-    public static int INF = (int) 1e9;
 
     public static int[] solution(int n, int[][] roads, int[] sources, int destination) {
-
-        int[] visited = new int[n+1];
-
-        for (int i = 0; i < n + 1; i++) {
+        int[] answer = new int[sources.length];
+        for (int i = 0; i <= n; i++) {
             map.add(new ArrayList<>());
-            visited[i] = INF;
         }
 
         for (int[] road : roads) {
-            int from = road[0];
-            int to = road[1];
-            map.get(from).add(to);
-            map.get(to).add(from);
+            int a = road[0];
+            int b = road[1];
+            map.get(a).add(b);
+            map.get(b).add(a);
         }
-
-        Queue<Integer> q = new LinkedList<>();
-        q.add(destination);
-        visited[destination] = 0;
-
-        while(!q.isEmpty())
-        {
-            int cur = q.poll();
-
-            for (int i = 0; i < map.get(cur).size(); i++) {
-                int next = map.get(cur).get(i);
-
-                if (visited[next] <= visited[cur] + 1)
-                    continue;
-
-                visited[next] = visited[cur] + 1;
-                q.add(next);
-            }
-        }
-        int[] answer = new int[sources.length];
-
+        int[] counts = bfs(destination, n);
         for (int i = 0; i < sources.length; i++) {
-
-            answer[i] = visited[sources[i]] == INF ? -1 : visited[sources[i]];
+            int idx = sources[i];
+            answer[i] = counts[idx] == 100_001 ? -1 : counts[idx];
         }
 
         return answer;
     }
 
+    public static int[] bfs(int start, int n) {
+        Queue<Integer> q = new LinkedList<>();
+        int[] visited = new int[n + 1];
+        Arrays.fill(visited, 100_001);
+        q.add(start);
+        visited[start] = 0;
+        
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            for (int i = 0; i < map.get(cur).size(); i++) {
+                int next = map.get(cur).get(i);
+
+                if (visited[next] <= visited[cur] + 1) {
+                    continue;
+                }
+
+                visited[next] = visited[cur] + 1;
+                q.add(next);
+            }
+        }
+
+        return visited;
+    }
 }
