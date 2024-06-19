7caab27b158bf4718c531541409baa51d6f7c913
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-19 17:24:22 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 5608.38 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,47 +1,61 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Solution {
-    public static Map<String, Integer> mapping = new HashMap<>();
-    public static int[] parent;
+
+    static class Person {
+
+        String name;
+        int money;
+        Person parent;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        void getReward(int i) {
+            int moneyToParent = (int) (i * 0.1);
+            this.money += i - moneyToParent;
+            if (this.parent != null)
+                this.parent.getReward(moneyToParent);
+        }
+    }
+
+    public static Map<String, Person> person = new HashMap<>();
 
     public static int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+
+        for (int i = 0; i < enroll.length; i++) {
+            person.put(enroll[i], new Person(enroll[i]));
+        }
+        person.put("-", new Person("-"));
+
+        for (int i = 0; i < referral.length; i++) {
+            person.get(enroll[i]).parent = person.get(referral[i]);
+        }
+
+        for (int i = 0; i < seller.length; i++) {
+            String sell = seller[i];
+
+            int money = amount[i] * 100;
+            person.get(seller[i]).getReward(amount[i] * 100);
+//            while (person.get(sell).parent != null) {
+//                Person cur = person.get(sell);
+//
+//                int up = (int) (money * 0.1);
+//                if (up < 1) {
+//                    up = 0;
+//                }
+//                cur.money += money - up;
+//                money = up;
+//                sell = cur.parent.name;
+//            }
+        }
+
         int[] answer = new int[enroll.length];
-        parent = new int[enroll.length+1];
-        mapping.put("-", enroll.length);
-        for(int i = 0; i < enroll.length; ++i)
-        {
-            mapping.put(enroll[i], i);
-        }
-
-        for(int i = 0; i < referral.length; ++i)
-        {
-            parent[i] = mapping.get(referral[i]);
-        }
-        parent[mapping.get("-")] = mapping.get("-");
-
-
-        for(int i = 0; i < seller.length; ++i) {
-            String name = seller[i];
-            int idx = mapping.get(name);
-            int count = amount[i];
-            int money = count * 100;
-            while(parent[idx] != idx)
-            {
-                int div = (int)(money * 0.1);
-
-                if (div < 1)
-                    div = 0;
-
-                answer[idx] += money - div;
-
-                money = (int)div;
-
-                idx = parent[idx];
-            }
+        for (int i = 0; i < enroll.length; i++) {
+            answer[i] = person.get(enroll[i]).money;
         }
 
         return answer;
