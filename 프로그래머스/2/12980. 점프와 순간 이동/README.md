4193168410db9688ae116cf3cb6126257a96ef08
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-20 21:00:49 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/README.md
+++ 프로그래머스/2/12980. 점프와 순간 이동/README.md
@@ -1,10 +1,10 @@
 # [level 2] 점프와 순간 이동 - 12980 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12980?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12980) 
 
 ### 성능 요약
 
-메모리: 3.58 MB, 시간: 0.01 ms
+메모리: 3.81 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 05일 13:42:59
+2024년 06월 20일 21:00:44
 
 ### 문제 설명
 
--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int solution(int n)
-{
+int solution(int n) {
     int answer = 0;
-    while(n > 0)
-    {
-        if (n % 2 == 0)
-        {
+    while(n > 0) {
+        if (n % 2 == 0) {
             n /= 2;
-        }
-        else
-        {
+        } else {
             --n;
             ++answer;
         }
     }
-
     return answer;
 }
