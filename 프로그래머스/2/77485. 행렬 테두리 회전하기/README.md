c34aa3ad5ba60771027688417de519eccf9e40cd
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-23 20:04:42 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 3.21 ms, Memory: 5.85 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/README.md
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬 테두리 회전하기 - 77485 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77485?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77485) 
 
 ### 성능 요약
 
-메모리: 5.97 MB, 시간: 3.09 ms
+메모리: 5.85 MB, 시간: 3.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 08일 14:08:53
+2024년 06월 23일 20:04:39
 
 ### 문제 설명
 
--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.cpp
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.cpp
@@ -12,16 +12,13 @@ vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
     vector<vector<int>> maps(rows, vector<int>(columns, 1));
 
     int num = 1;
-    for (int i = 0; i < rows; ++i)
-    {
-        for (int j = 0; j < columns; ++j)
-        {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j){
             maps[i][j] = num++;
         }
     }
 
-    for (int i = 0; i < queries.size(); ++i)
-    {
+    for (int i = 0; i < queries.size(); ++i){
         int topLeftY = queries[i][0] - 1;
         int topLeftX = queries[i][1] - 1;
         int bottomY = queries[i][2] - 1;
@@ -35,13 +32,11 @@ vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
         int curY = topLeftY;
         int saved = maps[topLeftY][topLeftX];
         int temp;
-        while (true)
-        {
+        while (true){
             int nx = curX + dx[curDir];
             int ny = curY + dy[curDir];
 
-            if (nx > bottomX || nx < topLeftX || ny > bottomY || ny < topLeftY)
-            {
+            if (nx > bottomX || nx < topLeftX || ny > bottomY || ny < topLeftY) {
                 ++curDir;
                 continue;
             }
