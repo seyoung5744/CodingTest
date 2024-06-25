d854f1f02359af7e43d7405d9ac779abda8cffb3
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-25 20:00:11 +0900

    [level 2] Title: 귤 고르기, Time: 45.16 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 100 MB, 시간: 51.58 ms
+메모리: 102 MB, 시간: 45.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 05일 14:56:17
+2024년 06월 25일 20:00:07
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,13 +1,16 @@
 import java.util.*;
 import java.util.stream.Collectors;
+
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
 
         Map<Integer, Integer> countMap = new HashMap<>();
+        
         for (int tang : tangerine) {
             countMap.put(tang, countMap.getOrDefault(tang, 0) + 1);
         }
+        
         List<Integer> values = countMap.values().stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         for(int value : values) {
             if(k <= 0)
@@ -16,6 +19,7 @@ class Solution {
             answer++;
             k -= value;
         }
+        
         return answer;
     }
 }
