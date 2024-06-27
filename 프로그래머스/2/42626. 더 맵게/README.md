690d8c3e0eda26944e9fb858c133f563edb22cbc
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-27 18:03:25 +0900

    [level 2] Title: 더 맵게, Time: 228.75 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 41.2 MB, 시간: 233.36 ms
+메모리: 41.3 MB, 시간: 228.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 16:24:16
+2024년 06월 27일 18:03:23
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -6,13 +6,11 @@ using namespace std;
 int solution(vector<int> scoville, int K) {
     int answer = 0;
     priority_queue<int, vector<int>, greater<int>> pq;
-    for(int i = 0; i < scoville.size(); ++i)
-    {
+    for(int i = 0; i < scoville.size(); ++i) {
         pq.push(scoville[i]);
     }
     
-    while(!pq.empty())
-    {
+    while(!pq.empty()) {
         if (pq.top() >= K)
             break;
         
