0e7d87ab50f0ee295075755ce29d12b7d38cc700
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-28 16:46:44 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -7,28 +7,22 @@ string solution(string s) {
     string answer = "";
     bool bIsFirst = true;
 
-    for (int i = 0; i < s.length(); ++i)
-    {
+    for (int i = 0; i < s.length(); ++i) {
         char ch = s[i];
-        if (bIsFirst)
-        {
+        if (bIsFirst) {
             if (ch == ' ')
                 bIsFirst = true;
             else
                 bIsFirst = false;
             
             ch = toupper(ch);
-        }
-        else
-        {
+        } else {
             if (ch == ' ')
                 bIsFirst = true;
 
             ch = tolower(ch);
         }
-
         answer += ch;
     }
-
     return answer;
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 3.62 MB, 시간: 0.01 ms
+메모리: 3.68 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 05일 12:30:29
+2024년 06월 28일 16:46:40
 
 ### 문제 설명
 
