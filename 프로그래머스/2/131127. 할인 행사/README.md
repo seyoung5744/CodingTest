8929f7003faf8f53172e9fd5e2d240e2fa56e9b5
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-28 17:16:27 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.01 ms
+메모리: 4.21 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 05일 15:52:35
+2024년 06월 28일 17:16:24
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -9,31 +9,26 @@ map<string, int> m;
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
     int answer = 0;
     int days = 0;
-    for(int i = 0; i < want.size(); ++i)
-    {
+    for(int i = 0; i < want.size(); ++i) {
         m.insert(make_pair(want[i], i));
     }
     
-    for(int i = 0; i < discount.size(); ++i)
-    {
+    for(int i = 0; i < discount.size(); ++i) {
         if (m.find(discount[i]) != m.end())
             --number[m[discount[i]]];
         
-        if (i - 10 >= 0)
-        {
+        if (i - 10 >= 0) {
             if (m.find(discount[i-10]) != m.end())
                 ++number[m[discount[i-10]]];
         }
         
         bool flag = true;
-        for(int j = 0; j < number.size() && flag; ++j)
-        {
+        for(int j = 0; j < number.size() && flag; ++j) {
             if (number[j] != 0)
                 flag = false;
         }
         
-        if (flag)
-        {
+        if (flag) {
             cout << i + 1 << endl;
             ++answer;
         }
