fcc87d7ed7baf804d10f648043c77918215e20b9
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-06-29 00:28:19 +0900

    [level 3] Title: 정수 삼각형, Time: 0.72 ms, Memory: 8.34 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
 
 ### 성능 요약
 
-메모리: 8.23 MB, 시간: 0.67 ms
+메모리: 8.34 MB, 시간: 0.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 05일 14:51:35
+2024년 06월 29일 00:28:14
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -7,29 +7,21 @@ int solution(vector<vector<int>> triangle) {
     int answer = 0;
     vector<vector<int>> dp = triangle;
     
-    for(int i = 1; i < dp.size(); ++i)
-    {
-        for(int j = 0; j <= i; ++j)
-        {
+    for(int i = 1; i < dp.size(); ++i) {
+        for(int j = 0; j <= i; ++j) {
             int left = j - 1;
             int right = j;
-            if (left < 0)
-            {
+            if (left < 0) {
                 dp[i][j] += dp[i-1][right];
-            }
-            else if (right > i-1)
-            {
+            } else if (right > i-1) {
                 dp[i][j] += dp[i-1][left];
-            }
-            else
-            {
+            } else {
                 dp[i][j] += max(dp[i-1][right], dp[i-1][left]);
             }
         }
     }
     
-    for(int i = 0; i < dp.back().size(); ++i)
-    {
+    for(int i = 0; i < dp.back().size(); ++i) {
         answer = max(answer, dp.back()[i]);
     }
     return answer;
