cdaccc3a56805d9f76ec22e9dc3d50ea88af70f9
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-07-12 20:26:29 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 82.11 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,54 +1,42 @@
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
 
     public static int[] solution(String[] gems) {
-        int[] answer = {0 ,0};
+         int[] answer = {0, 0};
         int minLength = gems.length + 1;
 
-        final Set<String> kinds = new HashSet<>();
+        Set<String> kinds = new HashSet<>();
         kinds.addAll(Arrays.asList(gems));
 
         Map<String, Integer> map = new HashMap<>();
 
         int start = 0;
         int end = 0;
+        
         while (end < gems.length) {
 
             map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
 
-            if (map.size() < kinds.size())
-            {
-                ++end;
-                continue;
-            }
+            while (map.keySet().size() == kinds.size()) {
 
-            while (true) {
                 if (map.get(gems[start]) > 1) {
                     map.put(gems[start], map.get(gems[start]) - 1);
-                    start++;
-                }
-                else
-                {
+                } else {
                     break;
                 }
+                start++;
             }
 
-            if(minLength > end - start + 1)
-            {
-                minLength = end - start + 1;
-                answer[0] = start + 1;
-                answer[1] = end + 1;
+            if(map.keySet().size() == kinds.size()) {
+                if (minLength > end - start + 1) {
+                    answer = new int[]{start + 1, end + 1};
+                    minLength = end - start + 1;
+                }
             }
-            ++end;
+            end++;
         }
 
         return answer;
     }
-
 }
