daba7edfc33ca8405acbc13a8688bd93b1a8aa13
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-07-20 19:32:28 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -7,14 +7,12 @@ vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     
     int total = brown + yellow;
-    int row = 3;
     
-    for(; row <= total/3; ++row)
-    {
-        if (total % row == 0 && (row - 2) * ((total/row) - 2) == yellow)
-        {
+    for(int row = 3; row <= total/3; ++row) {
+        int col = total / row;
+        if (col * row == total && (row - 2) * (col - 2) == yellow) {
             answer.push_back(row);
-            answer.push_back(total / row);
+            answer.push_back(col);
             break;
         }
     }
