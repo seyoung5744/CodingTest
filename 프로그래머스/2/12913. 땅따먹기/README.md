132da2475048e554d45fcfae40972ddadb7c8060
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-07-25 21:08:38 +0900

    [level 2] Title: 땅따먹기, Time: 49.10 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/README.md
+++ 프로그래머스/2/12913. 땅따먹기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 땅따먹기 - 12913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913) 
 
 ### 성능 요약
 
-메모리: 85.6 MB, 시간: 20.67 ms
+메모리: 111 MB, 시간: 49.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 17일 12:53:17
+2024년 07월 25일 21:08:34
 
 ### 문제 설명
 
--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -11,10 +11,8 @@ public class Solution {
         
         for (int i = 1; i < land.length; i++) {
             for (int j = 0; j < 4; j++) {
-                for(int k = 0; k < 4; ++k)
-                {
-                    if (k == j)
-                        continue;
+                for(int k = 0; k < 4; ++k) {
+                    if (k == j) continue;
                     
                     dp[i][j] = Math.max(dp[i][j], land[i][j] + dp[i-1][k]);
                 }
