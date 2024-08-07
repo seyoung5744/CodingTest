b8ac08b6f37f5db6623c7ed729a4b659766af45a
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-08-07 15:03:44 +0900

    [level 2] Title: 우박수열 정적분, Time: 14.46 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -1,35 +1,44 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
-    public static double[] solution(int k, int[][] ranges) {
-        double[] answer = new double[ranges.length];
 
-        int n = 0;
-        List<Integer> point = new ArrayList<>();
-        while(k > 1) {
-            point.add(k);
-            if(k % 2 == 0) {
+    public static double[] solution(int k, int[][] ranges) {
+
+        List<Double> areas = new ArrayList<>();
+        int init = k;
+
+        while (k > 1) {
+            if (k % 2 == 0) {
                 k /= 2;
-            }else {
+            } else {
                 k = k * 3 + 1;
             }
-            n++;
-        }
-        point.add(k);
 
+            double area = (double) (k + init) / 2;
+            areas.add(area);
+            init = k;
+        }
+
+        int n = areas.size();
+        double[] answer = new double[ranges.length];
 
         for (int i = 0; i < ranges.length; i++) {
-            int start = ranges[i][0];
-            int end = n + ranges[i][1];
-            System.out.println(start + ", " + end);
-            if(end < start) {
-                answer[i] = -1;
-            } else {
-                double sum = 0;
+            int[] range = ranges[i];
+
+            int start = range[0];
+            int end = 0;
+            if(range[1] <= 0) {
+                end = n + range[1];
+            }
+
+            if(start < end) {
                 for (int j = start; j < end; j++) {
-                    sum += (double)(point.get(j) + point.get(j + 1)) / 2;
+                    answer[i] += areas.get(j);
                 }
-                answer[i] = sum;
+            }else if(start > end){
+                answer[i] = -1;
             }
         }
         return answer;
