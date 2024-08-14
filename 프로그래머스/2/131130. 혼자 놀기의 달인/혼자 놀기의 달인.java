b619f82b0611f577542413b56e67e45104b4425c
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-08-14 15:38:46 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.13 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,60 +1,36 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class Solution {
 
-    public static int[] parent;
+    public static boolean[] visited;
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
 
     public static int solution(int[] cards) {
-        parent = new int[cards.length + 1];
-        for (int i = 0; i <= cards.length; i++) {
-            parent[i] = i;
-        }
+        int answer = 0;
+        visited = new boolean[cards.length];
 
         for (int i = 0; i < cards.length; i++) {
-            int a = cards[i];
-
-            if(find(i+1) != find(a)) {
-                union(i+1, a);
+            if(!visited[i]) {
+                dfs(i, 0, cards);
             }
         }
-
-        for (int i = 0; i < parent.length; i++) {
-            find(i);
-        }
-
-        Map<Integer, Integer> map = new LinkedHashMap<>();
-        for (int i = 1; i < parent.length; i++) {
-            map.put(parent[i], map.getOrDefault(parent[i], 0) + 1);
-        }
-
-        if(map.size() == 1) {
+        
+        if(pq.size() == 1){
             return 0;
         }
-
-        int[] nums = map.values().stream().mapToInt(Integer::intValue).sorted().toArray();
-        return nums[nums.length-2] * nums[nums.length-1];
+        return pq.poll() * pq.poll();
     }
 
-    public static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
+    public static void dfs(int idx, int len, int[] cards) {
 
-        if (a < b) {
-            parent[b] = a;
-        } else {
-            parent[a] = b;
-        }
-    }
-
-    public static int find(int x) {
-        if (x != parent[x]) {
-            parent[x] = find(parent[x]);
+        if(visited[idx]) {
+            pq.add(len);
+            return;
         }
 
-        return parent[x];
+        visited[idx] = true;
+        dfs(cards[idx] - 1, len + 1, cards);
     }
 
 }
