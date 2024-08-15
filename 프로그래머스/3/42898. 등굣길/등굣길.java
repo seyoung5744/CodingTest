c49aec489a1b401be50cefb23a9de52ed8e3b7c6
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-08-15 20:17:17 +0900

    [level 3] Title: 등굣길, Time: 1.08 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,5 +1,8 @@
 class Solution {
-     public static int solution(int m, int n, int[][] puddles) {
+     
+    private static final int INF = 1_000_000_007;
+    
+    public static int solution(int m, int n, int[][] puddles) {
         int[][] map = new int[n + 1][m + 1];
 
         for (int[] puddle : puddles) {
@@ -11,10 +14,12 @@ class Solution {
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
                 if(i == 1 && j == 1) continue;
+
                 if (map[i][j] != -1)
-                    map[i][j] = Math.max(map[i - 1][j], 0) % 1_000_000_007+ Math.max(map[i][j - 1], 0) % 1_000_000_007;
+                    map[i][j] = Math.max(map[i - 1][j], 0) % INF+ Math.max(map[i][j - 1], 0) % INF;
             }
         }
-        return map[n][m] % 1_000_000_007;
+
+        return map[n][m] % INF;
     }
 }
