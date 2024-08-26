07d38a6c67c6811346336d26e7d11de9f663a413
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-08-26 21:08:35 +0900

    [level 3] Title: 징검다리 건너기, Time: 3.61 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -9,39 +9,29 @@ int solution(vector<int> stones, int k) {
     int max = *max_element(stones.begin(), stones.end());
     int min = 1;
     
-    while(min <= max)
-    {
+    while(min <= max) {
         int mid = (min + max) / 2;
         
         int count = 1;
-        for(int i = 0; i < stones.size(); ++i)
-        {
-            if (stones[i] - mid < 0)
-            {
+        for(int i = 0; i < stones.size(); ++i) {
+            if (stones[i] - mid < 0) {
                 ++count;
-            }
-            else
-            {
+            } else {
                 count = 1;
             }
             
-            if (count > k)
-            {
+            if (count > k) {
                 break;
             }
         }
         
-        if (count > k)
-        {
+        if (count > k) {
             max = mid -1;
-        }
-        else
-        {
+        } else {
             answer = mid;
             min = mid + 1;
         }
     }
-    
     
     return answer;
 }
