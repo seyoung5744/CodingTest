efb1f05e6d8e86f83da959fa900950b1342553ec
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-08-26 21:10:02 +0900

    [level 3] Title: 섬 연결하기, Time: 0.63 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution {
 
@@ -14,46 +14,29 @@ public class Solution {
 
         Arrays.sort(costs, (a, b) -> a[2] - b[2]);
 
-        for(int[] c : costs) {
+        for (int[] c : costs) {
             int a = c[0];
             int b = c[1];
             int cost = c[2];
 
-           if (find(a) != find(b)) {
+            if (find(a) != find(b)) {
                 union(a, b);
                 answer += cost;
             }
-
-            int root = parents[0];
-            boolean flag = true;
-            for (int i = 1; i < parents.length; i++) {
-                if(root != parents[i]) {
-                    flag = false;
-                    break;
-                }
-            }
-
-            if(flag) {
-                return answer;
-            }
-
         }
         return answer;
     }
 
 
     public static void union(int a, int b) {
-        int aParent = find(a);
-        int bParent = find(b);
+        a = find(a);
+        b = find(b);
 
-        if (aParent < bParent) {
-            parents[bParent] = aParent;
-
+        if (a < b) {
+            parents[b] = a;
         } else {
-            parents[aParent] = bParent;
+            parents[a] = b;
         }
-        find(b);
-        find(a);
     }
 
     public static int find(int x) {
