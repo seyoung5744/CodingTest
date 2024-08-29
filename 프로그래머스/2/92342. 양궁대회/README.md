2dcf373f406174e820dc3f2280dadb823e2ab290
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2024-08-29 15:17:46 +0900

    [level 2] Title: 양궁대회, Time: 4.21 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/README.md
+++ 프로그래머스/2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342#qna) 
 
 ### 성능 요약
 
-메모리: 73.2 MB, 시간: 0.88 ms
+메모리: 66.1 MB, 시간: 4.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 21일 20:27:51
+2024년 08월 29일 15:17:42
 
 ### 문제 설명
 
--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -4,66 +4,95 @@ import java.util.List;
 
 public class Solution {
 
-    public static int[] result = new int[]{-1};
-    public static int max = 0;
+    public static List<int[]> results = new ArrayList<>();
+    public static final int[] SCORES = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
     public static int[] solution(int n, int[] info) {
-        dfs(0, 0, n, new int[11], info);
-        return result;
-    }
+        int[] answer = new int[11];
 
-    public static void dfs(int used, int curIdx, int n, int[] candidate, int[] info) {
-        if (curIdx == info.length) {
+        shoot(n, new int[11], 0, info);
 
-            if (used < n) {
-                candidate[curIdx - 1] += (n - used);
+        if (results.isEmpty()) {
+            return new int[]{-1};
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int[] candidate : results) {
+            int apachScore = 0;
+            int lionScore = 0;
+            for (int i = 0; i < candidate.length; i++) {
+                if (candidate[i] > info[i]) {
+                    lionScore += SCORES[i];
+                } else {
+                    if (info[i] != 0) {
+                        apachScore += SCORES[i];
+                    }
+                }
             }
 
-            int compare = compare(candidate, info);
-
-            if (compare > max) {
-                result = Arrays.copyOf(candidate, candidate.length);
-                max = compare;
-            } else if (compare == max) {
-                if (compare == 0) {
-                    return;
-                }
-
-                for (int i = 10; i >= 0; --i) {
-                    if (result[i] < candidate[i]) {
-                        result = Arrays.copyOf(candidate, candidate.length);
+            if (lionScore - apachScore > max) {
+                max = Math.max(max, lionScore - apachScore);
+                answer = candidate;
+            } else if (lionScore - apachScore == max) {
+                                for (int i = info.length - 1; i >= 0; i--) {
+                    if(answer[i] == candidate[i])
+                        continue;
+                    if (answer[i] < candidate[i]) {
+                        answer = candidate;
                         break;
-                    } else if (result[i] > candidate[i]) {
+                    }else {
                         break;
                     }
                 }
             }
 
+
+        }
+        return answer;
+    }
+
+    public static void shoot(int n, int[] candidate, int target, int[] info) {
+        if (n <= 0) { // 화살 다 쏘면 결과 저장?
+            int apachScore = 0;
+            int lionScore = 0;
+            for (int i = 0; i < candidate.length; i++) {
+                if (candidate[i] > info[i]) {
+                    lionScore += SCORES[i];
+                } else {
+                    if (info[i] != 0) {
+                        apachScore += SCORES[i];
+                    }
+                }
+            }
+            if (lionScore > apachScore) {
+                results.add(Arrays.copyOf(candidate, candidate.length));
+            }
             return;
         }
 
-        if (n - used >= info[curIdx] + 1) {
-            candidate[curIdx] = info[curIdx] + 1;
-            dfs(used + info[curIdx] + 1, curIdx + 1, n, Arrays.copyOf(candidate, candidate.length), info);
-            candidate[curIdx] = 0;
+        if (target >= info.length) {
+            return;
         }
 
-        dfs(used, curIdx + 1, n, Arrays.copyOf(candidate, candidate.length), info);
-    }
+        if (n > info[target]) { // 남은 화살 수가 해당 target 보다 크면
+            // 해당 점수 얻어보고
+            n -= info[target] + 1;
+            candidate[target] = info[target] + 1;
+            shoot(n, candidate, target + 1, info);
 
-    public static int compare(int[] candidate, int[] info) {
+            // 해당 점수 빼보고
+            n += info[target] + 1;
+            candidate[target] = 0;
+            shoot(n, candidate, target + 1, info);
+        } else { // 남은 화살 수가 target 보다 작거나 같으면
+            // 다음 target으로
+            candidate[target] = n;
+            n -= n;
+            shoot(n, candidate, target + 1, info);
 
-        int lion = 0;
-        int apeach = 0;
-
-        for (int i = 0; i < 11; i++) {
-            if (candidate[i] > info[i]) {
-                lion += 10 - i;
-            } else if (info[i] != 0) {
-                apeach += 10 - i;
-            }
+            n += candidate[target];
+            candidate[target] = 0;
+            shoot(n, candidate, target + 1, info);
         }
-
-        return lion - apeach;
     }
 }
