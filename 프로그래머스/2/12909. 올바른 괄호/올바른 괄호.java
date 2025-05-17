84ef374f9805b2396287ffe5d40eb40207d9068d
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-05-17 12:05:22 +0900

    [level 2] Title: 올바른 괄호, Time: 17.36 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,26 +2,16 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-
         Deque<Character> stack = new ArrayDeque<>();
         
         for (char p : s.toCharArray()) {
             if (p == '(') {
                 stack.push(p);
             } else {
-                if(stack.isEmpty()) {
-                    answer = false;
-                    break;
-                } else {
-                    char top = stack.pop();
-                    if (top == ')') {
-                        answer = false;
-                        break;
-                    }
-                }
+                if (stack.isEmpty()) return false;
+                stack.pop();
             }
         }
-        return stack.isEmpty() && answer;
+        return stack.isEmpty();
     }
 }
