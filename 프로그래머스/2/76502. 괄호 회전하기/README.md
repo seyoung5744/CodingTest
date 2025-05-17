31311387d5e6cec4efd66dc7a1302a03be24a095
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-05-17 12:33:42 +0900

    [level 2] Title: 괄호 회전하기, Time: 72.13 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
-메모리: 79.1 MB, 시간: 0.47 ms
+메모리: 83.9 MB, 시간: 72.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 05일 15:33:08
+2025년 05월 17일 12:33:25
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,50 +1,41 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Map;
+import java.util.*;
 
-public class Solution {
-
-    public static Map<Character, Character> parenthesis = Map.of('(', ')', '{', '}', '[', ']');
-
-    public static int solution(String s) {
+class Solution {
+    private static final Map<Character, Character> perenthesis = Map.of(
+        ')','(',
+        ']','[',
+        '}','{'
+    );
+    
+    public int solution(String s) {
         int answer = 0;
         Deque<Character> queue = new ArrayDeque<>();
-        for (char c : s.toCharArray()) {
-            queue.add(c);
-        }
-        
         for (int i = 0; i < s.length(); i++) {
-            if(isValid(queue)) {
+            queue.offer(s.charAt(i));
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (isValid(new ArrayDeque<>(queue))) {
                 answer++;
             }
-            
-            queue.add(queue.poll());
+            queue.offer(queue.poll());
         }
+        
         return answer;
     }
-
-    public static boolean isValid(Deque<Character> s) {
+    
+    private boolean isValid(Deque<Character> queue) {
         Deque<Character> stack = new ArrayDeque<>();
-        Deque<Character> queue = new ArrayDeque<>(s);
         
         while(!queue.isEmpty()) {
-            char ch = queue.poll();
-            if (parenthesis.containsKey(ch)) {
-                stack.push(ch);
+            char c = queue.poll();
+            if (!perenthesis.containsKey(c)) {
+                stack.push(c);
             } else {
-                if (stack.isEmpty()) {
-                    return false;
-                } else {
-                    if (parenthesis.get(stack.peek()) != ch) {
-                        return false;
-                    } else {
-                        stack.pop();
-                    }
-                }
+                if (stack.isEmpty()) return false;
+                if (perenthesis.get(c) == stack.peek()) stack.pop();
             }
         }
-
+        
         return stack.isEmpty();
     }
-
 }
