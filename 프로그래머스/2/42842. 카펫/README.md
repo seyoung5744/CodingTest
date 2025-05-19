afecea47419cb1d08833b35ff1d0932a87167ec6
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-05-19 17:56:29 +0900

    [level 2] Title: 카펫, Time: 8.84 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.01 ms
+메모리: 72.6 MB, 시간: 8.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 20일 19:32:26
+2025년 05월 19일 17:56:11
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,13 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = {};
-
-        for (int weight = 3; weight < 5000; weight++) {
-            for (int height = 3; height <= weight; height++) {
-                int boundary = 2 * (height + weight - 2);
-                int center = (weight * height) - boundary;
-
-                if (brown == boundary && yellow == center) {
-                    return new int[]{weight, height};
+        for (int width = 3; width < 5000; width++) {
+            for (int height = 3; height <= width; height++) {
+                if ((2 * (width + height - 2) == brown && ((height - 2) * (width - 2)) == yellow)) {
+                    return new int[]{width, height};
                 }
             }
         }
-
-        return answer;
+        return new int[]{-1, -1};
     }
 }
