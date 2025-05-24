3fea0294b72c8bcfab40cdf5810464433edef93e
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-05-24 13:18:48 +0900

    [level 3] Title: 순위, Time: 22.92 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/README.md
+++ 프로그래머스/3/49191. 순위/README.md
@@ -1,10 +1,10 @@
 # [level 3] 순위 - 49191 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191) 
 
 ### 성능 요약
 
-메모리: 83.9 MB, 시간: 36.25 ms
+메모리: 77.1 MB, 시간: 22.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 15일 14:45:00
+2025년 05월 24일 13:18:30
 
 ### 문제 설명
 
--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,61 +1,56 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
-public class Solution {
-
-    public static int solution(int n, int[][] results) {
+class Solution {
+    public int solution(int n, int[][] results) {
         int answer = 0;
         List<List<Integer>> winGraph = new ArrayList<>();
         List<List<Integer>> loseGraph = new ArrayList<>();
-        
-        for (int i = 0; i < n + 1; i++) {
+
+        for (int i = 0; i <= n; i++) {
             winGraph.add(new ArrayList<>());
             loseGraph.add(new ArrayList<>());
         }
 
-        for (int[] result : results) {
-            int win = result[0];
-            int lose = result[1];
+        for (int[] edge : results) {
+            int win = edge[0];
+            int lose = edge[1];
+
             winGraph.get(lose).add(win);
             loseGraph.get(win).add(lose);
         }
 
-        for (int start = 1; start < n + 1; start++) {
-            int win = countPerson(start, winGraph, new boolean[n + 1]);
-            int lose = countPerson(start, loseGraph, new boolean[n + 1]);
-            if (win + lose == n - 1) {
+        for (int i = 1; i <= n; i++) {
+            int winCount = getCount(winGraph, i);
+            int loseCount = getCount(loseGraph, i);
+            if (winCount + loseCount == n - 1) {
                 answer++;
             }
         }
-
         return answer;
     }
 
-    public static int countPerson(int start, List<List<Integer>> graph, boolean[] visited) {
-        int result = 0;
-
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
+    public int getCount(List<List<Integer>> graph, int start) {
+        boolean[] visited = new boolean[graph.size()];
+        Deque<Integer> q = new ArrayDeque<>();
+        q.add(start);
         visited[start] = true;
 
+        int count = 0;
         while (!q.isEmpty()) {
             int cur = q.poll();
 
             for (int i = 0; i < graph.get(cur).size(); i++) {
                 int next = graph.get(cur).get(i);
 
-                if (visited[next]) {
-                    continue;
-                }
+                if (visited[next]) continue;
 
-                result += 1;
-                q.offer(next);
+                count++;
                 visited[next] = true;
+                q.add(next);
             }
         }
 
-        return result;
+        return count;
     }
+
 }
