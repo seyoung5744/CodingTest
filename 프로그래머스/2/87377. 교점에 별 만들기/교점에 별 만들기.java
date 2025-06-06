8e9b64b3bc07ace69df4169a582f301a7c38a0e1
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-06-06 14:51:49 +0900

    [level 2] Title: 교점에 별 만들기, Time: 43.18 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,62 +1,51 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class Solution {
-
-    public static String[] solution(int[][] line) {
-
+import java.util.*;
+class Solution {
+    public String[] solution(int[][] line) {
         long minX = Long.MAX_VALUE, maxX = Long.MIN_VALUE;
         long minY = Long.MAX_VALUE, maxY = Long.MIN_VALUE;
+
         List<long[]> points = new ArrayList<>();
         for (int i = 0; i < line.length; i++) {
             for (int j = i + 1; j < line.length; j++) {
-                if (isParallel(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2])) {
-                    continue;
-                }
                 double x = getX(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
                 double y = getY(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
+                if (x % 1 != 0 || y % 1 != 0) continue;
 
-                if (isInteger(x) && isInteger(y)) {
-                    minX = Math.min((long) x, minX);
-                    maxX = Math.max((long) x, maxX);
-                    minY = Math.min((long) y, minY);
-                    maxY = Math.max((long) y, maxY);
-                    points.add(new long[]{(long) x, (long) y});
-                }
+                minX = Math.min(minX, (long) x);
+                maxX = Math.max(maxX, (long) x);
+                minY = Math.min(minY, (long) y);
+                maxY = Math.max(maxY, (long) y);
+                points.add(new long[]{(long) x, (long) y});
             }
         }
 
         int width = (int) (maxX - minX + 1);
         int height = (int) (maxY - minY + 1);
+        
         char[][] map = new char[height][width];
-        for (int i = 0; i < height; i++) {
-            Arrays.fill(map[i], '.');
+        for (char[] arr : map) {
+            Arrays.fill(arr, '.');
         }
 
         for (long[] point : points) {
-            map[(int) (maxY - point[1])][(int) (point[0] - minX)] = '*';
+            int x = (int) (point[0] - minX);
+            int y = (int) (maxY - point[1]);
+            map[y][x] = '*';
         }
+
         String[] answer = new String[height];
         for (int i = 0; i < height; i++) {
             answer[i] = new String(map[i]);
         }
+
         return answer;
     }
 
-    public static boolean isInteger(double num) {
-        return (long) num == num;
+    public double getX(long a, long b, long e, long c, long d, long f) {
+        return (double)((b * f) - (e * d)) / ((a * d) - (b * c));
     }
 
-    public static boolean isParallel(double A, double B, double E, double C, double D, double F) {
-        return (A * D - B * C) == 0;
-    }
-
-    public static double getX(double A, double B, double E, double C, double D, double F) {
-        return (B * F - E * D) / (A * D - B * C);
-    }
-
-    public static double getY(double A, double B, double E, double C, double D, double F) {
-        return (E * C - A * F) / (A * D - B * C);
+    public double getY(long a, long b, long e, long c, long d, long f) {
+        return (double)((e * c) - (a * f)) / ((a * d) - (b * c));
     }
 }
