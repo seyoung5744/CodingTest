e0c971dfc9102cd39f61dd1923b325ee6626a335
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-06-09 19:48:08 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 14.39 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 3.13 ms
+메모리: 86.6 MB, 시간: 14.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 05일 12:12:44
+2025년 06월 09일 19:47:45
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,20 +1,7 @@
 import java.util.*;
-
-public class Solution {
-
-    public static String solution(String s) {
-        int[] nums = Arrays.stream(s.split(" "))
-            .mapToInt(Integer::parseInt)
-            .toArray();
-
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            if(max < nums[i]) max = nums[i];
-            if(min > nums[i]) min = nums[i];
-        }
-
-        return String.format("%d %d", min, max);
+class Solution {
+    public String solution(String s) {
+        int[] arr = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+        return arr[0] + " " + arr[arr.length - 1];
     }
-
 }
