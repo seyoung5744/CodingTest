64fbdf2fd263d3e5f8462815b72aef586180958b
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-07 09:40:59 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            answer += digit;
+            n /= 10;
+        }
+        return answer;
+    }
+}
