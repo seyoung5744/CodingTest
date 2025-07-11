add135f24c85062be10c82363801a70884fb97be
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-11 10:26:05 +0900

    [level 1] Title: K번째수, Time: 0.54 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 
 class Solution {
@@ -8,16 +7,16 @@ class Solution {
         for (int i = 0; i < commands.length; i++) {
             int start = commands[i][0] - 1;
             int end = commands[i][1] - 1;
-            int target = commands[i][2] - 1;
-            
             int[] temp = new int[end - start + 1];
+            
             int idx = 0;
             for (int j = start; j <= end; j++) {
                 temp[idx++] = array[j];
             }
             Arrays.sort(temp);
-            answer[i] = temp[target];
+            answer[i] = temp[commands[i][2] - 1];
         }
+        
         return answer;
     }
 }
