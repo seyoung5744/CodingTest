0a626c8334fc1d61d719cce3fc9a9b229956f362
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-11 10:09:55 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 7.30 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,36 +1,13 @@
 import java.util.*;
 
 class Solution {
-    public static final Map<String, Integer> mapping = Map.of(
-        "zero", 0,
-        "one", 1,
-        "two", 2,
-        "three", 3,
-        "four", 4,
-        "five", 5,
-        "six", 6,
-        "seven", 7,
-        "eight", 8,
-        "nine", 9
-    );
+    public static final String[] alphabets = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     
     public int solution(String s) {
-        StringBuilder sb = new StringBuilder();
-        String temp = "";
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            
-            if (Character.isDigit(c)) {
-                sb.append(c);
-                continue;
-            }
-            temp += c;
-            
-            if (mapping.containsKey(temp)) {
-                sb.append(mapping.get(temp));
-                temp = "";
-            }
+        
+        for (int i = 0; i < 10; i++) {
+            s = s.replaceAll(alphabets[i], i + "");
         }
-        return Integer.parseInt(sb.toString());
+        return Integer.parseInt(s);
     }
 }
