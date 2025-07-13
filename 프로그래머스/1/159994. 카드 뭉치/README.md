00e0f9cd96466a58f4572f9da8dca41bbb7f4476
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-13 15:30:45 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994#qna) 
 
 ### 성능 요약
 
-메모리: 78.8 MB, 시간: 0.03 ms
+메모리: 69.8 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 2일 21:45:46
+2025년 07월 13일 15:30:40
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,19 +1,42 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        int idx1 = 0; 
-        int idx2 = 0; 
-        
-        for (String currentStr : goal) {
-            
-            if (idx1 < cards1.length && currentStr.equals(cards1[idx1])) {
-                idx1++; 
-            } else if (idx2 < cards2.length && currentStr.equals(cards2[idx2])) {
-                idx2++; 
+        int idx1 = 0;
+        int idx2 = 0;
+        int targetIdx = 0;
+
+        while (idx1 < cards1.length && idx2 < cards2.length) {
+            if (targetIdx < goal.length && goal[targetIdx].equals(cards1[idx1])) {
+                idx1++;
+                targetIdx++;
+            }
+            else if (targetIdx < goal.length && goal[targetIdx].equals(cards2[idx2])) {
+                idx2++;
+                targetIdx++;
             } else {
-                return "No"; 
+                idx2++;
+                targetIdx++;
             }
         }
-        
-        return "Yes";
+
+        while (idx1 < cards1.length && targetIdx < goal.length) {
+            if (goal[targetIdx].equals(cards1[idx1])) {
+                idx1++;
+                targetIdx++;
+            } else {
+                break;
+            }
+        }
+        while (idx2 < cards2.length && targetIdx < goal.length) {
+            if (goal[targetIdx].equals(cards2[idx2])) {
+                idx2++;
+                targetIdx++;
+            } else {
+                break;
+            }
+        }
+        if (idx1 == goal.length || idx2 == goal.length) return "Yes";
+        return idx1 + idx2 == goal.length ? "Yes" : "No";
     }
 }
