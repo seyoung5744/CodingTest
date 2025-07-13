53db5cd84cb3cc23abbd73970ec8ea009742c1ea
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-13 15:45:36 +0900

    [level 1] Title: 모의고사, Time: 6.87 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,40 +1,33 @@
-import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-    public static final int[][] people = {
+    
+    public static final int[][] students = {
         {1, 2, 3, 4, 5},
         {2, 1, 2, 3, 2, 4, 2, 5},
-        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}        
     };
     
     public int[] solution(int[] answers) {
-        int[][] scores = new int[3][2];
+        int[] corrects = new int[3];
+        int maxScore = -1;
         
-        for (int person = 0; person < 3; person++) {
-            int count = 0;
-            for (int i = 0; i < answers.length; i++) {
-                if (people[person][i % people[person].length] == answers[i])
-                    count++;
+        int idx = 0;
+        
+        for (int i = 0; i < students.length; i++) {
+            for (int answer : answers) {
+                if (answer == students[i][idx++ % students[i].length]) {
+                    corrects[i]++;
+                }
             }
-            scores[person][0] = person + 1;
-            scores[person][1] = count;
+            maxScore = Math.max(maxScore, corrects[i]);
+            idx = 0;
         }
         
-        Arrays.sort(scores, (a, b) -> {
-            if (a[1] == b[1]) {
-                return a[0] - b[0];
-            }
-            return b[1] - a[1];
-        });
-        
-        List<Integer> answer = new ArrayList<>();
-        int max = scores[0][1];
-        answer.add(scores[0][0]);
-        for (int i = 1; i < 3; i++) {
-            if (scores[i][1] == max) {
-                answer.add(scores[i][0]);
-            }
-        }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        int max = maxScore;
+        return IntStream.range(0, 3)
+            .filter(i ->  corrects[i] == max)
+            .map(x -> x + 1)
+            .toArray();
     }
 }
