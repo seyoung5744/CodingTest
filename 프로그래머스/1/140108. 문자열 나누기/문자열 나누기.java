653c3469f8869650369e52418e0ba67e61db4464
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-15 10:23:36 +0900

    [level 1] Title: 문자열 나누기, Time: 0.90 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,17 +1,36 @@
 class Solution {
     public int solution(String s) {
-        char first = s.charAt(0);
-		int firstNum = 0;
-		int diff = 0;
-		int result = 0;
-		for (int i = 0; i < s.length(); i++) {
-			if (firstNum == diff) {
-				result++;
-				first = s.charAt(i);
-			}
-			if (s.charAt(i) == first) firstNum++;
-			else diff++;
-		}
-		return result;
+        int answer = 0;
+
+        char xChar = 0;
+        int x = 0;
+        
+        int notX = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (xChar == 0) {
+                xChar = s.charAt(i);
+                x = 1;
+                continue;
+            }
+            
+            if (s.charAt(i) == xChar) {
+                x++;
+            } else {
+                notX++;
+            }
+            
+            if (x == notX) {
+                answer++;
+                xChar = 0;
+                x = 0;
+                notX = 0;
+            }
+        }
+        if (xChar != 0) {
+            answer++;
+        }
+        return answer;
     }
+    
 }
