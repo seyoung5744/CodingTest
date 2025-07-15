612471a8844ba56847f7d3ecf210f070ce1b9143
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-15 11:19:01 +0900

    [level 1] Title: 둘만의 암호, Time: 18.34 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652#qna) 
 
 ### 성능 요약
 
-메모리: 77.5 MB, 시간: 13.58 ms
+메모리: 73.7 MB, 시간: 18.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 22:22:12
+2025년 07월 15일 11:18:56
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,20 +1,26 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
-        
-        for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            int count = 1;
-            while(count <= index){
-                ++c;
-                if(c > 'z')
-                    c -= 26;
-                if(skip.contains(c+""))
+        Set<Integer> skipSet = new HashSet<>();
+
+        for (int i = 0; i < skip.length(); i++) {
+            skipSet.add(skip.charAt(i) - 'a');
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            int pos = s.charAt(i) - 'a';
+            int loop = index;
+
+            while (loop > 0) {
+                pos = (pos  + 1) % ('z' - 'a' + 1);
+                if (skipSet.contains(pos)) {
                     continue;
-                else
-                    count++;
+                }
+                loop--;
             }
-            answer += c;
+            answer += (char)(pos % ('z' - 'a' + 1) + 'a');
         }
         return answer;
     }
