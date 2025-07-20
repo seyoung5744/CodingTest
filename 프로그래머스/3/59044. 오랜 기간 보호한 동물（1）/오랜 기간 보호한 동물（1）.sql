2f21f62b40628ccb1892965c0b693969f59da17f
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-20 10:32:04 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -3,5 +3,5 @@ SELECT ai.name, ai.datetime
 from animal_ins ai
 left join animal_outs ao on ai.animal_id = ao.animal_id
 where ao.animal_id is null
-order by ai.datetime
-limit 3;
+order by ai.datetime asc
+limit 3
