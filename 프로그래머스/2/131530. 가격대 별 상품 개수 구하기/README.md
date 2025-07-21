a92d30077dc3801c3a67b2d6d2f42f94218d9b3c
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-21 10:58:33 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/README.md
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 가격대 별 상품 개수 구하기 - 131530 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131530) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131530#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 07월 21일 10:56:13
+2025년 07월 21일 10:58:27
 
 ### 문제 설명
 
--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,22 +1,27 @@
 -- 코드를 입력하세요
+# select price_group, count(*) as products
+# from (
+#     SELECT 
+#     case
+#         when price < 10000 then '0'
+#         when price < 20000 then '10000'
+#         when price < 30000 then '20000'
+#         when price < 40000 then '30000'
+#         when price < 50000 then '40000'
+#         when price < 60000 then '50000'
+#         when price < 70000 then '60000'
+#         when price < 80000 then '70000'
+#         when price < 90000 then '80000'
+#         else '90000'
+#     end as price_group
+#     from product
+# ) as p
+# group by price_group
+# order by price_group
 
-
-select price_group, count(*) as products
-from (
-    SELECT 
-    case
-        when price < 10000 then '0'
-        when price < 20000 then '10000'
-        when price < 30000 then '20000'
-        when price < 40000 then '30000'
-        when price < 50000 then '40000'
-        when price < 60000 then '50000'
-        when price < 70000 then '60000'
-        when price < 80000 then '70000'
-        when price < 90000 then '80000'
-        else '90000'
-    end as price_group
-    from product
-) as p
+select 
+    floor(price/10000) * 10000 as price_group,
+    count(*) as products
+from product
 group by price_group
 order by price_group
