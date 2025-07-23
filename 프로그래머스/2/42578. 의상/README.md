0ad8f1d97148113efe3420b69947bcd53b10673f
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-23 10:06:13 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.02 ms
+메모리: 81.1 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 05일 16:54:08
+2025년 07월 23일 10:06:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,16 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
+        Map<String, Integer> clothCount = new HashMap<>();
+        
+        for (String[] cloth : clothes) {
+            clothCount.put(cloth[1], clothCount.getOrDefault(cloth[1], 0) + 1);
+        }
+        
         int answer = 1;
-        Map<String, Integer> map = new HashMap<>();
-        
-        for(String[] cloth : clothes) {
-            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        List<Integer> count = new ArrayList<>(clothCount.values());
+        for (int c : count) {
+            answer *= (c + 1);
         }
         
-        for(int value : map.values()) {
-            answer *= (value + 1);
-        }
         return answer - 1;
     }
 }
