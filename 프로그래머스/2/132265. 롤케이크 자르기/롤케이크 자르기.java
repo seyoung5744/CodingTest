0073b084cd440c02e5d77d55cef499806c2f96aa
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-29 09:14:32 +0900

    [level 2] Title: 롤케이크 자르기, Time: 247.09 ms, Memory: 179 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,29 +1,28 @@
 import java.util.*;
 
-public class Solution {
-
+class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-
+        
         Map<Integer, Integer> person1 = new HashMap<>();
         for (int i = 0; i < topping.length; i++) {
             person1.put(topping[i], person1.getOrDefault(topping[i], 0) + 1);
         }
-
+        
         Map<Integer, Integer> person2 = new HashMap<>();
         for (int i = 0; i < topping.length; i++) {
             person2.put(topping[i], person2.getOrDefault(topping[i], 0) + 1);
-            person1.put(topping[i], person1.getOrDefault(topping[i], 0) - 1);
             
-            if(person1.get(topping[i]) == 0) {
+            int toppingCount = person1.get(topping[i]) - 1;
+            person1.put(topping[i], toppingCount);
+            if (toppingCount == 0) {
                 person1.remove(topping[i]);
             }
-
-            if(person2.keySet().size() == person1.keySet().size()) {
+            if (person1.size() == person2.size()) {
                 answer++;
             }
         }
-
+        
         return answer;
     }
 }
