665159dd5fc643dc1f4059c7856e2dd88df05a06
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-07-31 09:18:03 +0900

    [level 2] Title: 가장 큰 수, Time: 161.40 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,17 +3,13 @@ import java.util.stream.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] strings = Arrays.stream(numbers)
+        String result = Arrays.stream(numbers)
                 .mapToObj(String::valueOf)
-                .toArray(String[]::new);
-        String answer = Arrays.stream(strings)
                 .sorted((a, b) -> (b + a).compareTo(a + b))
                 .collect(Collectors.joining());
 
-        if (answer.charAt(0) == '0') {
-            return "0";
-        }
+        if (result.charAt(0) == '0') return "0";
 
-        return answer;
+        return result;
     }
 }
