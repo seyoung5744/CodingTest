fa4c46091443e149bfd21d9d10b8d4b65c03484a
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-02 12:28:21 +0900

    [level 2] Title: 삼각 달팽이, Time: 17.29 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -3,21 +3,28 @@ class Solution {
     public static final int[] dy = {1, 0, -1};
     
     public int[] solution(int n) {
-        int[][] triangle = new int[n][n];
-        int direction = 0;
-        int x = 0, y = 0;
+        int loop = 0;
+        for (int i = 1; i <= n; i++) {
+            loop += i;
+        }
+
+        int[] answer = new int[loop];
+        int[][] map = new int[n][n];
+
         int num = 1;
-
-        int loop = (n * (2 + (n - 1))) / 2;
-
+        int x = 0;
+        int y = 0;
+        int direction = 0;
         while (loop-- > 0) {
-            triangle[y][x] = num++;
+
+            map[y][x] = num++;
 
             int nx = x + dx[direction];
             int ny = y + dy[direction];
 
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n || triangle[ny][nx] != 0) {
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n || map[ny][nx] != 0) {
                 direction = (direction + 1) % 3;
+
                 nx = x + dx[direction];
                 ny = y + dy[direction];
             }
@@ -26,14 +33,14 @@ class Solution {
             y = ny;
         }
 
-        int[] answer = new int[(n * (2 + (n - 1))) / 2];
         int idx = 0;
-        for (int i = 0; i < triangle.length; i++) {
-            for (int j = 0; j <= i; j++) {
-                answer[idx++] = triangle[i][j];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(map[i][j] != 0) {
+                    answer[idx++] = map[i][j];
+                }
             }
         }
-
         return answer;
     }
 }
