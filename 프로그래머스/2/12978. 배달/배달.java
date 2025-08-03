1fa6e607f2a25f178474f54b4ba9eeafb987bb64
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-03 15:13:28 +0900

    [level 2] Title: 배달, Time: 8.85 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,49 +1,38 @@
 import java.util.Arrays;
 
-public class Solution {
+class Solution {
+    public int solution(int N, int[][] road, int K) {
+        int answer = 1;
 
-
-    public static int[][] graph;
-
-    public static int solution(int N, int[][] roads, int K) {
-        int answer = 0;
-
-        graph = new int[N + 1][N + 1];
-
-        for (int i = 0; i <= N; i++) {
-            Arrays.fill(graph[i], 1000000);
+        int[][] graph = new int[N + 1][N + 1];
+        for (int i = 0; i < graph.length; i++) {
+            Arrays.fill(graph[i], 500001);
         }
-
-        for (int[] road : roads) {
-            int a = road[0];
-            int b = road[1];
-            int cost = road[2];
-
-            if (cost > K)
-                continue;
+        
+        for (int[] r : road) {
+            int a = r[0];
+            int b = r[1];
+            int cost = r[2];
 
             graph[a][b] = Math.min(graph[a][b], cost);
             graph[b][a] = Math.min(graph[b][a], cost);
         }
 
+
         for (int k = 1; k <= N; k++) {
-            for (int a = 1; a <= N; a++) {
-                for (int b = 1; b <= N; b++) {
-                    if (a == b)
-                        graph[a][b] = 0;
-                    
-                    graph[a][b] = Math.min(graph[a][b], graph[a][k] + graph[k][b]);
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
                 }
             }
         }
+        
 
-
-        for (int a = 1; a <= N; a++) {
-            if (graph[1][a] <= K) {
+        for (int i = 2; i <= N; i++) {
+            if (graph[1][i] <= K) {
                 answer++;
             }
         }
         return answer;
     }
-
 }
