87dfc01601dec42e31df42d1549985457f672fd3
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-05 10:35:20 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.12 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,63 +1,67 @@
-import java.util.Arrays;
+class Solution {
+    private static final int[] dx = {0, -1, 1, 0};
+    private static final int[] dy = {-1, 0, 0, 1}; // 상, 좌, 우, 하
 
-public class Solution {
-
-    public static int[] dx = {0, 1, -1, 0}; // 하, 우, 좌, 상
-    public static int[] dy = {1, 0, 0, -1};
-
-    public static int[] solution(String[][] places) {
+    public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
 
         for (int i = 0; i < places.length; i++) {
-            if(validation(places[i])) {
+            char[][] room = new char[5][5];
+            for (int j = 0; j < places[i].length; j++) {
+                room[j] = places[i][j].toCharArray();
+            }
+
+            if (isDistanced(room)) {
                 answer[i] = 1;
             }
         }
         return answer;
     }
 
-    public static boolean validation(String[] place) {
-        for (int y = 0; y < place.length; y++) {
-            for (int x = 0; x < place[y].length(); x++) {
+    private boolean isDistanced(char[][] room) {
 
-                if (place[y].charAt(x) != 'P') {
-                    continue;
-                }
+        for (int i = 0; i < room.length; i++) {
+            for (int j = 0; j < room[0].length; j++) {
+                if (room[i][j] != 'P') continue;
 
-                for (int i = 0; i < 4; i++) {
-                    int nx = dx[i] + x;
-                    int ny = dy[i] + y;
-
-                    if (nx < 0 || nx >= place.length || ny < 0 || ny >= place[y].length()) {
-                        continue;
-                    }
-
-                    if(place[ny].charAt(nx) == 'P') {
-                        return false;
-                    }
-                    
-                    if (place[ny].charAt(nx) != 'O') {
-                        continue;
-                    }
-                    
-                    for (int j = 0; j < 4; j++) {
-                        if (j != 3 - i) {
-                            int nnx = dx[j] + nx;
-                            int nny = dy[j] + ny;
-
-                            if (nnx < 0 || nnx >= place.length || nny < 0 || nny >= place[y].length()) {
-                                continue;
-                            }
-
-                            if (place[nny].charAt(nnx) == 'P') {
-                                return false;
-                            }
-                        }
-                    }
+                if (!isDistanced(room, j, i)) {
+                    return false;
                 }
             }
         }
         return true;
     }
 
+    private boolean isDistanced(char[][] room, int x, int y) {
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= room[0].length || ny < 0 || ny >= room.length) continue;
+
+            if (room[ny][nx] == 'P') return false;
+            if (room[ny][nx] == 'O') {
+                if (isNextToVolunteer(room, nx, ny, 3 - i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isNextToVolunteer(char[][] room, int x, int y, int exclude) {
+
+        for (int i = 0; i < 4; i++) {
+            if (i == exclude) continue;
+
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= room[0].length || ny < 0 || ny >= room.length) continue;
+
+            if (room[ny][nx] == 'P') return true;
+        }
+        return false;
+    }
 }
