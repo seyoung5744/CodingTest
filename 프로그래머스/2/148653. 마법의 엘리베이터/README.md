5a33aedd671563d36c1b81cdad6bdf1669f0eaff
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-05 09:32:03 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#qna) 
 
 ### 성능 요약
 
-메모리: 69.5 MB, 시간: 0.02 ms
+메모리: 90.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 01일 15:05:04
+2025년 08월 05일 09:31:58
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -2,32 +2,28 @@ class Solution {
     public int solution(int storey) {
         int answer = 0;
         
-        while(storey != 0) {
-            int one = storey % 10;
-            storey /= 10;
-            
-            if(one < 5) {
-                answer += one;
+        while (storey > 0) {
+
+            int remain = storey % 10;
+
+            if (remain < 5) {
+                answer += remain;
+                storey /= 10;
+            } else {
+                answer += 10 - remain;
+                storey /= 10;
                 
-            }else if(one == 5) {
-                int ten = storey % 10;
-                
-                if (ten < 5)
-                {
-                    answer += one;
+                if (remain == 5) {
+                    if (storey % 10 >= 5) {
+                        storey += 1;    
+                    }
+                } else {
+                     storey += 1;  
                 }
-                else
-                {
-                     answer += 10 - one;
-                    ++storey;
-                }
                 
-            }else {
-                answer += 10 - one;
-                ++storey;
+                
             }
         }
-        
         return answer;
     }
 }
