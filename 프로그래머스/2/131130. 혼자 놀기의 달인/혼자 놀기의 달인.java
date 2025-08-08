1bc48594414ea61904441705fb3d8050bd173384
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-08 09:49:30 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.35 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -4,29 +4,28 @@ class Solution {
      public int solution(int[] cards) {
         boolean[] opened = new boolean[cards.length];
 
-        List<List<Integer>> cardSet = new ArrayList<>();
+        List<Integer> cardSetSizeList = new ArrayList<>();
         for (int i = 0; i < cards.length; i++) {
             if (opened[i]) continue;
 
-            List<Integer> openedBoxes = inOrderOpen(i, cards, opened);
-            cardSet.add(openedBoxes);
+            int size = inOrderOpen(i, cards, opened);
+            cardSetSizeList.add(size);
         }
 
-        Collections.sort(cardSet, (a, b) -> Integer.compare(a.size(), b.size()));
-        int size = cardSet.size();
+        Collections.sort(cardSetSizeList);
+        int size = cardSetSizeList.size();
         if (size < 2) {
             return 0;
         }
-        return cardSet.get(size - 1).size() * cardSet.get(size - 2).size();
+        return cardSetSizeList.get(size - 1) * cardSetSizeList.get(size - 2);
     }
 
-    private List<Integer> inOrderOpen(int num, int[] cards, boolean[] opened) {
-        List<Integer> openedBox = new ArrayList<>();
-        openedBox.add(num);
-
+    private int inOrderOpen(int num, int[] cards, boolean[] opened) {
         Deque<Integer> q = new ArrayDeque<>();
         q.offer(num);
         opened[num] = true;
+
+        int size = 1;
 
         while (!q.isEmpty()) {
             int cur = q.poll();
@@ -36,9 +35,9 @@ class Solution {
 
             opened[next] = true;
             q.offer(next);
-            openedBox.add(next);
+            size++;
         }
 
-        return openedBox;
+        return size;
     }
 }
