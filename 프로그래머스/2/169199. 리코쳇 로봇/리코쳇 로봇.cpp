c0f16425be766e3e48e4cd1e0a30d8110badf3b0
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-10 15:41:37 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.22 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.cpp
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.cpp
@@ -3,82 +3,75 @@
 #include <queue>
 using namespace std;
 
-int dx[4] = {0, 0, 1, -1};
-int dy[4] = {1, -1, 0, 0};
+vector<int> dx = {1, -1, 0, 0};
+vector<int> dy = {0, 0, 1, -1};
+
 
 struct Step
 {
-    int x;
-    int y;
+    int X;
+    int Y;
     
-    Step(int x, int y) : x(x), y(y){}
+    int StepCount;
+    
+    Step(int x, int y, int sc) : X(x), Y(y), StepCount(sc){};
 };
+
+pair<int, int> GetPoint(vector<string>& board, char target)
+{
+    for(int i = 0; i < board.size(); ++i)
+    {
+        for(int j = 0; j < board[i].size(); ++j)
+        {
+            if (board[i][j] == target)
+                return {j, i};
+        }
+    }
+    
+    return {-1, -1};
+}
 
 int solution(vector<string> board) {
     int answer = 0;
     
-    int startX, startY;
-    int endX, endY;
-    vector<vector<int>> visited (board.size(), vector<int>(board[0].length(), 10000000));
+    auto [StartX, StartY] = GetPoint(board, 'R');
+    auto [EndX, EndY] = GetPoint(board, 'G');
     
-    for(int i = 0; i < board.size(); ++i)
-    {
-        for(int j = 0; j < board[i].length(); ++j)
-        {
-            if (board[i][j] == 'R')
-            {
-                startX = j;
-                startY = i;
-            }
-            else if (board[i][j] == 'G')
-            {
-                endX = j;
-                endY = i;
-            }
-        }
-    }
+    vector<vector<int>> visited(board.size(), vector<int>(board[0].size(), -1));
+    visited[StartY][StartX] = 0;
     
     queue<Step*> q;
-    q.push(new Step(startX, startY));
-    visited[startY][startX] = 0;
+    q.push(new Step(StartX, StartY, 0));
     
     while(!q.empty())
     {
         Step* cur = q.front();
         q.pop();
         
-        if(cur->x == endX && cur->y == endY)
-        {
+        if (cur->X == EndX && cur->Y == EndY)
             break;
-        }
         
-        for(int i = 0; i < 4; ++i)
+        for(int dir = 0; dir < 4; ++dir)
         {
-            int nx = cur->x;
-            int ny = cur->y;
+            int nx = cur->X;
+            int ny = cur->Y;
             
-            while(true)
+            while(nx + dx[dir] >= 0 && nx + dx[dir] < board[0].size() &&
+                  ny + dy[dir] >= 0 && ny + dy[dir] < board.size() &&
+                  board[ny + dy[dir]][nx + dx[dir]] != 'D')
             {
-                if (nx + dx[i] < 0 || nx + dx[i] >= board[i].length() ||
-                   ny + dy[i] < 0 || ny + dy[i] >= board.size())
-                    break;
-                
-                if (board[ny + dy[i]][nx + dx[i]] == 'D')
-                    break;
-                
-                nx += dx[i];
-                ny += dy[i];
+                nx = nx + dx[dir];
+                ny = ny + dy[dir];
             }
             
-            if(visited[ny][nx] <= visited[cur->y][cur->x] + 1)
-                continue;
-            
-            visited[ny][nx] = visited[cur->y][cur->x] + 1;
-            q.push(new Step(nx, ny));
+            if (visited[ny][nx] == -1 || visited[ny][nx] > cur->StepCount + 1)
+            {
+                visited[ny][nx] = cur->StepCount + 1;
+                q.push(new Step(nx, ny, cur->StepCount + 1));
+            }
         }
     }
     
-    answer = visited[endY][endX];
     
-    return answer == 10000000 ? -1 : answer;
+    return visited[EndY][EndX];
 }
