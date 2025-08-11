ac5739d16e6f68f1084d0c3a4cce68ae389e6a82
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-11 10:21:05 +0900

    [Silver IV] Title: 괄호, Time: 72 ms, Memory: 11756 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,43 +1,48 @@
-import java.io.*;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int T = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        
-        for (int i = 0; i < n; i++) {
-            Stack<Character> stack = new Stack<>();
-            boolean flag = true;
-            String s = br.readLine();
-            if (s.length() < 2 || s.length() > 50) {
-                flag = false;
+        while (T-- > 0) {
+            String parenthesis = br.readLine();
+
+            if (isValid(parenthesis)) {
+                System.out.println("YES");
             } else {
-                for (char ch : s.toCharArray()) {
-                    if (ch == '(') {
-                        stack.push(ch);
-                    } else if (ch == ')') {
-                        if (!stack.isEmpty()) {
-                            stack.pop();
-                        } else {
-                            flag = false;
-                            break;
-                        }
+                System.out.println("NO");
+            }
+        }
+
+
+        br.close();
+    }
+
+    private static boolean isValid(String parenthesis) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : parenthesis.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else {
+                if (stack.peek() == ')') {
+                    return false;
+                } else {
+                    if (c == '(') {
+                        stack.push(c);
+                    } else {
+                        stack.pop();
                     }
                 }
             }
-
-            if (flag && stack.isEmpty()) {
-                bw.write("YES" + "\n");
-            } else {
-                bw.write("NO" + "\n");
-            }
         }
-        bw.flush();
-        br.close();
-        bw.close();
+        return stack.isEmpty();
     }
 }
