3d333984e591ead2b785eb30bd8c9f30f86e5c31
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-12 11:02:17 +0900

    [Silver II] Title: 스택 수열, Time: 292 ms, Memory: 26316 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,50 +1,45 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int idx = 1;
-        Stack<Integer> stack = new Stack<>();
-        ArrayList<String> answer = new ArrayList<>();
+        int[] targets = new int[n];
+        for (int i = 0; i < n; i++) {
+            targets[i] = Integer.parseInt(br.readLine());
+        }
+        Deque<Integer> stack = new ArrayDeque<>();
+        int cur = 1;
+        boolean flag = true;
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i <= n; i++) {
-            int num = Integer.parseInt(br.readLine());
-
-            if (!stack.contains(num)) {
-                for (int j = idx; j <= num; j++) {
-                    stack.push(j);
-                    answer.add("+");
-                }
-
-                idx = num + 1;
+        for (int target : targets) {
+            while (cur <= target) {
+                stack.push(cur++);
+                sb.append("+").append("\n");
             }
 
-            if (stack.contains(num)) {
-                if (stack.peek() == num) {
-                    stack.pop();
-                    answer.add("-");
-                } else {
-                    answer.clear();
-                    answer.add("NO");
-                    break;
-                }
+            if (stack.peek() == target) {
+                stack.pop();
+                sb.append("-").append("\n");
+            } else {
+                flag = false;
+                break;
             }
-
         }
 
-        for (String an : answer) {
-            bw.write(an + "\n");
+        if (flag) {
+            System.out.println(sb);
+        } else {
+            System.out.println("NO");
         }
 
-        bw.flush();
         br.close();
-        bw.close();
     }
 }
