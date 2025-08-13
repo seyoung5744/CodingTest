c3d25f34769358dffd5032e9e056e453866c05d4
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-13 10:32:17 +0900

    [Silver II] Title: 최소 힙, Time: 1124 ms, Memory: 32496 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -9,21 +9,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        while(N-- > 0) {
+        while (N-- > 0) {
             int num = Integer.parseInt(br.readLine());
-
-            if (pq.isEmpty() && num == 0) {
-                System.out.println(0);
-            }else if(num == 0) {
-                System.out.println(pq.poll());
-            }else {
-                pq.add(num);
+            if (num == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            } else {
+                pq.offer(num);
             }
-
         }
+        br.close();
     }
-
 }
