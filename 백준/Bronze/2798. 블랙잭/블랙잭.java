d75d92f93404eb575b617290b64ec5d13883166b
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-15 11:17:19 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14016 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,37 +1,38 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] NM = br.readLine().split(" ");
-        int N = Integer.parseInt(NM[0]);
-        int M = Integer.parseInt(NM[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int targer = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
 
         int[] nums = new int[N];
-        String[] numsStr = br.readLine().split(" ");
-
         for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(numsStr[i]);
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int num = nums[i] + nums[j] + nums[k];
-                    if (num <= M) {
-                        max = Math.max(num, max);
+        int max = -1;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum <= targer) {
+                        max = Math.max(max, sum);
                     }
                 }
             }
         }
 
-        bw.write(max + "\n");
+        System.out.println(max);
 
-        bw.flush();
         br.close();
-        bw.close();
     }
 }
