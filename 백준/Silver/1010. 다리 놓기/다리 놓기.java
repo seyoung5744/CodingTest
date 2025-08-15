53e3e87b7a0298db8cd71e6a1378d20fee242063
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-15 10:35:53 +0900

    [Silver V] Title: 다리 놓기, Time: 88 ms, Memory: 12772 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -5,33 +5,33 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static int[][] dp = new int[30][30];
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
-
         while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
-            System.out.println(bridge(M, N));
+            System.out.println(bridge(N, M));
         }
+
+        br.close();
     }
 
-    public static int bridge(int N, int M) {
+    private static int bridge(int n, int m) {
+        int[][] dp = new int[n + 1][m + 1];
 
-        if (dp[N][M] > 0) {
-            return dp[N][M];
+        for (int i = 1; i < m + 1; i++) {
+            dp[1][i] = i;
         }
 
-        if (M == 0 || M == N) {
-            return dp[N][M] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = i; j < m + 1; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+            }
         }
-
-        return dp[N][M] = bridge(N - 1, M - 1) + bridge(N - 1, M);
+        return dp[n][m];
     }
-
 }
