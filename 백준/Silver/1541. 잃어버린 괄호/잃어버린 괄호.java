06af8db266c1042f6e92a4f518aafda6ea79f646
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-17 11:22:52 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 64 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,39 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static long ans = Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine(),"-");
-		
-		while(st.hasMoreTokens()) {
-			
-			int temp = 0;
-			
-			StringTokenizer str = new StringTokenizer(st.nextToken(),"+");
-			
-			while(str.hasMoreTokens()) {
-				temp += Integer.parseInt(str.nextToken());
-			}
-			
-			if(ans == Integer.MAX_VALUE) 
-				ans = temp;
-			else
-				ans -= temp;
+        long answer = Integer.MAX_VALUE;
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+        while (st.hasMoreTokens()) {
+            StringTokenizer plus = new StringTokenizer(st.nextToken(), "+");
 
-		}
+            int sum = 0;
+            while (plus.hasMoreTokens()) {
+                sum += Integer.parseInt(plus.nextToken());
+            }
 
-		System.out.println(ans);
-		
-		}
-	
+            if (answer == Integer.MAX_VALUE) {
+                answer = sum;
+            } else {
+                answer -= sum;
+            }
+        }
+
+
+        System.out.println(answer);
+
+        br.close();
+    }
+
 }
