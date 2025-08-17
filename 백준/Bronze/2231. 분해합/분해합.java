c576f85a3b5cd4d53c4bc5e0988ab432ab513a13
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-17 10:49:18 +0900

    [Bronze II] Title: 분해합, Time: 88 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,38 +1,33 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
+
         int N = Integer.parseInt(br.readLine());
 
-        int answer = Integer.MAX_VALUE;
-
-        int start = (int) Math.pow(10, (int) (Math.log10(N) + 1) - 1);
-
-        for (int i = 0; i < N; i++) {
-            int num = i;
-            int digit = (int) (Math.log10(num) + 1); // 자리수
-            int sum = 0;
-            for (int j = 0; j < digit; j++) {
-                sum += (num % 10);
-                num /= 10;
-            }
-            if (i + sum == N) {
-                answer = Math.min(answer, i);
+        int answer = 0;
+        for (int i = 1; i < N; i++) {
+            int sum = distributionSum(i);
+            if (sum == N) {
+                answer = i;
+                break;
             }
         }
-
-        if(answer == Integer.MAX_VALUE){
-            answer = 0;
-        }
-
-        bw.write(answer + "\n");
-
-        bw.flush();
+        System.out.println(answer);
         br.close();
-        bw.close();
+    }
+
+    public static int distributionSum(int num) {
+        int sum = num;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
     }
 }
