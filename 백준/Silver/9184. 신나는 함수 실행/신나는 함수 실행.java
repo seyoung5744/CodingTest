e78773fab483973e076751e5185b4f1135dc9d34
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-19 09:49:08 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1316 ms, Memory: 68288 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -5,50 +5,39 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static int[][][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        dp = new int[21][21][21];
-
+        int[][][] dp = new int[21][21][21];
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
             if (a == -1 && b == -1 && c == -1) {
                 break;
             }
-
-            System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+            System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c, dp));
         }
 
+        br.close();
     }
 
-    private static int w(int a, int b, int c) {
-        if (inRange(a, b, c) && dp[a][b][c] != 0) {
+    public static int w(int a, int b, int c, int[][][] dp) {
+        if (0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20 && dp[a][b][c] != 0) {
             return dp[a][b][c];
         }
-
         if (a <= 0 || b <= 0 || c <= 0) {
             return 1;
         }
 
         if (a > 20 || b > 20 || c > 20) {
-            return dp[20][20][20] = w(20, 20, 20);
+            return dp[20][20][20] = w(20, 20, 20, dp);
         }
-
         if (a < b && b < c) {
-            return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+            return dp[a][b][c] = w(a, b, c - 1, dp) + w(a, b - 1, c - 1, dp) - w(a, b - 1, c, dp);
         }
 
-        return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        return dp[a][b][c] = w(a - 1, b, c, dp) + w(a - 1, b - 1, c, dp) + w(a - 1, b, c - 1, dp) - w(a - 1, b - 1, c - 1, dp);
     }
-
-    private static boolean inRange(int a, int b, int c) {
-        return 0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20;
-    }
-
 }
