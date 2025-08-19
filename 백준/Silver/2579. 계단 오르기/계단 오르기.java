d2dcb006d3f8f01a34ec1a28e4b55e386eb5811e
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-19 09:12:13 +0900

    [Silver III] Title: 계단 오르기, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -5,39 +5,27 @@ import java.util.Arrays;
 
 public class Main {
 
-    private static int max;
-    private static int[] steps;
-    private static Integer[] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        steps = new int[n + 1];
-        dp = new Integer[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            steps[i] = Integer.parseInt(br.readLine());
+        int stairCount = Integer.parseInt(br.readLine());
+        int[] scores = new int[stairCount + 1];
+        for (int i = 1; i <= stairCount; i++) {
+            scores[i] = Integer.parseInt(br.readLine());
         }
 
-        dp[0] = steps[0];
-        dp[1] = steps[1];
+        int[] dp = new int[stairCount + 1];
+        dp[1] = scores[1];
 
-        if (n >= 2) {
-            dp[2] = steps[1] + steps[2];
+        if (stairCount >= 2) {
+            dp[2] = scores[1] + scores[2];
+        }
+        // 6 10 20 15 25 10 20
+        for (int i = 3; i <= stairCount; i++) {
+            dp[i] = Math.max(dp[i - 2], dp[i - 3] + scores[i - 1]) + scores[i];
         }
 
-        System.out.println(find(n));
-    }
-
-    private static int find(int n) {
-
-        if (dp[n] == null) {
-
-            dp[n] = Math.max(find(n - 2), find(n - 3) + steps[n - 1]) + steps[n];
-
-        }
-
-        return dp[n];
+        System.out.println(dp[stairCount]);
+        br.close();
     }
 }
