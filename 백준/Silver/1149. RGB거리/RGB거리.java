0b2c5fadb3556d002b30a133ed564f32dbeac7dc
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-20 09:37:03 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 12028 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -9,44 +9,26 @@ public class Main {
     private static final int GREEN = 1;
     private static final int BLUE = 2;
 
-    private static int[][] dp;
-    private static int[][] rgb;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        dp = new int[n][3];
-        rgb = new int[n][3];
+        int N = Integer.parseInt(br.readLine());
+        int[][] rgb = new int[N + 1][3];
 
-        StringTokenizer st = null;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             rgb[i][RED] = Integer.parseInt(st.nextToken());
             rgb[i][GREEN] = Integer.parseInt(st.nextToken());
             rgb[i][BLUE] = Integer.parseInt(st.nextToken());
         }
 
-        dp[0][RED] = rgb[0][RED];
-        dp[0][GREEN] = rgb[0][GREEN];
-        dp[0][BLUE] = rgb[0][BLUE];
-
-        System.out.println(Math.min(paint(n-1, RED), Math.min(paint(n-1, GREEN), paint(n-1, BLUE))));
-    }
-
-    private static int paint(int n, int color) {
-
-        if(dp[n][color] == 0) {
-            switch (color) {
-                case RED:
-                    return dp[n][RED] = rgb[n][RED] + Math.min(paint(n - 1, GREEN), paint(n - 1, BLUE));
-                case GREEN:
-                    return dp[n][GREEN] =rgb[n][GREEN] + Math.min(paint(n - 1, RED), paint(n - 1, BLUE));
-                case BLUE:
-                    return dp[n][BLUE] =rgb[n][BLUE] + Math.min(paint(n - 1, GREEN), paint(n - 1, RED));
-            }
+        for (int i = 1; i <= N; i++) {
+            rgb[i][RED] += Math.min(rgb[i - 1][GREEN], rgb[i - 1][BLUE]);
+            rgb[i][GREEN] += Math.min(rgb[i - 1][RED], rgb[i - 1][BLUE]);
+            rgb[i][BLUE] += Math.min(rgb[i - 1][RED], rgb[i - 1][GREEN]);
         }
-
-        return dp[n][color];
+        int answer = Math.min(rgb[N][RED], Math.min(rgb[N][GREEN], rgb[N][BLUE]));
+        System.out.println(answer);
+        br.close();
     }
 }
