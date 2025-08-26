7039f4c059ef2b98ba77c9cb8d3cb637774ce409
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2025-08-26 09:16:41 +0900

    [Silver I] Title: 정수 삼각형, Time: 196 ms, Memory: 23480 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,56 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static int N;
-    public static int[][] triangle;
-    public static Integer[][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-
-        triangle = new int[N][N];
-        dp = new Integer[N][N];
-
-        for (int i = 0; i < N; i++) {
-            String[] tokens = br.readLine().split(" ");
-            for (int j = 0; j < tokens.length; j++) {
-                triangle[i][j] = Integer.parseInt(tokens[j]);
+        int n = Integer.parseInt(br.readLine());
+        int[][] board = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int idx = 0;
+            while (st.hasMoreTokens()) {
+                board[i][idx++] = Integer.parseInt(st.nextToken());
             }
         }
 
-        dp[0][0] = triangle[0][0];
-        for (int i = 0; i < N; i++) {
-            maxValue(N - 1, i);
-        }
-
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < N; i++) {
-            if(dp[N-1][i] > max){
-                max = dp[N-1][i];
+        for (int i = n - 2; i >= 0; i--) { // 3
+            for (int j = 0; j < i + 1; j++) {
+                board[i][j] += Math.max(board[i + 1][j], board[i + 1][j + 1]);
             }
         }
-        System.out.println(max);
+
+        System.out.println(board[0][0]);
+        br.close();
     }
-
-
-    public static int maxValue(int floor, int index) {
-        if (floor < 0 || index < 0 || index >= N) {
-            return 0;
-        }
-
-        if (dp[floor][index] == null) {
-            dp[floor][index] = triangle[floor][index] + Math.max(maxValue(floor - 1, index - 1), maxValue(floor - 1, index));
-
-        }
-
-        return dp[floor][index];
-    }
-
 }
